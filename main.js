3e6f07a43775437ff9fd4c39ed4082dc5b709a2c
lapeterson01 <lapeterson01@gmail.com>
2018-01-16 13:23:31 -0500

    Change ticTacToe to board

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("Input move with \'play(" + "%cinput move here" + ")\'", "font-style: italic");
 
-var ticTacToe = [
+var board = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9]
@@ -9,16 +9,34 @@ var ticTacToe = [
 var player1 = {
   type: "X",
   turn: true
-}
+};
+
 var player2 = {
   type: "O",
   turn: !player1.turn
-}
+};
+
 if (player1.turn) {
   var player = player1;
 } else if (player2.turn) {
   var player = player2;
 };
+
+var setBoard = function() {
+  board = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+  ];
+  for (i = 0; i < board.length; i++) {
+    console.log("" + board[0][0] + " " + board[0][1] + " " + board[0][2] + "\n" + board[1][0] + " " + board[1][1] + " " + board[1][2] + "\n" + board[2][0] + " " + board[2][1] + " " + board[2][2]);
+  }
+  if (player == player1) {
+    console.log("%cPLAYER 1, it is your turn.", "font-weight: bold");
+  } else if (player == player2) {
+    console.log("%cPLAYER 2, it is your turn.", "font-weight: bold");
+  };
+}
 
 var checkWinner = function() {
   if (player1.turn) {
@@ -28,55 +46,25 @@ var checkWinner = function() {
     var player = player2;
     var nPlayer = player1;
   };
-  if (ticTacToe[0][0] == nPlayer.type && ticTacToe[0][1] == nPlayer.type && ticTacToe[0][2] == nPlayer.type
-  || ticTacToe[1][0] == nPlayer.type && ticTacToe[1][1] == nPlayer.type && ticTacToe[1][2] == nPlayer.type
-  || ticTacToe[2][0] == nPlayer.type && ticTacToe[2][1] == nPlayer.type && ticTacToe[2][2] == nPlayer.type
-  || ticTacToe[0][0] == nPlayer.type && ticTacToe[1][0] == nPlayer.type && ticTacToe[2][0] == nPlayer.type
-  || ticTacToe[0][1] == nPlayer.type && ticTacToe[1][1] == nPlayer.type && ticTacToe[2][1] == nPlayer.type
-  || ticTacToe[0][2] == nPlayer.type && ticTacToe[1][2] == nPlayer.type && ticTacToe[2][2] == nPlayer.type
-  || ticTacToe[0][0] == nPlayer.type && ticTacToe[1][1] == nPlayer.type && ticTacToe[2][2] == nPlayer.type
-  || ticTacToe[0][2] == nPlayer.type && ticTacToe[1][1] == nPlayer.type && ticTacToe[2][0] == nPlayer.type) {
+  if (board[0][0] == nPlayer.type && board[0][1] == nPlayer.type && board[0][2] == nPlayer.type
+  || board[1][0] == nPlayer.type && board[1][1] == nPlayer.type && board[1][2] == nPlayer.type
+  || board[2][0] == nPlayer.type && board[2][1] == nPlayer.type && board[2][2] == nPlayer.type
+  || board[0][0] == nPlayer.type && board[1][0] == nPlayer.type && board[2][0] == nPlayer.type
+  || board[0][1] == nPlayer.type && board[1][1] == nPlayer.type && board[2][1] == nPlayer.type
+  || board[0][2] == nPlayer.type && board[1][2] == nPlayer.type && board[2][2] == nPlayer.type
+  || board[0][0] == nPlayer.type && board[1][1] == nPlayer.type && board[2][2] == nPlayer.type
+  || board[0][2] == nPlayer.type && board[1][1] == nPlayer.type && board[2][0] == nPlayer.type) {
     console.log("%cGame over!", "font-size: 40px")
     if (nPlayer == player1) {
       console.log("%cPLAYER 1 WINS!", "font-size: 50px");
     } else if (nPlayer == player2) {
       console.log("%cPLAYER 2 WINS!", "font-size: 50px");
     };
-    var setTicTacToe = function() {
-      ticTacToe = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9]
-      ];
-      for (i = 0; i < ticTacToe.length; i++) {
-        console.log("" + ticTacToe[0][0] + " " + ticTacToe[0][1] + " " + ticTacToe[0][2] + "\n" + ticTacToe[1][0] + " " + ticTacToe[1][1] + " " + ticTacToe[1][2] + "\n" + ticTacToe[2][0] + " " + ticTacToe[2][1] + " " + ticTacToe[2][2]);
-      }
-      if (player == player1) {
-        console.log("%cPLAYER 1, it is your turn.", "font-weight: bold");
-      } else if (player == player2) {
-        console.log("%cPLAYER 2, it is your turn.", "font-weight: bold");
-      };
-    }
-    return setTicTacToe();
-  } else if (ticTacToe[0][0] != 1 && ticTacToe[0][1] != 2 && ticTacToe[0][2] != 3 && ticTacToe[1][0] != 4 && ticTacToe[1][1] != 5 && ticTacToe[1][2] != 6 && ticTacToe[2][0] != 7 && ticTacToe[2][1] != 8 && ticTacToe[2][2] != 9) {
+    return setBoard();
+  } else if (board[0][0] != 1 && board[0][1] != 2 && board[0][2] != 3 && board[1][0] != 4 && board[1][1] != 5 && board[1][2] != 6 && board[2][0] != 7 && board[2][1] != 8 && board[2][2] != 9) {
     console.log("%cGame over!", "font-size: 40px");
     console.log("%cIt's a tie...", "font-size: 50px");
-    var setTicTacToe = function() {
-      ticTacToe = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9]
-      ];
-      for (i = 0; i < ticTacToe.length; i++) {
-        console.log("" + ticTacToe[0][0] + " " + ticTacToe[0][1] + " " + ticTacToe[0][2] + "\n" + ticTacToe[1][0] + " " + ticTacToe[1][1] + " " + ticTacToe[1][2] + "\n" + ticTacToe[2][0] + " " + ticTacToe[2][1] + " " + ticTacToe[2][2]);
-      }
-      if (player == player1) {
-        console.log("%cPLAYER 1, it is your turn.", "font-weight: bold");
-      } else if (player == player2) {
-        console.log("%cPLAYER 2, it is your turn.", "font-weight: bold");
-      };
-    }
-    return setTicTacToe();
+    return setBoard();
   } else {
     if (player == player1) {
       console.log("%cPLAYER 1, it is your turn.", "font-weight: bold");
@@ -86,14 +74,14 @@ var checkWinner = function() {
   };
 };
 
-var showTicTacToe = function() {
-  for (i = 0; i < ticTacToe.length; i++) {
-    console.log("" + ticTacToe[0][0] + " " + ticTacToe[0][1] + " " + ticTacToe[0][2] + "\n" + ticTacToe[1][0] + " " + ticTacToe[1][1] + " " + ticTacToe[1][2] + "\n" + ticTacToe[2][0] + " " + ticTacToe[2][1] + " " + ticTacToe[2][2]);
+var showBoard = function() {
+  for (i = 0; i < board.length; i++) {
+    console.log("" + board[0][0] + " " + board[0][1] + " " + board[0][2] + "\n" + board[1][0] + " " + board[1][1] + " " + board[1][2] + "\n" + board[2][0] + " " + board[2][1] + " " + board[2][2]);
   };
   return checkWinner();
 };
 
-showTicTacToe();
+showBoard();
 
 var play = function(num) {
   var errMsg = function() {
@@ -108,56 +96,56 @@ var play = function(num) {
     var player = player2;
   };
   if (num == 1) {
-    if (ticTacToe[0][0] == num) {
-      ticTacToe[0][0] = player.type;
+    if (board[0][0] == num) {
+      board[0][0] = player.type;
     } else {
       return errMsg();
     }
   } else if (num == 2) {
-    if (ticTacToe[0][1] == num) {
-      ticTacToe[0][1] = player.type;
+    if (board[0][1] == num) {
+      board[0][1] = player.type;
     } else {
       return errMsg();
     }
   } else if (num == 3) {
-    if (ticTacToe[0][2] == num) {
-      ticTacToe[0][2] = player.type;
+    if (board[0][2] == num) {
+      board[0][2] = player.type;
     } else {
       return errMsg();
     }
   } else if (num == 4) {
-    if (ticTacToe[1][0] == num) {
-      ticTacToe[1][0] = player.type;
+    if (board[1][0] == num) {
+      board[1][0] = player.type;
     } else {
       return errMsg();
     }
   } else if (num == 5) {
-    if (ticTacToe[1][1] == num) {
-      ticTacToe[1][1] = player.type;
+    if (board[1][1] == num) {
+      board[1][1] = player.type;
     } else {
       return errMsg();
     }
   } else if (num == 6) {
-    if (ticTacToe[1][2] == num) {
-     ticTacToe[1][2] = player.type;
+    if (board[1][2] == num) {
+     board[1][2] = player.type;
    } else {
      return errMsg();
    }
   } else if (num == 7) {
-    if (ticTacToe[2][0] == num) {
-      ticTacToe[2][0] = player.type;
+    if (board[2][0] == num) {
+      board[2][0] = player.type;
     } else {
       return errMsg();
     }
   } else if (num == 8) {
-    if (ticTacToe[2][1] == num) {
-      ticTacToe[2][1] = player.type;
+    if (board[2][1] == num) {
+      board[2][1] = player.type;
     } else {
       return errMsg();
     }
   } else if (num == 9) {
-    if (ticTacToe[2][2] == num) {
-      ticTacToe[2][2] = player.type;
+    if (board[2][2] == num) {
+      board[2][2] = player.type;
     } else {
       return errMsg();
     }
@@ -166,5 +154,5 @@ var play = function(num) {
   };
   player1.turn = !player1.turn;
   player2.turn = !player1.turn;
-  return showTicTacToe();
+  return showBoard();
 };
