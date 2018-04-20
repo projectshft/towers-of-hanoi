75a91835dde1a537dcacf7ded293794e979b83a3
isfturtle <singingturtle@gmail.com>
2018-04-20 11:22:40 -0400

    Works in repl.it, but when in my HTML file it doesn't console.log

---
+++ main.js
@@ -0,0 +1,147 @@
+class Board {
+  constructor(){
+    this.board =
+    [[1,2,3],
+    [4,5,6],
+    [7,8,9]];
+    this.available=[1,2,3,4,5,6,7,8,9];
+    this.ready = false;
+  }
+  addPlayer(player){
+    if(this.player1===undefined){
+      this.player1=player;
+      this.player1.setTurn(true);
+    }
+    else if(this.player2===undefined){
+      this.player2=player;
+      this.player2.setTurn(false);
+      this.ready = true;
+      this.print();
+    }
+    else{
+      console.log("May only have two players");
+    }
+  }
+  get checkWin(){
+    for(var i=0;i<3;i++){
+      if(this.board[0][i]==this.board[1][i] && this.board[1][i]==this.board[2][i]){
+        return true;
+      }
+      if(this.board[i][0]==this.board[i][1] && this.board[i][1]==this.board[i][2]){
+        return true;
+      }
+    }
+    if(this.board[0][0]==this.board[1][1] && this.board[1][1]==this.board[2][2]){
+      return true;
+    }
+    if(this.board[2][0]==this.board[1][1] && this.board[1][1]==this.board[0][2]){
+      return true;
+    }
+    if(this.available.length==0){
+      return "tie";
+    }
+    return false;
+  }
+  update(n, symbol){
+    if(!this.ready){
+      console.log("Please add players");
+    }
+    else if(this.available.indexOf(n)==-1){
+      console.log("Position "+n+" is not available. Choose another option.");
+    }
+    else{
+      switch(n){
+        case 1:
+          this.board[0][0]=symbol;
+          break;
+        case 2:
+          this.board[0][1]=symbol;
+          break;
+        case 3:
+          this.board[0][2]=symbol;
+          break;
+        case 4:
+          this.board[1][0]=symbol;
+          break;
+        case 5:
+          this.board[1][1]=symbol;
+          break;
+        case 6:
+          this.board[1][2]=symbol;
+          break;
+        case 7:
+          this.board[2][0]=symbol;
+          break;
+        case 8:
+          this.board[2][1]=symbol;
+          break;
+        case 9:
+          this.board[2][2]=symbol;
+          break;
+        default:
+          console.log("Please choose a position between 1 and 9");
+          break;
+      }
+      this.print();
+      this.available =this.available.filter(num => num !=n);
+      //console.log(this.available);
+      if(player1.turn){
+        player1.setTurn(false);
+        player2.setTurn(true);
+      }
+      else{
+        player1.setTurn(true);
+        player2.setTurn(false);
+      }
+    }
+  }
+  reset(){
+    this.board =
+      [[1,2,3],
+      [4,5,6],
+      [7,8,9]];
+    this.available=[1,2,3,4,5,6,7,8,9];
+    this.ready = false;
+    this.player1 = undefined;
+    this.player2 = undefined;
+  }
+  print(){
+    for(var i=0;i<3;i++){
+      debugger
+      console.log(this.board[i]);
+    }
+    console.log("");
+  }
+}
+
+class Player {
+  constructor(name, symbol, board){
+    this.name=name;
+    this.symbol=symbol;
+    this.board = board;
+    this.board.addPlayer(this);
+  }
+  setTurn(turn){
+    this.turn=turn;
+  }
+  move(n){
+    if(this.turn){
+      this.board.update(n, this.symbol);
+      //console.log(this.board.checkWin);
+      if(this.board.checkWin=="tie"){
+        console.log("This game was a tie!");
+        this.board.reset();
+      }
+      else if(this.board.checkWin){
+        console.log("Congratulations "+this.name+"! You are the winner");
+        this.board.reset();
+      }
+    }
+    else{
+      console.log("Wait your turn!");
+    }
+  }
+}
+
+var myBoard = new Board();
+myBoard.print();
