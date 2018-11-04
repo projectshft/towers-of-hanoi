698d5f326d23409d0a5211c048e0920167e47fed
Rhys Lutsky <rhyslutsky@gmail.com>
2018-11-04 18:19:35 -0500

    created Moves and Board Module

--- main.js
+++ main.js
@@ -1,41 +1,63 @@
-// A board representing 3 pegs, and 5 discs on the first peg in ascending order.  This is a normal starting position for the game. Take off keys
-var board = [
-    ["5", "4", "3", "2", "1"],
-    [],
-    []
-];
-
-//We'll want to be able to print the board horizontally. You MUST utilize a map function at least once 
-//start with copying startboard to currentboard and use it through out game??????
-
-// Move the disc from peg 1 to peg 2 AND game accepts or rejects move
-//Is there a disc on the peg, if Yes what is its value
-var checkPeg = function (fromPeg, toPeg) {
-    if (board[fromPeg - 1].length != 0) {
-        var topDisc = board[fromPeg - 1][board[fromPeg - 1].length - 1];
-        return topDisc;
-    } else {
-        console.log("This peg does not have a disc; try again.");
-    }
+// A board representing 3 pegs, and 5 discs on the first peg in ascending order.  This is a normal starting position for the game. 
+//BOARD MODULE
+var Set = function (moves, currentState) {
+    var board = [
+        ["5", "4", "3", "2", "1"],
+        [],
+        []
+    ];
+    var Board = {
+        moves: function () {
+            var counter = 0;
+            counter += 1;
+            return counter;
+        },
+        state: board.map(function () {
+            return moveDisc();
+        })
+    };
 };
-checkPeg(1, 3);
-checkPeg(2, 3);
 
 
 
-//which pegs can take that disc value (num<top num) array[array.length -1] = top item 
-/*var whichPeg = function (fromPeg, toPeg) {
-    if (board[toPeg - 1])
-}
-board.filter(function (peg) {
-//     if (peg[peg.length - 1] > )
-// }
 
 
+//MOVES MODULE 
+var Moves = function (fromPeg, toPeg) {
+    //generic board
+    var pegs = {
+        fromPeg: fromPeg,
+        toPeg: toPeg
+    };
 
-/*board.moveDisc(1, 2);
-That move was successful, board is now:
---- 5 4 3 2
---- 1
----*/
+    //1. which pegs are not the fromPeg
+    var availPeg = Board.state.filter(function (pegs) {
+        return Board[state][i] !== fromPeg;
+    });
+
+    //2. Which availPegs can take the fromDisc?
+    var checkPeg = function () {
+        if (Board.state[fromPeg - 1].length == 0) {
+            console.log("This peg does not have a disc. Try again.  The board is still:" + Board.state);
+        } else if (toPeg == availPeg && Board[state][fromPeg - 1][Board[state][fromPeg - 1].length - 1] < Board[state][toPeg - 1][toPeg - 1].length - 1) {
+            moveDisc();
+        } else {
+            console.log("Remember that you can only move a disc onto a larger disc." + "The board is still :" + Board.state);
+
+        }
+    };
+
+    //3. move the discs
+    var moveDisc = function () {
+        Board.state[fromPeg - 1].pop();
+        Board.state[toPeg - 1].push();
+        Board.moves();
+
+    }
+    return moveDisc;
+};
+
+var setUpBoard = Set();
+
+var guess1 = Moves(1, 2);
 
