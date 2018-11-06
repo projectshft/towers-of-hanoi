e96aaf5ef57c3c68af47488fb055106d2f3b65f9
Rhys Lutsky <rhyslutsky@gmail.com>
2018-11-06 17:53:34 -0500

    declare moveDisc, availPeg, moveCount and checkWinner functions

--- main.js
+++ main.js
@@ -8,7 +8,12 @@ var board = [
 console.log(board);
 
 //User submits to move the disc from 1 peg to another
-moveDisc(fromPeg, toPeg)
+//fromPeg and toPeg need to be avail to
+var moveDisc = function (fromDisc, toDisc) {
+    board.pop(fromDisc);
+    board.push(toDisc);
+    //recreate board with move included
+}
 moveDisc(1, 2); //this is an example
 
 var fromDisc = board[fromPeg - 1][board[fromPeg - 1].length - 1];
@@ -27,10 +32,37 @@ var checkPeg = function (fromPeg) {
 };
 /*Check to see if the toPeg can take that disc; the value of its top disc is < the value of the fromPeg topDisc.  You can only put the disc on a smaller disc. If yes,  move the peg and add to the move count. If no, ask user to choose the pegs again and show them the board.*/
 var availPeg = function (toPeg) {
-    (var i = 0; i < board.length; i++) {
-        if board[toPeg - 1][i] =
+    for (var i = 0; i < board.length; i++) {
+        if (fromDisc < toDisc) {
+            moveDisc();
+            moveCount();
+        } else {
+            console.log("Remember that you can only move a disc onto a larger disc. Please try again" + "The board is still :" + board)
+        }
     }
 }
+
+var moveCount = function () {
+    var count = 0;
+    count = +;
+    return count;
+}
+console.log("Moves: " + count);
+
 // Repeat steps
 /*Win when all 5 discs are in order on a different peg than they started. Check if they won after every move.  If yes, end the game by announcing the winner (console.log) and reset the board.*/
-
+var checkWinner = function () {
+    for (var i = 0, i < board.length; i++) {
+        if (board[].length == 5) {
+            //check order or not, since would not let move unless in order
+            console.log("YOU DID IT!");
+            board = [
+                ["5", "4", "3", "2", "1"],
+                [],
+                []
+            ];
+        } else {
+            console.log("Please submit your next move.")
+        }
+    }
+}
