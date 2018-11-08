e4d18806a6107a93b760955e4d9cc2528b67b31a
PGNQ <41378170+PGNQ@users.noreply.github.com>
2018-11-08 17:17:46 -0500

    Change checkWinner() to work for any number of pegs and discs defined by the user upon initialization.

--- main.js
+++ main.js
@@ -1,38 +1,57 @@
 
-var game = function () {
-
-  var theBoard = [];
-  var numberOfMoves = 0;
+var game = function (numberOfPegs, numberOfDiscs) {
 
   /*
-  setBoard should initialize the game board to a user defined number of pegs and discs. The board will always be initialized where all the discs are stacked on the first peg, which is index 0 of theBoard.
+  Description of variables used in the game object:
+    theBoard : This is the state of the board. It should only be able to be
+      altered by the methods moveDisc(arg1,   arg2) and resetBoard().
+    numberOfMoves : tracker to keep track of all valid moves throughout the
+      game play. This value should be printed to the console upon winning the
+      game. The method resetBoard() reinitializes the variable to zero.
+    numberOfPegsOnBoard : user-defined choice that initializes the board's
+      number of pegs.
+    numberOfDiscsOnBoard : user-defined choice that initializes the board's
+      number of discs.
   */
-  var setBoard = function (numberOfPegs, numberOfDiscs) {
-    for (var peg = 0; peg < numberOfPegs; peg++){
+  var theBoard = [];
+  var numberOfMoves = 0;
+  var numberOfPegsOnBoard = numberOfPegs;
+  var numberOfDiscsOnBoard = numberOfDiscs;
+
+  /*
+  setBoard() is a private method that should set the board according to the user
+  defined number of pegs and number of discs when the game object is created.
+  setBoard() is also invoked by resetBoard() at any time by the user, or
+  automatically when the game has been won.
+  */
+  var setBoard = function() {
+    for (var peg = 0; peg < numberOfPegsOnBoard; peg++) {
       theBoard[peg]=[];
-      for (var disc = 1; disc <= numberOfDiscs; disc++){
+      for (var disc = 1; disc <= numberOfDiscsOnBoard; disc++) {
         if (peg === 0){
         theBoard[peg].unshift(disc);
         }
       }
     }
   }
+  setBoard();
 
   /*
-  resetBoard should reset the game board to the orginal state of the board when the game was initialized, utilizing the same number of pegs and discs chosen by the user. Also, the total number of moves being tracked for the game is reset to zero.
+  resetBoard should reset the game board to the orginal state of the board when
+  the game was initialized, utilizing the same number of pegs and discs chosen
+  by the user. Also, the total number of moves being tracked for the game is
+  reset to zero. This method is called automatically when the game is won, or
+  can be called at any time by the user.
   */
   var resetBoard = function () {
     numberOfMoves = 0;
-    var discCount = 0;
-    for (var i=0; i<theBoard.length; i++){
-      discCount += theBoard[i].length;
-    }
-    setBoard(theBoard.length, discCount);
+    setBoard();
   }
 
   /*
-  printBoard() is a user accessible method to print the current state of the
-  game board, theBoard.
+  printBoard() is a user-accessible method that should print the current state
+  of the game board, theBoard, automatically after every move attempt and board
+  reset. This method should also be called by the user at any time.
   */
   var printBoard = function () {
     for (var i = 0; i < theBoard.length; i++){
@@ -48,8 +67,9 @@ var game = function () {
   }
 
   /*
-  moveDisc(pegOrig, pegDest) is a user-accessible method to move discs from one peg (pegOrig) to another
-  peg (pegDest). Only valid moves will increment the move counter, numberOfMoves.
+  moveDisc(pegOrig, pegDest) is a user-accessible method to move discs from one
+  peg (pegOrig) to another peg (pegDest). Only valid moves will increment the
+  move counter, numberOfMoves.
   */
   var moveDisc = function (pegOrig, pegDest) {
     var validStr = "That move was successful. Board is now:";
@@ -88,23 +108,23 @@ var game = function () {
   }
 
   /*
-  help() is a method accessible by the user to help determine valid moves from a chosen
-  peg. Handles the error of the chosen peg being empty.
+  help() is a method accessible by the user to help determine valid moves from
+  a chosen peg. Handles the error of the chosen peg being empty.
   */
   var help = function (givenPeg) {
     var givenPegValue = theBoard[givenPeg-1][theBoard[givenPeg-1].length-1];
     var outputPegStr = '';
     if (givenPegValue === undefined){
-      console.log("There is no disc on the chosen peg.");
+      console.log("There is no disc on peg " + givenPeg + ". Try another peg.");
     } else {
       var validPegs = theBoard.filter(function(boardPeg){
         return (boardPeg.length === 0 || (givenPegValue < boardPeg[boardPeg.length - 1]))
       });
       /*
       validPegs is an array of every valid peg that the givenPeg can be moved to.
-      However, the index of validPegs does not represent the actual pegs of the game board.
-      The following nested loop compares the validPegs to theBoard to build a string of
-      valid peg numbers.
+      However, the index of validPegs does not represent the actual pegs of the
+      game board. The following nested loop compares the validPegs to theBoard
+      to build a string of valid peg numbers.
       */
       for (var i = 0; i < theBoard.length; i++){
         for (var j = 0; j < validPegs.length; j++){
@@ -114,12 +134,12 @@ var game = function () {
         }
       }
       if (outputPegStr == "") {
-        console.log("There are no allowed moves from the chosen peg.");
+        console.log("There are no allowed moves from peg " + givenPeg + ". Try another peg.");
       } else {
-      console.log("Valid moves are to peg(s) " + outputPegStr);
+      console.log("From peg: " + givenPeg + ", valid moves are to peg(s) " + outputPegStr);
       }
     }
-
+    printBoard();
   }
 
 /*
@@ -128,29 +148,28 @@ A value of 15 indicates that all 5 discs exist on that peg, and the game has
 been solved. checkWinner() is not accessible by the user.
 */
   var checkWinner = function () {
-    var pegTwoCount = theBoard[1].reduce(function(sum, number){
-      return sum + number;
-    }, 0);
-    var pegThreeCount = theBoard[2].reduce(function(sum, number){
-      return sum + number;
-    }, 0);
-    return (pegTwoCount === 15 || pegThreeCount === 15);
+
+    var result = theBoard.reduce(function(state, peg){
+      if (!state) {
+        return (peg.length == numberOfDiscsOnBoard && peg != theBoard[0]);
+      } else {
+        return true;
+      }
+    }, false);
+
+    return result;
   }
 
   return {
     printBoard: printBoard,
     moveDisc: moveDisc,
     help: help,
-    setBoard: setBoard,
     resetBoard: resetBoard
   }
 }
 
 //initialize the game
-var game = game();
-
-//setup the board with 3 pegs and 5 discs
-game.setBoard(3,5);
+var game = game(3,5);
 
 //check printBoard() functionality
 game.printBoard();
@@ -200,3 +219,7 @@ game.help(2);
 //sequence showing an invalid move where chosen peg's disc is larger than destination
 game.moveDisc(1,2);
 game.moveDisc(1,2);
+game.moveDisc(1,3);
+
+//test help() for no move available
+game.help(1);
