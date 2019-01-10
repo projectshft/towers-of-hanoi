989893bb8ef77723f6c3ed7088f757aaa3cd53f9
aidanhennessy99 <aidanhennessy99@gmail.com>
2019-01-10 16:57:18 -0500

    complete

--- main.js
+++ main.js
@@ -1,67 +1,77 @@
-var HanoiTower = function(disk, start, end, via) {
-var board = [[3, 2, 1], [], []];
+//This is the Board State Array.
+var pegs = [[3, 2, 1], [], []];
 
-  var displayDisc = board.map(function (row) {
-        console.log(row.join(''));
-  });
+//This is meant to maintain the state of the board.
+var boardState = {
+  pegs,
+  count: 0,
+  gameWon: false
+}
 
-  var moveDisc = board.filter(function (start, end) {
-    var disk = board[0].pop(2);
-    if (disk == 1) {
-      board[2].push(disk);
-      console.log("Disk Moved!");
-      console.log(board);
+//This is meant to display the Board.
+var printBoard = function() {
+  return boardState.pegs.map(function(peg, i) {
+    console.log(`Peg #${i} -- ${peg}`)
+  })
+}
 
+//This is supposed to update every time a user posts a change in console log. You put in the console a move like moveDisc(0,1) which pops the last item out of Sub-Array 0 and pushes it into becoming the first item of the 2nd Array. 
+
+var moveDisc = function(start, end) {
+  var { pegs } = boardState;
+  boardState.count += 1;
+
+  var discToMove = pegs[start][pegs[start].length - 1];
+  var pegToMoveTo = pegs[end];
+  checkWinner()
+  if (pegToMoveTo.length === 0) {
+      console.log('VALID MOVE');
+      pegs[start].pop();
+      pegToMoveTo.push(discToMove);
+      checkWinner()
+      printBoard();
+    } else if (discToMove > pegToMoveTo[pegToMoveTo.length - 1]) {
+      return alert('invalid move -- the disc you are trying to move is bigger than the top disc of the peg you are trying to move to')
+    } else {
+      console.log('VALID MOVE');
+      pegs[start].pop();
+      pegToMoveTo.push(discToMove);
+      checkWinner()
+      printBoard();
     }
-
-      else {
-        return "disk not allowed";
-      }
-       var disk = board[0].pop(1);
-   board[1].push(disk);
-       console.log("Disk Moved!");
-   console.log(board);
-   var disk = board[2].pop(0);
-   board[1].push(disk);
-       console.log("Disk Moved!");
-   console.log(board);
-   var disk = board[0].pop(0);
-   board[2].push(disk);
-       console.log("Disk Moved!");
-   console.log(board);
-   var disk = board[1].pop(1);
-   board[0].push(disk);
-       console.log("Disk Moved!");
-   console.log(board);
-   var disk = board[1].pop(0);
-   board[2].push(disk);
-       console.log("Disk Moved!");
-   console.log(board);
-    var disk = board[0].pop(0);
-   board[2].push(disk);
-       console.log("Disk Moved!");
-   console.log(board);
+}
 
 
-
-});
-
-
-  var result = board.reduce(function checkWinner(pegs) {
-    if (board[2].length = 3) {
-          console.log("Congratulations!");
-        }
-      else if (board[1].length = 3) {
-          console.log("Congratulations!");
-        }
-
-    else {
-      console.log("Sorry! Do better next time!");
+//This is meant to limit the number of moves a user can make. They cannot put a bigger number to the right of a smaller number within one of the three sub-arrays within the bigger array. 
+var checkPossibleMoves = function(startPeg) {
+  var availableMoves = pegs.filter(function(peg) {
+    if (peg.length === 0) {
+      return peg
+    } else if (peg[peg.length - 1] > startPeg) {
+      return peg
     }
-   });
+  })
+  return availableMoves;
+}
 
 
+//When you move all three Discs to another peg other than 0 where it starts, it will declare you as the winner. 
+ function checkWinner() {
+  return boardState.pegs.reduce(function(tally, peg, index){               
+      if (peg.length === 3 && index !== 0) {
+          boardState.gameWon = true;  
+          console.log('you won')
+      };
+  }, 0);
+}
+  
+ 
 
-};
+//This is meant to reset the array to what it started as. To reset it you do reset()
+ function reset() {
+  console.log('board reset!')
+  boardState.count = 0;
+  boardState.pegs = [[3, 2, 1], [], []];
+  printBoard();
+}
 
-HanoiTower(3, 'start', 'end', 'via');
