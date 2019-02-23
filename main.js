c36779e53c064a7d00edc5773351fbf3dbd41651
Knox <knox.brand@gmail.com>
2019-02-23 09:49:58 -0500

    drawBoard function working

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 var Game = function() {
-  var board = [["5", "4", "3"],["2","1"],[]]
-  var input;
-  var output;
+  var makeBoard = [["5", "4", "3"],["2","1"],[]]
 
-var makeBoard = board.map(function(position) {
-  console.log(position);
+function drawBoard() {
+  var pegOne = "";
+  var pegTwo = "";
+  var pegThree = "";
+  makeBoard = makeBoard.map(function(position,i,makeBoard) {
+    if (makeBoard[0][i]) {
+      pegOne += makeBoard[0][i] + " ";
+    } 
+    if (makeBoard[1][i]) {
+      pegTwo += makeBoard[1][i] + " ";
+    } 
+    if (makeBoard[2][i]) {
+      pegThree += makeBoard[2][i] + " ";
+    }
   return position;
 });
+  console.log(pegOne);
+  console.log(pegTwo);
+  console.log(pegThree);
+}
 
-  // var drawBoard = function() {
-  //   console.log(makeBoard[0]); //[][] for values, [] for peg(0,1,2), board for whole board as object
-  //   console.log(makeBoard[1]);
-  //   console.log(makeBoard[2]);
-  // } 
-  var moveDisc = function() {
+  var moveDisc = function(input, output) {
     if (input !== null && output !== null) {
       var a = makeBoard[input-1].length-1;
       var moveFromValue = makeBoard[input-1][a];
@@ -24,9 +33,10 @@ var makeBoard = board.map(function(position) {
     }
   }
   var userSelection = function() {
-    input = window.prompt("Move from which peg?");
-    output = window.prompt("Move to which peg?");
-    moveDisc();
+    drawBoard();
+    var input = window.prompt("Move from which peg?");
+    var output = window.prompt("Move to which peg?");
+    moveDisc(input, output);
   }
 
   return {
