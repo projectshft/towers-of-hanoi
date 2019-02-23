2367b322a3332cd2de1ff3659fc507c4d8cba638
alice <alicectung@gmail.com>
2019-02-22 20:41:52 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+console.log('Life is a game and you are the player.'+' As you master the game, so you also create it.');
+
+// create board array
+board = [['3', '2', '1'], [], []];
+  //console.log(board);
+  //console.log(board.length);
+
+// use map method to display current stateOfBoard
+stateOfBoard = board.map(function(state) {
+    // print current board
+    console.log('---' + state);
+});
+
+
+// moveDisc function to take in player submitted moves
+
+// checkMove function checks if move is valid
+
+// checkWinner function checks if won
