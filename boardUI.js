24fbee6045113eff0b4273912b90086518686355
Jason Doss <doss2k@gmail.com>
2019-02-23 16:57:56 -0500

    Add numbers above pegs, refactoring

--- boardUI.js
+++ boardUI.js
@@ -5,14 +5,11 @@ const BoardUI = function() {
   const y = canvas.height;
   const pegWidth = 20;
   const pegHeight = 200;
+  const discHeight = 20;
+  const discWidth = 60;
   pegMiddle1 = x / 4;
   pegMiddle2 = x / 2;
   pegMiddle3 = (x / 4) * 3;
-  const numOfDiscs = board.discsStart;
-  const discHeight = 20;
-  const discWidth = 60;
-
-  
 
   const drawPegs = function() {
     ctx.beginPath();
@@ -21,14 +18,19 @@ const BoardUI = function() {
     ctx.rect(pegMiddle3 - pegWidth / 2, y - pegHeight, pegWidth, pegHeight);
     ctx.fillStyle = '#850007';
     ctx.fill();
+    ctx.font = '30px Arial';
+    ctx.fillText(1, pegMiddle1 - 10, y - pegHeight - 15);
+    ctx.fillText(2, pegMiddle2 - 10, y - pegHeight - 15);
+    ctx.fillText(3, pegMiddle3 - 10, y - pegHeight - 15);
     ctx.closePath();
   }
 
   const drawDiscs = function(boardState) {
     for(let i = 0; i < boardState.length; i++){
+      var peg = 'pegMiddle'.concat(i + 1);
       for(let j = 0; j <= boardState[i].length; j++) {
         ctx.beginPath();
-        ctx.rect(window['pegMiddle' + (i + 1)] - ((discWidth / 2) * boardState[i][j]), y - discHeight * (j + 1), discWidth * boardState[i][j], discHeight);
+        ctx.rect(window[peg] - ((discWidth / 2) * boardState[i][j]), y - discHeight * (j + 1), discWidth * boardState[i][j], discHeight);
         ctx.fillStyle = '#00FF00';
         ctx.fill();
         ctx.stroke();
