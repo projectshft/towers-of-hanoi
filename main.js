d7772757917894e8ead6b26f3ea0b1fb4a7c12fc
Ritchie Fesperman <ritchie.fes@gmail.com>
2019-02-24 18:05:23 -0500

    code clean up and bug fixes

--- main.js
+++ main.js
@@ -1,22 +1,6 @@
 var myBoard = [["3", "2", "1"],[],[]]; //each nested array myBoard[0], myBoard[1], and myBoard[2] represent
 //the pegs of the board. The index of the subarrays represent discs, and their values represent the disc's diameter.
 
-// We'll want to be able to print the board horizontally.
-// You MUST utilize a map function at least once to accomplish this part of the assignment.
-// The starting board will log the 2D array to the console like this
-// (if you'd like a challenge, try printing the board vertically):
-//
-// --- 5 4 3 2 1
-// ---
-// ---
-//
-//-------- example of moving a disc ----------
-// myBoard.moveDisc(1,3);
-// That move was successful, board is now:
-// --- 5 4 3
-// --- 1
-// --- 2
-
 var boardState = { //records and manipulates the state of the board, and how many moves have been made.
   checkWinner: function(){  //must have a reduce function
       if (myBoard[1].reduce(function(acc, cV) {return parseInt(acc) + parseInt(cV)}, 0) === 6
@@ -33,10 +17,10 @@ var boardState = { //records and manipulates the state of the board, and how man
   },
   numOfMoves: 0,
   moveDisc: function(give, receive){
-    if (checkIfLegal(give, receive) === false) {
+    if (boardState.checkIfLegal(give - 1, receive - 1) === false) {
       console.log("Illegal move, try again");
-      boardState.moveDisk();
-    } else myBoard[receive - 1].push(myBoard[give - 1].pop()); // moves the "disc" from the give peg to the receive peg.
+    } else {
+      myBoard[receive - 1].push(myBoard[give - 1].pop()); // moves the "disc" from the give peg to the receive peg.
 
       boardState.numOfMoves += 1; // logs that a move was made.
 
@@ -46,22 +30,11 @@ var boardState = { //records and manipulates the state of the board, and how man
 
       console.log("Number of moves: " + boardState.numOfMoves); // logs the number of moves
 
-      boardState.CheckWinner();
-  },
+      boardState.checkWinner();
+    };
+  }
 };
 
-
-// var checkWinner = function(){
-// //must have a reduce function
-//   if (reducerPeg2 === 6 || reducerPeg3 === 6) {
-//     console.log("Winner!");
-//   }
-// }
-//
-// var reducerPeg2 = myBoard[1].reduce(function(acc, cV) {
-//   return parseInt(acc) + parseInt(cV);
-// }, 0);
-//
-// var reducerPeg3 = myBoard[2].reduce(function(acc, cV) {
-//   return parseInt(acc) + parseInt(cV);
-// }, 0);
+//Invoke boardState.moveDisc(give, receive); below to play the game.
+//The give argument is the peg you want to remove the disc from.
+//The receive argument is the peg you want to place the disc on.
