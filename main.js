0e4a65c3b4e5b7294b7b41609229a416a9be17cc
Richard Eldridge <eldridge.rich@gmail.com>
2019-02-24 13:31:41 -0500

    progress with checking for a winner

--- main.js
+++ main.js
@@ -26,33 +26,38 @@ const moveDisc = (currentPeg, newPeg) => {
     board[newPeg].push(selectedDisc);// and place it on the top of newPeg
   }
   renderBoard();
-  checkForWin(board);
+  console.log(isWinner(board))
 }
 
 //*************************************************/
 
-const isInvalidMove = (selectedDisc, newPeg) => 
-  // 'filter' out the pegs of 'board' in which selectedDisc >= the last element of the peg then check if 'some' of the elements of the filtered array are equal to the newPeg
-  board.filter(peg => (selectedDisc >= peg[peg.length - 1])).some(element => element === board[newPeg]);
+// 'filter' out the pegs of 'board' in which selectedDisc >= the last element of the peg then check if 'some' of the elements of the filtered array are equal to the newPeg
+const isInvalidMove = (selectedDisc, newPeg) => board.filter(peg => (selectedDisc >= peg[peg.length - 1])).some(element => element === board[newPeg]);
 
 //*************************************************/
 
 //There should be a function checkWinner that checks to see if the player has won the game. You win the game by putting all the discs back in the original order but on a new peg. As a part of this function, you MUST use the reduce function at least once. As a helpful hint, we suggest that you test this function with only 3 pegs and 3 discs on the board as it will take significantly less moves to "win".
 
-const checkForWin = board => {
+const isWinner = board => {
   // if I reduce this down to only the pegs with discs on them, then a single peg will denote a win, so 'pegsWithDiscs.length' should be '1'...right? I just have to make sure that the peg with discs isn't the first peg...or they must've cheated! This won't work for any initial condition, but only that where the discs all start on the first peg. For now, I'm going to bed...
-  const checkWin = board.reduce((pegHasDiscs, currentPeg) => {
-
-  }, [])
-
-  console.log(checkWin);
+  // const checkWin = board.filter(peg => peg.length)
+  const checkWin = board.reduce((total, peg) => {
+    if(peg.length) {
+      total.push(peg);
+    }
+    return total;
+  }, []);
+  
+  !(checkWin.length > 1) === true ? console.log(`Congratulations! You won!`) : console.log(`Next move please.`);
+  
+   
 }
 
 //*************************************************/
 
 renderBoard();
 
-// moveDisc(1, 2);
+moveDisc(1, 2);
 // moveDisc(1, 3);
 // moveDisc(2, 3);
 // moveDisc(1, 2);
