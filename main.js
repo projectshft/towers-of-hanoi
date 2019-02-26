584ea27d5974dbf8723008d8a154e85b69e7409b
Luc Saunders <lucsaunders@gmail.com>
2019-02-26 07:45:59 -0500

    Update html file. Add CSS. Strip down main.js and revert to earlier basic functionality. Remove problematic legalMoves, checkWinner, and endGame/restartGame.

--- main.js
+++ main.js
@@ -1,197 +1,96 @@
-//create a global variable for depicting the bottom portion of the board
-//doesn't have to be global; move to renderBoard ???
+// TOWERS OF HANOI
+// Stripped down to basic function declarations (still using some aray helper methods) to startGame (with flexible, inputted numberOfDics), renderBoard, establish the winningSum, and allow player to moveDisc. Maintains the current state of the board via 2D array and current pegOfOrigin, destinationPeg, and unusedPeg. 
 
-//Creat a Game "module"
+// REMOVED legalMoves
+// REMOVED isPegEmpty and topDiscOfPeg, which feed into legalMoves
+// REMOVED: checkWinner, endGame, and restartGame
+// ALL are necessary for a fully operational game and will be reconceptualized and completed later
 
-var Game = function() { 
+// CURRENTLY DOES NOT *formally* prevent illegal moves and notify the player, although the program initiates errors, breaks, and won't proceed if the player tries to moveDisc with an illegal move.
+
+// DOES NOT check for winner and yield a winning message with numberOfMoves, and then resetGame. As long as they play by the rules, a player could theoretically play one game for ~585 billion years... and beyond.  
+
+//********************************************
+
+// create a variable for depicting the bottom portion of the board
 const PEG_BASE = "--- "; 
-var board = []; // variable for "flexible" board (flexible number of discs to play with)
-var stateOfTheBoard = {};
-var moveCounter = 0;
-  //Create a function to initialize board and establish beginning values.
-  function startGame(startingNumberOfDiscs) { //Flexible number of discs, hard-coded for three pegs; add peg parame ter later to make dynamic
-    // const PEG_BASE = "--- "; 
-    // var board = []; // variable for "flexible" board (flexible number of discs to play with)
-    // var stateOfTheBoard = {};
-    // var moveCounter = 0;
-    //Hardcoding 3 pegs; Create a function for populating the board with given number of pegs -- using a loop, likely a forEach -- to allow dynamic number of pegs
-    //These three peg variables are comparable to what's inside the board/stateOfTheBoard 
-    var firstPeg = [];
-    var secondPeg = [];
-    var thirdPeg = [];
 
-    //populate the firstPeg array with the starting number of discs; all on firstPeg for starting position at beginning of the game
-    for (let i = startingNumberOfDiscs; i > 0; i--){        
-        firstPeg.push(i);
-      };
+// create empty board array to be populated with 3 peg arrays and the startingNumberOfDiscs. 
+var board = []; 
 
-    //populate the board with three pegs in starting position (hard-coded)
-    board.push(firstPeg, secondPeg, thirdPeg);
+// create a function to initialize board and establish beginning values.
+function startGame(startingNumberOfDiscs) { //flexible number of discs, hard-coded for three pegs
 
-    //establish the winningTotal value to be checked by checkWinner
-    //var currentPegToSum = firstPeg; 
-    var winningTotalSum = firstPeg.reduce(function(discAccumulator, startingValue) {
-      console.log(discAccumulator);
-      return accumulator + startingValue; //startingValue = 0, so is it necessary here???
-    }, 0);
-    //call this in checkWinner? 
+  // three peg variables are comparable to what's inside the board/stateOfTheBoard 
+  var firstPeg = [];
+  var secondPeg = [];
+  var thirdPeg = [];
 
-    //graphical depiction to console of the board: 3 pegs, all discs on first peg, in starting position
-    //REPETITIVE FUNCTION, THIS SHOULD JUST CALL renderBoard (I think). But this allows an introductory "first move" message to be delivered, distinct from the "next move" to be used through the rest of the game.
-   
-    //renderBoard(board);
-
-    //Create a message variable that can be selected via moveCount (if moveCount === 0, console.log first message.) ???
-   
-    board.forEach(function (currentInstanceOfPegArray) {
-        console.log(PEG_BASE + currentInstanceOfPegArray.join("  "))
-      });
-
-      // console.log("To play this game, please enter moveDisc(disc, destinationPeg), where disc is the number of disc you want to move, and destinationPeg is the peg to which you wish to move it.";
-      console.log("First move, please."); //create variable for message string that updates to "new move" or 
-
-
-      // //create an object for the state of the board; (review inheritence, prototypes, this)
-      // // key: value pairs are a way to keep track of which peg discs are on; perhaps cleaner than keeping track of which peg discs are on as array ???
-      // BUT IS THIS NECESSARY FOR THIS DESIGN? 
-      stateOfTheBoard.peg1 = board[0]; // or pegOfOrigin, destinationPeg, unusedPeg???
-      stateOfTheBoard.peg2 = board[1]; 
-      stateOfTheBoard.peg3 = board[2];
-      // stateOfTheBoard.winnngTotal = (sumDiscs(startingNumberOfDiscs); //(board[0].length));
-
-      //console testing
-      console.log(stateOfTheBoard);
-      
-      return board; // is a return necessary here? return to stateOfTheBoardObj?
-      //return stateOfTheBoardObj // necessary? consider where to place, re: scope
-  };
-//placement of stateOfTheBoard object?
-//include also in the object key:value pairs for numberOfMovesMade (a number) and gameStatus (a boolean, "gameOver")
-    //finalNumberOfMoves: function(numberofMovesMade) {
-    //   console.log("You won in " + numberOfMovesMade.toString)
-    // }
-    // };
-// let stateOfTheBoard = {    
-// };
-// peg1: board[0], // or pegOfOrigin, destinationPeg, unusedPeg???
-// peg2: board[1], 
-// peg3: board[2]
-
-// console.log(stateOfTheBoard);
-
-  var renderBoard = function(updatedBoard, pegToCheckForWinner){ //use map?
-    updatedBoard.forEach(function (currentInstanceOfPegArray) {
-      console.log(PEG_BASE + currentInstanceOfPegArray.join("  "))
-    });
-    moveCounter++; // need to fix this // moveCount function or inside stateOfBoard object
-    if (checkWinner(pegToCheckForWinner)) {
-      console.log("You win!")
-      //console.log("You won in " + moveCounter + " moves.") // moveCounter value to string ???
-    }
-    else console.log("Next move, please.");
+  // populate the firstPeg array with the starting number of discs; all on firstPeg for starting position at beginning of the game
+  for (let i = startingNumberOfDiscs; i > 0; i--){ 
+    firstPeg.push(i);
   };
 
-// a function that, given a certain peg, determines which other pegs the top disc from that peg can be moved to.
-//filter OUT the pegs that are NOT available; filter should create subset of avaialable pegs.
-//In order to complete this function, you MUST use a filter function at least once.
+  // populate the board with three pegs in starting position (hard-coded)
+  board.push(firstPeg, secondPeg, thirdPeg);
   
-function legalMove(discToBeMoved) { //return boolean
-  var legalMovesArr = []; //push legal moves here
-
-  //a function to find and return empty pegs;
-  var emptyPegs = function() { //what parameter(s) to take here ??? 
-    var arrayOfEmptyPegs = board.filter(function (potentiallyEmptyPeg) {
-      return potentiallyEmptyPeg.length === 0; //need to reference index (indexOf()?)
+  // establish the winningTotal value to be checked by checkWinner function, as yet unwritten, which will also utilize reduce
+  var winningTotalSum = firstPeg.reduce(function(discAccumulator, startingValue) {
+  return discAccumulator + startingValue; //startingValue = 0, so is it necessary here???
+      }, 0);
+  
+  // *initial* graphical depiction to console of the board: 3 pegs, all discs on first peg, in starting position. 
+  // this could be removed and renderBoard invoked, but this design allows an introductory "first move" prompt to be consoled, distinct from the "next move" that will prompt the player throughout the rest of the game.
+  board.forEach(function (currentInstanceOfPegArray) {
+    console.log(PEG_BASE + currentInstanceOfPegArray.join("  "))
     });
-  };
 
-  //return the topmost disc on a peg
-  var topDisc = function () {
-    board.forEach(function (instanceOfPegArray) {
-      instanceOfPegArray.length !== 0;
-       // last element in each peg array
-    });
-    return instanceOfPegArray[(instanceOfPegArray.length - 1)];
-  };
+  // request first move from player  
+  console.log("First move, please.");
 }
 
-//   //if (emptyPeg.length === 0)
-//   //if (pegIsEmpty || topDisc > discToBeMoved) {
-//     return true; 
-//   }
-//   //var legalMoves = board.filter(function (individualPegState) {
-//   else return false;
-// }
-
-  // a function to moveDisc
-  function moveDisc(selectedDisc, destinationPeg){  
-    // check if move is legal
-    //invoke legalMove (to return boolean)
-    // if (!legalMove) {
-    //   console.log("That's not a legal move. Try again.");
-    //   //break?
-    // }
-  
-
-    //create variable for pegOfOrigin
-    // pegOfOrigin's value is inherent in selected disc value (there is only ever one disc/number on the entire board, regardless of which peg it's on)
-    //use filter here; related to legalMove
-    //var unusedPeg = 
-    var pegOfOrigin = board.find(function(onePeg) { //assignment calls for filter???
-      return onePeg[onePeg.length - 1] === selectedDisc;
-      // if ((onePeg[onePeg.length - 1]) === selectedDisc){
-      //   return true;
-      // };
+// a function to graphically render the board in the console (use map???
+//pegToCheckForWinner us taken as parameter for later, as yet unimplemented design
+function renderBoard(updatedBoard, pegToCheckForWinner){ //use map?
+  updatedBoard.forEach(function (currentInstanceOfPegArray) {
+    console.log(PEG_BASE + currentInstanceOfPegArray.join("  "))
     });
+  // if !endOfGame, request next move from player
+  console.log("Next move, please.");
+}
 
-    //Create a variable for the peg number (1, 2, or 3) of the origin peg
-    // The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
-    var numberOfPegOfOrigin = (board.indexOf(pegOfOrigin) + 1); //add one to value because peg numbers start at one, not zero
+// a function to move disc
+function moveDisc(selectedDisc, destinationPeg){ 
     
-    // CONSOLE TESTING 
-    console.log("pegOfOrigin[0]")
-    console.log(pegOfOrigin[0]);
+  // create variable for arrayOfpegOfOrigin
+  // arrayOfPegOfOrigin's value is inherent in selected disc value (there is only ever one disc/number on the entire board, regardless of which peg it's on)
+  var arrayOfPegOfOrigin = board.find(function(onePeg) {
+    return ((onePeg[onePeg.length - 1]) === selectedDisc);
+  });
+ 
+  // create variable for the number of the peg of origin
+  var numberOfPegOfOrigin = (board.indexOf(arrayOfPegOfOrigin) + 1); //add one to value because peg numbers start at one, not zero
+ 
+  // create variable array of peg numbers to be used to find the number of the unused peg
+  var possiblePegs = [1, 2, 3];
+ 
+  // create variable for numberofUnusedPeg and find value (potentially useful in later design) 
+  var numberOfUnusedPeg = possiblePegs.find(function(pegNumberToLookFor) { 
+    return ((pegNumberToLookFor != numberOfPegOfOrigin) && (pegNumberToLookFor != destinationPeg));
+  }); 
 
-    console.log("numberOfPegOfOrigin");
-    console.log(numberOfPegOfOrigin);
+  // pop disc from pegOfOrigin array and push it onto the destinationPeg
+  board[destinationPeg - 1].push(arrayOfPegOfOrigin.pop());
+   
+  // call renderBoard to update graphical representation of board
+  renderBoard(board, destinationPeg);
+}
+//______________________________________
 
-    //pop disc from pegOfOrigin array and push it onto the destinationPeg
-    //use MAP here?
-    board[destinationPeg - 1 ].push(pegOfOrigin.pop());
-    
-    console.log("board"); 
-    console.log(board);
-  
-    //MAP // or map in renderBoard function
-
-    // update of stateOfBoard, for moveDisc, to create board to be passed to renderBoard
-    //Add moveCounter, isPegEmpty, legalMoves
-
-
-    renderBoard(board, destinationPeg);
-  };
-
-  var checkWinner = function(board) { 
-    //if // pegsToCheck ~= board
-    // .reduce(function (potentialWinningPeg) {
-      // reduce() on an empty array without an initial value is an error.
-      var winningTotalSum = currentPegToSum.reduce(function(discAccumulator, startingValue) {
-        console.log(discAccumulator);
-        return accumulator + startingValue; //startingValue = 0, so is it necessary here???
-      }, 0);
-      // moveCounter here?
-
-  };
-  
-//return moveDisc function so it's "publicly" available to the player;
-//moveDisc should be the only function available to the player. 
-// return {
-//   moveDisc: moveDisc
-// };
-//could offer "quit" "reset" "goBack" moves to the player for added dynamism
-//}
-
-//______________________________________________________________________________
-
-
+// Executable
 startGame(5);
+// board will be rendered and prompt user for first move
+
+
+
+   
