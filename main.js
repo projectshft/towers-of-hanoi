3b4efacca8c16d5c76b5f105853bc2947078d1dc
dposse <dposse.125@gmail.com>
2019-06-07 11:23:47 -0400

    3x3 Board prints correctly

--- main.js
+++ main.js
@@ -1,8 +1,45 @@
 /**
- * 
- * Tower of Hanoi evaluation from Project Shift
- * Author: Daniel Posse
- * 
- */
+* 
+* Tower of Hanoi evaluation from Project Shift
+* Author: Daniel Posse
+* 
+*/
 
- 
+//later impletement user passing number of discs and pegs, check if it is legal
+//1st draft - don't use discs and pegs, create board manually to 3x3
+let Board = () => {
+
+  let board = [
+    ['3','2','1'],
+    [],
+    []
+  ];
+
+  let printBoard = () => {
+
+    //iterate through pegs, map each peg array to string
+    board.forEach( peg => {
+
+      //better with reduce since creating a return string?
+
+      let pegString = '---';
+
+      peg.map( disc => {
+
+        pegString += ` ${disc} `;
+
+      });
+
+      console.log(`${pegString} \n`);
+
+    });
+
+  };
+
+  return {
+
+    printBoard: printBoard
+
+  };
+
+} //end board
