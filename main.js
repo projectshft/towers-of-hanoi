039642c39431114379d730a0a0994b6ad5db4098
unknown <nathaniel.poli@gmail.com>
2019-06-07 15:05:13 -0400

    Move function implemented

--- main.js
+++ main.js
@@ -4,12 +4,29 @@ var board = [
   []
 ];
 
-var displayBoard = board.map(function (peg) {
+var objBoard = {board};
+
+//Displays the current state of the board
+var displayBoard = objBoard.board.map(function (peg) {
   var boardString = "--- "
   peg.map(function (disc) {
     return (boardString += disc + " ")
   })
   return boardString;
 })
+
+//Moves discs on the board
+var moveDisc = function(source, target) {
+  var moveValue = objBoard.board[source-1].pop();
+  objBoard.board[target-1].push(moveValue);
+  displayBoard = objBoard.board.map(function (peg) {
+    var boardString = "--- "
+    peg.map(function (disc) {
+      return (boardString += disc + " ")
+    })
+    return boardString;
+  })
+  console.log(displayBoard.toString().replace(/,/g, '\n'));
+  }
 
 console.log(displayBoard.toString().replace(/,/g, '\n'));
