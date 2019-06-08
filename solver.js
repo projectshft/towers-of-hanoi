0f41e65b9232f91cd8e160bb3cd4877b35345762
dposse <dposse.125@gmail.com>
2019-06-08 18:24:17 -0400

    solveGame started

---
+++ solver.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Tower of Hanoi evaluation from Project Shift
+ * Author: Daniel Posse
+ * 
+ */
+
+/**
+ * Use expensive recursive algorithm for now
+ * 
+ * base case: 1 disc on stack => move to destination
+ *  recusrive case: 
+ *    move n-1 discs from stack to aux peg
+ *    move 1 disc from stack to destination (base case)
+ *    move n-1 discs from aux peg to destination
+ */
+
+let solveGame = () => {
+
+  
+
+};
