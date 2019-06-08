cf15c6260bd259667346b8ff17b5f6e6c2267e27
K. Bender <kristinajbender@gmail.com>
2019-06-08 13:46:42 -0400

    update with second draft

--- main.js
+++ main.js
@@ -1,107 +1,136 @@
+//
 var numberOfPegs = 3;
 var numberOfDiscs = 3;
+var game;
+var board;
 
-var makeBoard = function(pegNumber, discNumber) {
+game = Game();
+board = Board(numberOfPegs, numberOfDiscs);
+
+game.initiateGame(board);
+
+game.printBoard();
+game.moveDisc(1, 2);
+game.moveDisc(1, 3);
+game.moveDisc(2, 3);
+game.moveDisc(1, 2);
+game.moveDisc(3, 1);
+game.moveDisc(3, 2);
+game.moveDisc(1, 2);
+game.checkWinner();
+//
+
+var Game = function() {
   var boardArray = [];
-  for (var i = 0; i < pegNumber; i++) {
-    if (i === 0) {
-      var firstPeg = [];
-      for (var j = 0; j < discNumber; j++) {
-        firstPeg.push((discNumber - j) + '');
+  var pegNumber = board.getAttribute('pegNumber');
+  var discNumber = board.getAttribute('discNumber');
+
+  var initiateGame = function(board) {
+
+    for (var i = 0; i < pegNumber; i++) {
+      if (i === 0) {
+        var firstPeg = [];
+        for (var j = 0; j < discNumber; j++) {
+          firstPeg.push((discNumber - j) + '');
+        }
+        boardArray.push(firstPeg);
+      } else {
+        var otherPegs = [];
+        boardArray.push(otherPegs);
       }
-      boardArray.push(firstPeg);
-    } else {
-      var otherPegs = [];
-      boardArray.push(otherPegs);
     }
-  }
-  return boardArray;
-};
+  };
 
-var board = makeBoard(numberOfPegs, numberOfDiscs);
-
-var printBoard = function(boardArray) {
-  return boardArray.forEach(function(peg) {
-    var discString = '--- ';
-    peg.forEach(function(disc) {
-      discString += disc + ' ';
+  var printBoard = function() {
+    boardArray.forEach(function(peg) {
+      var discString = '--- ';
+      peg.forEach(function(disc) {
+        discString += disc + ' ';
+      });
+      console.log(discString);
     });
-    console.log(discString);
-  });
-};
+  };
 
-printBoard(board);
-
-var getSizeOfTopDiscOnPeg = function(peg) {
-  if (peg.length === 0) {
-    return numberOfDiscs + 1;
-  } else {
-    return Number(peg[peg.length - 1]);
-  }
-};
-
-// specific to "board" array (generalize?)
-var moveDisc = function(sourcePeg, targetPeg) {
-  var sourcePegTopDiscSize = getSizeOfTopDiscOnPeg(board[sourcePeg - 1]);
-  var targetPegTopDiscSize = getSizeOfTopDiscOnPeg(board[targetPeg - 1]);
-  if (sourcePegTopDiscSize < targetPegTopDiscSize) {
-    board[targetPeg - 1].push(board[sourcePeg - 1].pop());
-    return "The move was successful.";
-  } else {
-    return "The move was not successful.";
-  }
-};
-
-moveDisc(1, 2);
-printBoard(board);
-
-var verifyFullPegOrder = function(fullPeg) {
-  result = false;
-  for (var i = 0; i < fullPeg.length - 1; i++) {
-    if (Number(fullPeg[i]) > Number(fullPeg[i + 1])) {
-      result = true;
-    } else {
-      result = false;
-    }
-  }
-  return result;
-};
-
-var checkWinner = function(boardArray) {
-  var indexOfFullPeg = 0;
-  var winStatus = false;
-
-  var checkFullPeg = boardArray.reduce(function(accumulator, peg) {
+  var getSizeOfTopDiscOnPeg = function(peg) {
     if (peg.length === 0) {
-      accumulator++;
+      return discNumber + 1;
     } else {
-      indexOfFullPeg += accumulator;
+      return Number(peg[peg.length - 1]);
     }
-    return accumulator;
-  }, 0);
+  };
 
-  var fullPegDiscOrderCheck = verifyFullPegOrder(boardArray[indexOfFullPeg]);
+  var moveDisc = function(sourcePeg, targetPeg) {
+    var sourcePegTopDiscSize = getSizeOfTopDiscOnPeg(boardArray[sourcePeg - 1]);
+    var targetPegTopDiscSize = getSizeOfTopDiscOnPeg(boardArray[targetPeg - 1]);
+    if (sourcePegTopDiscSize < targetPegTopDiscSize) {
+      boardArray[targetPeg - 1].push(boardArray[sourcePeg - 1].pop());
+      return "The move was successful.";
+    } else {
+      return "The move was not successful.";
+    }
+  };
 
-  if ((checkFullPeg === boardArray.length - 1) && (indexOfFullPeg !== 0) && (fullPegDiscOrderCheck)) {
-    winStatus = true;
-  }
-  return winStatus;
+  var verifyFullPegOrder = function(fullPeg) {
+    result = false;
+    for (var i = 0; i < fullPeg.length - 1; i++) {
+      if (Number(fullPeg[i]) > Number(fullPeg[i + 1])) {
+        result = true;
+      } else {
+        result = false;
+      }
+    }
+    return result;
+  };
+
+  var checkWinner = function() {
+    if (board.getAttribute('winStatus')) {
+      return "The game has been won."
+    } else {
+      var indexOfFullPeg = 0;
+      var checkFullPeg = boardArray.reduce(function(accumulator, peg) {
+        if (peg.length === 0) {
+          accumulator++;
+        } else {
+          indexOfFullPeg += accumulator;
+        }
+        return accumulator;
+      }, 0);
+
+      var fullPegDiscOrderCheck = verifyFullPegOrder(boardArray[indexOfFullPeg]);
+
+      if ((checkFullPeg === boardArray.length - 1) && (indexOfFullPeg !== 0) && (fullPegDiscOrderCheck)) {
+        board.setAttribute('winStatus', true);
+      }
+      return (board.getAttribute('winStatus')) ? "The game has been won." : "The game has not been won.";
+    }
+  };
+
+  return {
+    initiateGame: initiateGame,
+    printBoard: printBoard,
+    moveDisc: moveDisc,
+    checkWinner: checkWinner
+  };
 };
 
-checkWinner(board);
-
-moveDisc(1, 3);
-printBoard(board);
-moveDisc(2, 3);
-printBoard(board);
-moveDisc(1, 2);
-printBoard(board);
-moveDisc(3, 1);
-printBoard(board);
-moveDisc(3, 2);
-printBoard(board);
-moveDisc(1, 2);
-printBoard(board);
-
-checkWinner(board);
-printBoard(board);
+var Board = function(pegNumber, discNumber) {
+  var attributes = {
+    pegNumber: pegNumber,
+    discNumber: discNumber,
+    winStatus: false
+  };
+  var getAttribute = function(attribute) {
+    if (attributes.hasOwnProperty(attribute)) {
+      return attributes[attribute];
+    }
+  };
+  var setAttribute = function(attribute, value) {
+    if (attributes.hasOwnProperty(attribute)) {
+      attributes[attribute] = value;
+    }
+  };
+  return {
+    getAttribute: getAttribute,
+    setAttribute: setAttribute
+  };
+};
