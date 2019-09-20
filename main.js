0d73aa909cdd07c05275ab53a87428cb0eaf0e4d
Tyler <laorik1@live.com>
2019-09-20 13:46:18 -0400

    added tests for checking winner. implemented checking winner

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var HanoiGame = (pegs, discs) => {
     var attributes = {
         pegs: pegs,
-        discs: discs
+        discs: discs,
+        moves: 0,
+        //gauss
+        solution: ((discs * (discs + 1)) / 2)
     };
 
     var boardArr;
@@ -18,6 +21,11 @@ var HanoiGame = (pegs, discs) => {
         }
     }
 
+    var initGame = () => {
+        attributes.moves = 0;
+        buildBoard(attributes.pegs, attributes.discs);
+    }
+
     var boardView = () => {
         return boardArr.map((peg) => { return '--- ' + peg.join(" "); })
             .reduce((disp, pegStr) => { return disp += pegStr.trimRight() + '\n'; }, "")
@@ -28,29 +36,35 @@ var HanoiGame = (pegs, discs) => {
         //check for valid index on from and to peg
         var indexFromPeg = fromPeg - 1;
         var indexToPeg = toPeg - 1;
-        if(indexFromPeg !== indexToPeg 
+        if (indexFromPeg !== indexToPeg
             && indexFromPeg >= 0 && indexFromPeg < attributes.pegs
-            && indexToPeg >= 0 && indexToPeg < attributes.pegs)
-        {
+            && indexToPeg >= 0 && indexToPeg < attributes.pegs) {
             // if there is even a disc to move on the from peg
-            if(boardArr[indexFromPeg].length > 0){
-                if(boardArr[indexToPeg].length == 0 
-                    || boardArr[indexToPeg][boardArr[indexToPeg].length - 1] > boardArr[indexFromPeg][boardArr[indexFromPeg].length - 1])
-                    {
-                        boardArr[indexToPeg].push(boardArr[indexFromPeg].pop())
-                    }
+            if (boardArr[indexFromPeg].length > 0) {
+                if (boardArr[indexToPeg].length == 0
+                    || boardArr[indexToPeg][boardArr[indexToPeg].length - 1] > boardArr[indexFromPeg][boardArr[indexFromPeg].length - 1]) {
+                    boardArr[indexToPeg].push(boardArr[indexFromPeg].pop())
+                }
             }
         }
+    }
+
+    //check if only 1 peg is populated (not the first) and if the sum of it's discs is equal to solution
+    var checkWinner = () => {
+        var populatedPegs = boardArr.filter((peg) => { return peg.length > 0; });
+        return boardArr[0].length == 0 && populatedPegs.length == 1
+            && populatedPegs[0].reduce((sum, number) => { return sum + number; }, 0) == attributes.solution;
     }
 
     if (attributes.pegs < 3 || attributes.discs < 1) {
         return null;
     }
     else {
-        buildBoard(attributes.pegs, attributes.discs);
+        initGame();
     }
 
     return {
+        checkWinner: checkWinner,
         boardView: boardView,
         moveDisc: moveDisc,
     };
@@ -59,9 +73,9 @@ var HanoiGame = (pegs, discs) => {
 var test = HanoiGame(3, 1);
 console.log(test.boardView());
 console.log('---------');
-hanoiGame = HanoiGame(5,5);
-hanoiGame.moveDisc(1,2);
-hanoiGame.moveDisc(1,3);
-hanoiGame.moveDisc(1,4);
-hanoiGame.moveDisc(2,4);
+hanoiGame = HanoiGame(5, 5);
+hanoiGame.moveDisc(1, 2);
+hanoiGame.moveDisc(1, 3);
+hanoiGame.moveDisc(1, 4);
+hanoiGame.moveDisc(2, 4);
 console.log(hanoiGame.boardView());
--- spec/excerciseSpec.js
+++ spec/excerciseSpec.js
@@ -6,97 +6,137 @@ beforeEach(function () {
 });
 
 // sets books back to 'checked in' after each spec
-afterEach(function() {
+afterEach(function () {
   //don't need any after eaches
 });
 
 describe("checks proper game setup", function () {
   it("pegs must be at least 3 and discs must be at least 1", function () {
-    hanoiGame = HanoiGame(0,0);
+    hanoiGame = HanoiGame(0, 0);
     expect(hanoiGame).toEqual(null);
-    hanoiGame = HanoiGame(0,1);
+    hanoiGame = HanoiGame(0, 1);
     expect(hanoiGame).toEqual(null);
-    hanoiGame = HanoiGame(0,2);
+    hanoiGame = HanoiGame(0, 2);
     expect(hanoiGame).toEqual(null);
-    hanoiGame = HanoiGame(0,3);
+    hanoiGame = HanoiGame(0, 3);
     expect(hanoiGame).toEqual(null);
-    hanoiGame = HanoiGame(1,0);
+    hanoiGame = HanoiGame(1, 0);
     expect(hanoiGame).toEqual(null);
-    hanoiGame = HanoiGame(1,1);
+    hanoiGame = HanoiGame(1, 1);
     expect(hanoiGame).toEqual(null);
-    hanoiGame = HanoiGame(1,2);
+    hanoiGame = HanoiGame(1, 2);
     expect(hanoiGame).toEqual(null);
-    hanoiGame = HanoiGame(1,3);
+    hanoiGame = HanoiGame(1, 3);
     expect(hanoiGame).toEqual(null);
-    hanoiGame = HanoiGame(2,0);
+    hanoiGame = HanoiGame(2, 0);
     expect(hanoiGame).toEqual(null);
-    hanoiGame = HanoiGame(2,1);
+    hanoiGame = HanoiGame(2, 1);
     expect(hanoiGame).toEqual(null);
-    hanoiGame = HanoiGame(2,2);
+    hanoiGame = HanoiGame(2, 2);
     expect(hanoiGame).toEqual(null);
-    hanoiGame = HanoiGame(2,3);
+    hanoiGame = HanoiGame(2, 3);
     expect(hanoiGame).toEqual(null);
-    hanoiGame = HanoiGame(3,0);
+    hanoiGame = HanoiGame(3, 0);
     expect(hanoiGame).toEqual(null);
-    hanoiGame = HanoiGame(3,1);
+    hanoiGame = HanoiGame(3, 1);
     expect(typeof hanoiGame).toEqual('object');
-    hanoiGame = HanoiGame(3,2);
+    hanoiGame = HanoiGame(3, 2);
     expect(typeof hanoiGame).toEqual('object');
-    hanoiGame = HanoiGame(3,3);
+    hanoiGame = HanoiGame(3, 3);
     expect(typeof hanoiGame).toEqual('object');
   });
-  
-    it("prints board. should be all discs on first peg", function () {
-      hanoiGame = HanoiGame(3,1);
-      expect(hanoiGame.boardView()).toEqual("--- 1\n---\n---");
-      hanoiGame = HanoiGame(3,3);
-      expect(hanoiGame.boardView()).toEqual("--- 3 2 1\n---\n---");
-      hanoiGame = HanoiGame(3,5);
-      expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---");
-      hanoiGame = HanoiGame(5,5);
-      expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---\n---\n---");
-    });
+
+  it("prints board. should be all discs on first peg", function () {
+    hanoiGame = HanoiGame(3, 1);
+    expect(hanoiGame.boardView()).toEqual("--- 1\n---\n---");
+    hanoiGame = HanoiGame(3, 3);
+    expect(hanoiGame.boardView()).toEqual("--- 3 2 1\n---\n---");
+    hanoiGame = HanoiGame(3, 5);
+    expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---");
+    hanoiGame = HanoiGame(5, 5);
+    expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---\n---\n---");
+  });
 });
 
 describe("checks proper game movement", function () {
-  it("moves pegs in different scenarios", function () {
-      hanoiGame = HanoiGame(5,5);
-      expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---\n---\n---");
-      //move invalid peg numbers (both non-existing and same to same)
-      hanoiGame.moveDisc(0,0);
-      expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---\n---\n---");
-      hanoiGame.moveDisc(0,1);
-      expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---\n---\n---");
-      hanoiGame.moveDisc(1,0);
-      expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---\n---\n---");
-      hanoiGame.moveDisc(1,10);
-      expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---\n---\n---");
-      hanoiGame.moveDisc(1,1);
-      expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---\n---\n---");
-      //check valid moves
-      hanoiGame = HanoiGame(5,5);
-      hanoiGame.moveDisc(1,2);
-      expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2\n--- 1\n---\n---\n---");
-      hanoiGame = HanoiGame(5,5);
-      hanoiGame.moveDisc(1,3);
-      expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2\n---\n--- 1\n---\n---");
-      hanoiGame = HanoiGame(5,5);
-      hanoiGame.moveDisc(1,2);
-      hanoiGame.moveDisc(1,3);
-      hanoiGame.moveDisc(1,4);
-      expect(hanoiGame.boardView()).toEqual("--- 5 4\n--- 1\n--- 2\n--- 3\n---");
-      hanoiGame = HanoiGame(5,5);
-      hanoiGame.moveDisc(1,2);
-      hanoiGame.moveDisc(1,3);
-      hanoiGame.moveDisc(1,4);
-      hanoiGame.moveDisc(2,4);
-      expect(hanoiGame.boardView()).toEqual("--- 5 4\n---\n--- 2\n--- 3 1\n---");
-      //check invalid moves due to disc size
-      hanoiGame = HanoiGame(5,5);
-      hanoiGame.moveDisc(1,2);
-      hanoiGame.moveDisc(1,3);
-      hanoiGame.moveDisc(1,4);
-      hanoiGame.moveDisc(4,2);
-      expect(hanoiGame.boardView()).toEqual("--- 5 4\n--- 1\n--- 2\n--- 3\n---");
-    });
+  it("moves pegs in mismatching board scenarios", function () {
+    hanoiGame = HanoiGame(5, 5);
+    expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---\n---\n---");
+    //move invalid peg numbers (both non-existing and same to same)
+    hanoiGame.moveDisc(0, 0);
+    expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---\n---\n---");
+    hanoiGame.moveDisc(0, 1);
+    expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---\n---\n---");
+    hanoiGame.moveDisc(1, 0);
+    expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---\n---\n---");
+    hanoiGame.moveDisc(1, 10);
+    expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---\n---\n---");
+    hanoiGame.moveDisc(1, 1);
+    expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2 1\n---\n---\n---\n---");
+  });
+  it("moves pegs in valid scenarios", function () {
+    //check valid moves
+    hanoiGame = HanoiGame(5, 5);
+    hanoiGame.moveDisc(1, 2);
+    expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2\n--- 1\n---\n---\n---");
+    hanoiGame = HanoiGame(5, 5);
+    hanoiGame.moveDisc(1, 3);
+    expect(hanoiGame.boardView()).toEqual("--- 5 4 3 2\n---\n--- 1\n---\n---");
+    hanoiGame = HanoiGame(5, 5);
+    hanoiGame.moveDisc(1, 2);
+    hanoiGame.moveDisc(1, 3);
+    hanoiGame.moveDisc(1, 4);
+    expect(hanoiGame.boardView()).toEqual("--- 5 4\n--- 1\n--- 2\n--- 3\n---");
+    hanoiGame = HanoiGame(5, 5);
+    hanoiGame.moveDisc(1, 2);
+    hanoiGame.moveDisc(1, 3);
+    hanoiGame.moveDisc(1, 4);
+    hanoiGame.moveDisc(2, 4);
+    expect(hanoiGame.boardView()).toEqual("--- 5 4\n---\n--- 2\n--- 3 1\n---");
+  });
+  it("attempts to move larger pegs onto smaller pegs", function () {
+    //check invalid moves due to disc size
+    hanoiGame = HanoiGame(5, 5);
+    hanoiGame.moveDisc(1, 2);
+    hanoiGame.moveDisc(1, 3);
+    hanoiGame.moveDisc(1, 4);
+    hanoiGame.moveDisc(4, 2);
+    expect(hanoiGame.boardView()).toEqual("--- 5 4\n--- 1\n--- 2\n--- 3\n---");
+  });
+});
+
+describe("play the game to check for winner", function () {
+  it("moves pegs in solution order checking winner after each", function () {
+    hanoiGame = HanoiGame(3, 3);
+    hanoiGame.moveDisc(1, 2);
+    expect(hanoiGame.checkWinner()).toEqual(false);
+    hanoiGame.moveDisc(1, 3);
+    expect(hanoiGame.checkWinner()).toEqual(false);
+    hanoiGame.moveDisc(2, 3);
+    expect(hanoiGame.checkWinner()).toEqual(false);
+    hanoiGame.moveDisc(1, 2);
+    expect(hanoiGame.checkWinner()).toEqual(false);
+    hanoiGame.moveDisc(3, 1);
+    expect(hanoiGame.checkWinner()).toEqual(false);
+    hanoiGame.moveDisc(3, 2);
+    expect(hanoiGame.checkWinner()).toEqual(false);
+    hanoiGame.moveDisc(1, 2);
+    expect(hanoiGame.checkWinner()).toEqual(true);
+    //move it back
+    hanoiGame.moveDisc(2, 1);
+    expect(hanoiGame.checkWinner()).toEqual(false);
+    hanoiGame.moveDisc(2, 3);
+    expect(hanoiGame.checkWinner()).toEqual(false);
+    hanoiGame.moveDisc(1, 3);
+    expect(hanoiGame.checkWinner()).toEqual(false);
+    hanoiGame.moveDisc(2, 1);
+    expect(hanoiGame.checkWinner()).toEqual(false);
+    hanoiGame.moveDisc(3, 2);
+    expect(hanoiGame.checkWinner()).toEqual(false);
+    hanoiGame.moveDisc(3, 1);
+    expect(hanoiGame.checkWinner()).toEqual(false);
+    //still false because first peg
+    hanoiGame.moveDisc(2, 1);
+    expect(hanoiGame.checkWinner()).toEqual(false);
+  });
 });
