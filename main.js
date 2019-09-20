b8d105baff133c0e077300f04ec273beaca85e0a
George Green <greezy@Georges-MacBook-Air.local>
2019-09-20 11:09:40 -0400

    here's my first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let board = ["--"];
+//format to add pegs to the board
+
+let myBoard = board.map(function(num){
+  return num += board;
+})
+
+
+
+let tyler = [
+  ["5", "4", "3", "2", "1"],
+  [],
+  []
+];
+
+
+console.log(myBoard);
+console.log(tyler);
