f046be6bb86bf47444029a3f307fbc983b55f1c5
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-09-20 14:42:26 -0400

    Add move function

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
-// Initialize board representing 3 pegs and 3 discs in starting position
-var board = [[3,2,1],[],[]]
+var BoardModule = function() {
+  // Initialize board representing 3 pegs and 3 discs in starting position
+  var board = [[3, 2, 1], [], []]
 
-// Print board status horizontally
-var boardPrint = function() {board.map(function (peg) {
-    let boardString = "--- "
-    peg.forEach(function(disc) {
-      boardString += (" " + disc)
+  // Print board status horizontally
+  var boardPrint = function () {
+    board.map(function (peg) {
+      let boardString = "--- "
+      peg.forEach(function (disc) {
+        boardString += (" " + disc)
+      })
+      console.log(boardString)
     })
-  console.log(boardString)
-})
+  }
+
+  // Move disc from start peg to end peg
+  function move(startPos, endPos) {
+    var currentPegArr = board[startPos - 1]
+    var targetPegArr = board[endPos - 1]
+    var grabDisc = currentPegArr.pop();
+    targetPegArr.push(grabDisc)
+  }
+
+  // Checks topmost disc of peg for potential moves
+  function possibleMoves(peg) {
+    const disc = peg[peg.length-1]
+    
+  }
+
+  return {
+    move: move,
+    boardPrint: boardPrint
+  }
 }
 
-boardPrint();
-
-// Move disc from start peg to end peg
-function move(startPos, endPos) {
-  // Find top disc(smallest) in startPos array
-
-  // Pop it off and place into endPos array
-
-
-}
+var game = BoardModule();
+game.boardPrint()
+game.move(1, 3)
+game.boardPrint()
+game.move(1,3)
+game.boardPrint()
