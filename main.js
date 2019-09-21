bf0f6e9e31507d0cf1c0d1f590cadbe75dd9a08f
George Green <greezy@Georges-MacBook-Air.local>
2019-09-21 15:41:26 -0400

    Updated Copy with 5 pegs

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 //initial board
 let board = [
-  [3, 2, 1],
+  [5, 4, 3, 2, 1],
+  [],
+  [],
   [],
   []
 ]
@@ -13,84 +15,79 @@ let printBoard = function () {board.map(function(pegs){
 
 printBoard();
 
-//const BoardState = function() {
-/*counter able to track the number of moves needs to be stored inside variable
+const BoardState = function() {
+/*counter able to track the number of moves to be stored inside variable
 but use closure to continue to iterate with moves.
 */
   let counter = 0;
 
-
   let boardMove = function() {
 //takes input from user which peg to remove from
-    let pillarFrom = prompt("Number of moves: " + counter + " Enter the pillar number from which you want to move");
+  let pillarFrom = prompt("Number of moves: " + counter +
+  " Enter the pillar number from which you want to move");
 //change input into number form for comparison
-    let pillarFrom2 = parseInt(pillarFrom);
+  let pillarFrom2 = parseInt(pillarFrom);
 //value of the disc at the pillar selected
-    let pillarFromValue = board[pillarFrom2 - 1];
-      pillarFromValue = pillarFromValue[pillarFromValue.length - 1];
-      console.log(pillarFromValue);
+  let pillarFromValue = board[pillarFrom2 - 1];
+  pillarFromValue = pillarFromValue[pillarFromValue.length - 1];
 //removes the last item on the peg specified by the user
-    let fromValue = board[pillarFrom2 - 1].pop();
-        console.log(fromValue);
-//checks the board for which moves are possible based on the selection of pillar they are moving from
-    let possibleMoves = board.filter(function(moves){
-        console.log(moves[moves.length - 1]);
-        return  (fromValue < moves[moves.length - 1] || moves[moves.length - 1] === undefined)
-    });
-        console.log("You can move this disc to the following pegs: " + possibleMoves);
+  let fromValue = board[pillarFrom2 - 1].pop();
+//checks the board for which moves are possible based
+//on the selection of pillar they are moving from
+  let possibleMoves = board.filter(function(moves){
+    return  (fromValue < moves[moves.length - 1] ||
+    moves[moves.length - 1] === undefined);
+  });
+  console.log("You can move this disc to the following pegs: " + possibleMoves);
 //takes input from user which peg to move item to
-    let pillarTo = prompt("Enter the pillar to which you want to move");
-
-
+  let pillarTo = prompt("Enter the pillar to which you want to move");
 //stores the last item in the to array for checking if it is a valid move
-      let toValue = board[pillarTo - 1];
-      toValue = toValue[toValue.length -1];
+  let toValue = board[pillarTo - 1];
+  toValue = toValue[toValue.length -1];
 //compares value of item to move to the last item in the array specified to move to
-      if (fromValue < toValue || toValue == undefined) {
+  if (fromValue < toValue || toValue == undefined) {
 //adds item removed to the specified peg
       board[pillarTo - 1].push(fromValue);
 //increments counter by one for each move
       counter += 1;
-      } else {
-        board[pillarFrom2 - 1].push(fromValue)
-        alert("That's not a valid move")
-        }
+  } else {
+    board[pillarFrom2 - 1].push(fromValue);
+    alert("That's not a valid move");
+    }
 
-      printBoard();
+  printBoard();
 /*
 function to check if any item in the array is 3 items long(I tried checking 3,
 2, 1 order, but couldn't get reduce to change the bolean value. so this could
 be manipulated by putting the fist disc back on the original peg.
 */
-      let checkWinner = board.reduce(function(hasElement, pegs) {
-        if (pegs.length === 3) {
-        hasElement = true;
-        }
-        return hasElement;
-      }, false);
-
-          if (checkWinner) {
-        alert("Congratulations! You won the game in " + counter + " moves." )
-          };
-
+  let checkWinner = board.reduce(function(hasElement, pegs) {
+    if (pegs.length === 5) {
+    hasElement = true;
     }
-  //}
+    return hasElement;
+  }, false);
 
-
-  boardMove();
-  boardMove();
-  boardMove();
-
-
-/*
-  return {
-    boardMove: boardMove
+  if (checkWinner) {
+    alert("Congratulations! You won the game in " + counter + " moves." );
+    counter = 0;
+    board = [
+      [5, 4, 3, 2, 1],
+      [],
+      [],
+      [],
+      []
+    ]
+    printBoard();
   }
-};
+  }
+  return boardMove;
+}
 
-BoardState.boardMove();
+let playTheGame = BoardState();
 
 
 
-console.log(myBoard);
-*/
+while (true){
+playTheGame();
+}
