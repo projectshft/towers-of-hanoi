154e8d80964ab21a199b4bba7584fc3148056c34
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-09-21 15:43:42 -0400

    Complete mandatory logic and functionality

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
+const welcome = "Welcome to Towers of Hanoi! Input 'game.move(start, end)' to move your disc! Input 'game.startNew()' for a new game."
+
 var BoardModule = function() {
+  let board;
+  let hasWon;
+  let discNum
   // Initialize board representing 3 pegs and 3 discs in starting position
-  let board = [[3, 2, 1], [], []]
+  function initGame() {
+    board = [[3, 2, 1], [], []]
+    discNum = board[0].length;
+    hasWon = false;
+    console.log(welcome);
+    boardPrint();
+  }
+  
+  // Checks current board state checking if user won game
+  function checkWinner() {
+    if (board[0].length === 0) {
+      hasWon = board.reduce(function (prevCondition, peg) {
+        if (peg.length === discNum) {
+          prevCondition = true;
+        }
+        return prevCondition;
+      }, false)
+    }
+  }
+
 
   // Print board status horizontally
   var boardPrint = function () {
@@ -13,25 +37,36 @@ var BoardModule = function() {
     })
   }
 
-  // Move disc from start peg to end peg
+  // Move disc from start peg to end peg if allowed
   function move(startPos, endPos) {
     var acceptableMove = false;
     var currentPegArr = board[startPos - 1]
     var targetPegArr = board[endPos - 1]
-    var potentialPegs = possibleMoves(currentPegArr)
 
+    // Checks for potential moves via possibleMoves function
+    // If target array is found in possibleMoves, flag as acceptable move
+    var potentialPegs = possibleMoves(currentPegArr)
     potentialPegs.forEach(function (peg) {
       if (peg == targetPegArr){
         acceptableMove = true;
       } 
     })
+
     if (acceptableMove === true) {
       var grabDisc = currentPegArr.pop();
       targetPegArr.push(grabDisc)
-    } else {
-      console.log('AH AH AH')
+      console.log("Move complete! Board state is: ")
+    } 
+      else {
+      console.log("Nope - can't move larger disc on top of smaller one. Board state is :")
     }
     boardPrint();
+    checkWinner(); 
+    if(hasWon) {
+      console.log('WINNER WINNER CHICKEN DINNER')
+      initGame();
+    }
+    
   }
 
   // Checks topmost disc of peg for potential moves
@@ -46,17 +81,21 @@ var BoardModule = function() {
     return acceptablePegs;
   }
 
-
   return {
     move: move,
     boardPrint: boardPrint,
-    possibleMoves: possibleMoves
+    newGame: initGame
   }
 }
 
 var game = BoardModule();
-game.boardPrint()
-game.move(1, 2)
-game.move(1, 3)
-game.move(1, 3)
-game.move(3, 1)
+game.newGame()
+console.log(welcome)
+game.move(1,3)
+game.move(1,2)
+game.move(3,2)
+game.move(1,3)
+game.move(2,1)
+// game.move(2,3)
+// game.move(1,3)
+//game.startnew()
