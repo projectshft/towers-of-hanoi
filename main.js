dab0d9e67a4d188140a6d2ab456bda33511382f1
Dave Awad <Dave@MacBook-Pro.local>
2019-09-21 19:08:44 -0400

    saturday night changes

---
+++ main.js
@@ -0,0 +1,65 @@
+// A board representing 3 pegs, and 3 discs on the first peg in ascending order.  
+var game = {
+    boardArray: [["3", "2", "1"],[],[]]
+};
+
+// function that prints board to console using map
+function printBoard() {
+    game.boardArray.map(function(peg) {
+        console.log(" --- " + peg.join(''));
+    });
+};
+
+// prompt that welcomes user to game 
+var windowPrompt = alert("Welcome to Towers of Hanoi");
+//var inputPrompt = prompt("Choose a disc and which peg you would like to move it to.");
+
+//if (windowPrompt != 
+// prompt that takes in disc and peg input
+
+
+
+
+
+function isMovePossbile() {
+    game.boardArray.filter(function(filter){
+       if (move > game.boardArray {
+    })
+    // conditional logic 
+    if ( ) {
+        // possible move
+    } else {
+        // not possible move
+    }
+};
+
+
+
+
+var move = function(startPeg, endPeg) {
+    console.log(startPeg, endPeg);
+    var discToBeMoved = game.boardArray[startPeg].pop();
+    game.boardArray[endPeg].push(move);
+    console.log(discToBeMoved);
+    console.log(game.boardArray);
+    
+};
+
+//var checkWinner = 
+
+printBoard();
+move(0, 2);
+
+
+
+
+/* need to create three arrays here for the three pegs 
+on game start run board (use map to print board!)
+step 1. player submits move ( function)
+step 2. game checks if move is valid ( function)
+step 3. action based on step 2. If move is valid, excute move and print. If move is invalid we'll print message to the user. 
+
+need a hint function where user can enter peg number. Use filter to return pegs with valid moves 
+all discs on one peg 
+have to be in certain order, highest value to lowest value, has to be a different peg. if all conditions are met game will check winner and the gane will end using reduce function 
+*/
