8211612889db355621da248e39b74ba63864c8d8
Patrick Crawford <53802272+pjcrawford@users.noreply.github.com>
2019-11-23 11:54:30 -0500

    restart w/ global function

--- main.js
+++ main.js
@@ -1,95 +1,32 @@
-var startingBoard = [
-  [5, 4, 3, 2, 1],
-  [],
-  []
-]
+
+//creating function to wrap game in
+const Towers = function() {
+  let boardMapped = [];
+  let checkWinner;
+  let peg;
+  let disc;
+  //counter function
+  let moves = 0;
 
 
-
-var boardMapped = [];
-
-var createBoard = function(array) {
-  startingBoard.forEach(function(arr) {
-    var newArr = arr.map(function(num) {
-      return Number(num);
-    })
-    array.push(newArr);
-  });
-}
-
-createBoard(boardMapped);
-
-
-var printBoard = function() {
+  let printBoard = function() {
+    alert('The starting board is shown on your console:')
     console.log("--- " + boardMapped[0].join(" "));
     console.log("--- " + boardMapped[1].join(" "));
     console.log("--- " + boardMapped[2].join(" "));
+
+  console.log(printBoard)();
+  }
+
+
+
 };
 
 
 
-var checkWinner = function() {
-  if (boardMapped.find(function(arr) {
-      return arr.reduce(function(sum, number) {
-        return sum + number
-      }, 0) === 5 && arr.every(function(num, i, array) {
-        return num > array[i + 1] || num === 1
-      })
-    }) && boardMapped[0].length === 0) {
-    console.log("You won the game! " + board.totalMoves +
-    " moves!");
-    createBoard(boardMapped);
-  }
-}
-//
-function moveDisc(fromPeg, toPeg) {
-  start = prompt(("Which disk do you want to move?  Choose between 1, 2, or 3"));
-    if (typeof fromPeg === 'number' && typeof toPeg === 'number') {
-        if (fromPeg > 0 && fromPeg < boardMapped.totalPegs + 1 && toPeg > 0 && toPeg < boardMapped.totalPegs + 1) {
-          if ((boardMapped[fromPeg- 1][boardMapped[fromPeg - 1].length - 1] < boardMapped[toPeg - 1][boardMapped[toPeg - 1].length - 1]))
-            boardMapped.totalMoves += 1;
-            console.log("Total Moves: " + board.totalMoves);
-            console.log("Nice Move!:");
-            checkWinner();
-
-        } else {
-          console.log("Invalid Move!:");
-          printBoard();
-        }
-      }
-    }
-  
-  
 
 
-alert("Here is the starting board:");
-printBoard();
-
-moveDisc();
-
-// var board = {
-
-//   totalPegs: 3,
-
-//   totalMoves: 0,
-
-//   moveDisc: function(start, finish) {
-//     start = prompt(("Which disk do you want to move?  Choose between 1, 2, or 3"));
-//     if (typeof start === 'number' && typeof finish === 'number') {
-//         if (start > 0 && start < board.totalPegs + 1 && finish > 0 && finish < board.totalPegs + 1) {
-//           if ((boardMapped[start - 1][boardMapped[start - 1].length - 1] < boardMapped[finish - 1][boardMapped[finish - 1].length - 1]))
-//             board.totalMoves += 1;
-//             console.log("Total Moves: " + board.totalMoves);
-//             console.log("Nice Move!:");
-//             checkWinner();
-
-//         } else {
-//           console.log("Invalid Move!:");
-//           printBoard();
-//         }
-//       }
-//     }
-//   }
 
 
- 
+
+Towers();
