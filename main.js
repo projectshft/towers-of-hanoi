d841d80ea8f452568823086f3f961970fef8db05
Patrick Crawford <53802272+pjcrawford@users.noreply.github.com>
2019-11-24 20:44:34 -0500

    updated checkwinner and moveDisc functions

--- main.js
+++ main.js
@@ -20,16 +20,30 @@ const Towers = function() {
   printBoard();
 
 
+  var moveDisc = function(startPeg, endPeg) {  
+    var userInput = board[startPeg].pop();
+    board[endPeg - 1].push(userInput);
+    //test to make sure userInput works when moving disc
+    console.log("worked!");
+    printBoard();
+    moves +- 1;
 
 
 
+  //reducer function from Array.prototype.reduce
+    var checkWinner = board[endPeg].reduce(function(accumulator, currentValue) {
+      if (board[endPeg] !== board[0]) { 
+      return accumulator + currentValue;
+    } else console.log("You are a winner!");
+    })
+  };
+
+//gives access to moveDisc function
+  return {
+    moveDisc: moveDisc
+  };
 };
 
 
+var newGame = Towers();
 
-
-
-
-
-
-Towers();
