151cf95cd01e9ded9136d186d3fc09abc96f3826
Tatiana Grigorieva <tatianagrigorieva@Tatianas-MacBook-Pro.local>
2020-01-27 18:44:24 -0500

    reduce method added, filter method fixed, rendering board fixed

--- main.js
+++ main.js
@@ -1,74 +1,81 @@
 //The Challenge - Towers of Hanoi
 
-function Game(pegs, steps, height) {
+function Game(pegs) {
   this.pegs = pegs
   //the number of moves that the player has done
-  this.steps = steps
+  this.steps = 0
   //the size of the winner peg array
-  this.height = height
+  this.height = pegs.length;
+
+  this.score = this.pegs[0].reduce((acc, value) => {
+    return parseInt(acc) + parseInt(value);
+  })
 }
 
 Game.prototype.initialize = function(array) {
   this.pegs = Array.from(array) || [];
-  height = (array.length > 0 ? this.pegs[0].length : 0);
+  //height = (array.length > 0 ? this.pegs[0].length : 0);
 }
 
 Game.prototype.moveDisc = function(source, destination) {
   let winner = false;
+  let valid = false;
   //get the source array and the disc size of the last element
   let srcArr = this.pegs[source - 1];
   let srcDisc = parseInt(srcArr[srcArr.length - 1]);
+  //console.log('Source disc: ' + srcDisc);
 
-  //check the last element size of the destination if any
-  //getDiscSize will return 0 if peg is empty
-  let existingDiscSize = this.getDiscSize(destination - 1);
+  //check if the disc is a valid size to move
+  if(this.pegs[destination - 1].length > 0) {
+    valid = this.validMove(this.pegs[destination - 1], srcDisc);
+  }
+  else valid = true;
 
   //if src size bigger, report a console.error();
-  if (existingDiscSize > 0 && existingDiscSize < srcDisc) {
-    console.log("Can't move a disc on top of another if it is bigger ");
-  }
-  else {
-    //else update the Game
+  if (valid) {
     this.pegs[destination - 1].push(srcDisc);
     this.pegs[source - 1].pop(srcDisc);
-    winner = this.checkWinner(this.height);
+    //console.log('Initial score: ' + this.score);
+    winner = this.checkWinner(this.pegs[destination - 1]);
+    //console.log('Winner check return: ' + winner);
+    console.log("That move was successful, board is now:");
+  }
+  else {
+    console.log("You cannot move a larger disc on top of a smaller one, board is still:");
   }
   //increment number of moves
   this.steps++;
+  this.printGame();
 
   if(winner) {
-    //print final Game and reset the game
-    this.printGame();
-    this.resetGame(this.pegs);
+    //reset the game
+    this.resetGame();
   }
 }
 
-Game.prototype.getDiscSize = function(dest) {
+Game.prototype.validMove = function(dest, srcDiscSize) {
   //use filter
-  let discs = Array.from(this.pegs[dest]);
+  //debugger;
+  let result = dest.filter(function(item) {
+      return item > srcDiscSize;
+  });
+  //console.log('Return from filter: ' +result);
 
-  if(discs.length > 0) {
-    let size = discs.filter(item => {
-      if((discs.length - 1) === discs.indexOf(item))
-      return parseInt(item);
-    });
-    return size[0];
-  } else return 0;
-}
-
-Game.prototype.checkWinner = function() {
-  let result = 0;
-  this.pegs.forEach((item, index) => {
-    if(index > 0 && item.length > 0) {
-      result = item.length;
-    }
-  })
-  //return true if we moved all the discs to a new peg false otherwise
-  if(this.height === result) return true;
+  if(result[result.length - 1] > srcDiscSize) return true;
   else return false;
 }
 
-Game.prototype.resetGame = function(result) {
+Game.prototype.checkWinner = function(dest) {
+  let result = dest.reduce((acc, value) => {
+      return parseInt(acc) + parseInt(value);
+    }, 0 );
+
+  //console.log('checkWinner result var:' + result);
+  if(result === this.score) return true;
+  else return false;
+}
+
+Game.prototype.resetGame = function() {
   //announce the winner and initialize pegs array for a new game
   console.log('You WIN!');
   console.log('Number of moves: ' + this.steps + '. Illegal moves included if any.');
@@ -79,19 +86,23 @@ Game.prototype.resetGame = function(result) {
 }
 
 Game.prototype.printGame = function() {
-  this.pegs.map((peg) => {
-    console.log('--- ' + peg);
-    console.log('\n');
+  const pegDisplay = this.pegs.map((peg) => {
+    return '--- ' + peg;
+  })
+  pegDisplay.forEach(item => {
+    console.log(item);
   })
 }
 
-let test = new Game([["3", "2", "1"],[],[]], 0, 3);
+let test = new Game([["3", "2", "1"],[],[]]);
 test.printGame();
 test.moveDisc(1,2);
 test.moveDisc(1,2);//illegal move
 test.moveDisc(1,3);
 test.moveDisc(2,3);
+test.moveDisc(1,3); //illegal move
 test.moveDisc(1,2);
 test.moveDisc(3,1);
+test.moveDisc(3,1); //illegal move
 test.moveDisc(3,2);
 test.moveDisc(1,2);
