5b52369b23d8e7f3c1a88e82dc2fc373297488eb
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-05-01 11:52:19 -0400

    moveDisc completed

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert("hello, world")
+// A board representing 3 pegs and 3 discs on the first peg in ascending order
+board = [
+  ['3', '2', '1'],
+  [],
+  []
+]
+
+var moveDisc = function(x, y) {
+  board[y].push(board[x][board[x].length -1]);
+  board[x].splice(board[x].length -1)
+  console.log(board)
+}
+
+moveDisc(0, 2);
+
+
+//take board[x].length and move it to board[y].length
+//
