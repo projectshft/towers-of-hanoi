8a2dedd4570c6bd248e0f6acd99a02e16e306d00
MCola <mcolapietro502@gmail.com>
2020-05-01 18:57:14 -0400

    added functionality to moveDisc function

--- main.js
+++ main.js
@@ -14,27 +14,48 @@ let isValidMoveType = (moveType) => {
 }
 
 const moveDisc = (start, end) => {
+  let startDisc = board[start][board[start].length - 1];
+  console.log(`startDisc = ${startDisc}`);
+  let endDisc = board[end][board[end].length - 1]
+  console.log(`endDisc = ${endDisc}`)
 
   if (!isValidMoveType(start) || !isValidMoveType(end)) {
-    return console.log(`Please enter a valid peg number (Between 0 and 2)`)
+    console.log(`INVALID MOVE: Please enter a valid peg number (Between 0 and 2). The board is still:`);
+    console.log(board);
+    return;
   }
 
-  if(board[start].length = 0) {
-    return console.log(`There are no more moves for that peg, please refer to board printout`);
+  if(board[start].length === 0) {
+    console.log(`INVALID MOVE: There are no more moves for that peg, please refer to board printout. The board is still:`);
+    console.log(board);
+    return;
   }
+
+  if(startDisc > endDisc) {
+    console.log(`INVALID MOVE: You can not move a larger disc on top of a smaller disc. The board is still:`);
+    console.log(board);
+    return;
+  }
+
   
   board[end].push(board[start][board[start].length - 1]);
-  board[start].pop() 
+  board[start].pop();
+  console.log(`The move was successful. The board is now:`);
+  console.log(board);
 };
 
-moveDisc(-1, 1)
-moveDisc('pizza', 1)
-moveDisc(0,3)
+// moveDisc(-1, 1)
+// moveDisc('pizza', 1)
+// moveDisc(0,3)
+// moveDisc(0, 2)
+// moveDisc(0, 2)
+// moveDisc(0, 2)
 moveDisc(0, 2)
-moveDisc(0, 2)
-moveDisc(0, 2)
-moveDisc(0, 2)
-console.log(board);
+
+moveDisc(0,2)
+
+moveDisc(2, 0)
+
 
 
 
