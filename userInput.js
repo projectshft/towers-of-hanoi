ab4ac5cfce3fa3d5c09141c366e4f4be5f864e20
Pierre Lourens <plourens3@gmail.com>
2020-05-02 19:06:50 -0400

    Adding alert to instruct user to play in console

--- userInput.js
+++ userInput.js
@@ -1,3 +1,4 @@
 let userPegs = Number(prompt("Welcome to Towers of Hanoi! How many pegs would you like", 3));
 let userDiscs = Number(prompt("How many discs would you like?", 3));
+alert("Open your console to play!");
 let board = TowersGame(userPegs, userDiscs);
