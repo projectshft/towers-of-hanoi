c2e9298367f7337a8ffffc97862e4b70b498130b
Joe Krall <kralljr@outlook.com>
2020-05-02 02:20:58 -0400

    created checkWin function

--- main.js
+++ main.js
@@ -1,39 +1,74 @@
-var pegs = [
-  [3, 2, 1],
-  [],
-  []
-];
+var board = {
 
-var move = function(fromPeg, toPeg) {
-  var disc = fromPeg.pop();
-  toPeg.push(disc);
-};
+  pegs: [
+    [3, 2, 1],
+    [],
+    []
+  ],
 
-//Why does printing the whole array - console.log(pegs) - give identical
-// results if move was called? It's passed by reference - do you want it
-// array at the time of console.log, or at the time?
+  totalMoves: 0,
 
-// var printBoard = function() {
-//   console.log(pegs);
-// }
+  move: function(fromPeg, toPeg) {
+    var disc = this.pegs[fromPeg-1].pop();
+    // Check to make sure
+    this.pegs[toPeg-1].push(disc);
 
-var printBoard = function() {
+    this.totalMoves++
+    //this.printBoard();
+    this.checkWin();
+  },
 
-  var printablePegs = pegs.map(function(peg) {
-    var base = "---  "; //strings are immutable
-    peg.forEach(function(disc) {
-      base = base.concat(disc + " "); //so we reassign base each time
-    })
-    return base;
+  checkMove: function() {},
+
+  checkWin: function() {
+
+    var discWeights = this.pegs.reduce(function(array, currentPeg) {
+      array.push(currentPeg.reduce(function(sum, currentDisc) {
+        return sum += currentDisc;
+      }, 0));
+      return array;
+    }, []);
+
+    console.log(discWeights);
+    console.log(discWeights[1]);
+    console.log(discWeights[2]);
+
+    if (discWeights[1] === 6 || discWeights[2] === 6) {
+      console.log('You have won in ' + this.totalMoves + ' moves. Starting new game.');
+      this.resetBoard();
+      console.log("------------------------------");
+    }
+
+  },
+
+  resetBoard: function() {
+    this.pegs = [ [3, 2, 1], [], [] ];
+  },
+
+  printBoard: function() {
+
+    var printablePegs = this.pegs.map(function(peg) {
+      var base = "---  "; //strings are immutable
+      peg.forEach(function(disc) {
+        base = base.concat(disc + " "); //so we reassign base each time
+      })
+      return base;
     });
 
-  printablePegs.forEach(function(peg) {
-    console.log(peg);
-  });
+    printablePegs.forEach(function(peg) {
+      console.log(peg);
+    });
 
-  console.log("------------------------------")
+    console.log("------------------------------");
+  }
+
 }
 
-printBoard();
-move(pegs[0], pegs[2]);
-printBoard();
+board.move(1, 3);
+board.move(1, 2);
+board.move(3, 2);
+board.move(1, 3);
+board.move(2, 1);
+board.move(2, 3);
+board.move(1, 3);
+board.printBoard();
