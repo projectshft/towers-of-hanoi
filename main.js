3b034737a52baddb752c2a4cdd733cb719d6e19c
MCola <mcolapietro502@gmail.com>
2020-05-02 11:29:43 -0400

    finished function to check for winner

--- main.js
+++ main.js
@@ -1,70 +1,101 @@
-//Create 2D array
 
 let board = [
-  [3,2,1],
   [],
+  [3,2,1],
   []
 ];
 
-console.log(board)
+let controlBoard = {
 
-//helper function to determine if start & end values are valid peg numbers
-let isValidMoveType = (moveType) => {
-  return (moveType >= 0) && (moveType <= 2)
+  moveCount: 0,
+
+  isValidMoveType (moveType) { //helper function to help simplify moveDisc function
+    return (moveType >= 0) && (moveType <= 2)
+  },
+
+  moveDisc(start, end) {
+
+    if (!this.isValidMoveType(start) || !this.isValidMoveType(end)) {
+      console.log(`INVALID MOVE: Please enter a valid peg number (Between 0 and 2). The board is still:`);
+      console.log(board);
+      return;
+    }
+  
+    if(board[start].length === 0) {
+      console.log(`INVALID MOVE: There are no more moves for that peg, please refer to board printout. The board is still:`);
+      console.log(board);
+      return;
+    }
+  
+    let startDisc = board[start][board[start].length - 1];
+    let endDisc = board[end][board[end].length - 1]
+    
+    if(startDisc > endDisc) {
+      console.log(`INVALID MOVE: You can not move a larger disc on top of a smaller disc. The board is still:`);
+      console.log(board);
+      return;
+    }
+  
+    board[end].push(board[start][board[start].length - 1]);
+    board[start].pop();
+    this.moveCount = this.moveCount + 1
+    console.log(`The move was successful. The board is now:`);
+    console.log(board);
+  },
+
+  checkWinner() {
+    
+    let boardCalc =  board.reduce((accumulator, boards) => {
+      let id = board.indexOf(boards);
+      let pegCalc = boards.reduce((accumulator, peg) => (accumulator += peg),0)
+
+      accumulator[id] = pegCalc;
+
+      return accumulator
+    }, {});
+
+    if (boardCalc['1'] === 6 || boardCalc['2'] === 6) {
+      return console.log('winner')
+    };
+
+  },
+
+  resetBoard() {
+    this.moveCount = 0
+    return board = [[3,2,1],[],[]]
+  }
+
+
+
 }
 
-const moveDisc = (start, end) => {
-
-  if (!isValidMoveType(start) || !isValidMoveType(end)) {
-    console.log(`INVALID MOVE: Please enter a valid peg number (Between 0 and 2). The board is still:`);
-    console.log(board);
-    return;
-  }
-
-  if(board[start].length === 0) {
-    console.log(`INVALID MOVE: There are no more moves for that peg, please refer to board printout. The board is still:`);
-    console.log(board);
-    return;
-  }
-
-  let startDisc = board[start][board[start].length - 1];
-  console.log(`startDisc = ${startDisc}`);
-  let endDisc = board[end][board[end].length - 1]
-  console.log(`endDisc = ${endDisc}`)
-  
-  if(startDisc > endDisc) {
-    console.log(`INVALID MOVE: You can not move a larger disc on top of a smaller disc. The board is still:`);
-    console.log(board);
-    return;
-  }
-
-  board[end].push(board[start][board[start].length - 1]);
-  board[start].pop();
-  console.log(`The move was successful. The board is now:`);
-  console.log(board);
-};
-
-moveDisc(-1, 1)
-moveDisc('pizza', 1)
-moveDisc(0,3)
-moveDisc(0, 2)
-moveDisc(0, 1)
-moveDisc(2,1)
-moveDisc(0, 2)
-moveDisc(0,1)
+console.log(controlBoard.checkWinner())
 
 
 
+// controlBoard.moveDisc(-1, 1)
+// console.log(controlBoard.moveCount)
+// controlBoard.moveDisc('pizza', 1)
+// console.log(controlBoard.moveCount)
+// controlBoard.moveDisc(0,3)
+// console.log(controlBoard.moveCount)
+// controlBoard.moveDisc(0, 2)
+// console.log(controlBoard.moveCount)
+// controlBoard.moveDisc(0, 1)
+// console.log(controlBoard.moveCount)
+// controlBoard.moveDisc(2,1)
+// console.log(controlBoard.moveCount)
+// controlBoard.moveDisc(0, 2)
+// console.log(controlBoard.moveCount)
+// controlBoard.moveDisc(0,1)
+// console.log(controlBoard.moveCount)
+// controlBoard.resetBoard();
+// console.log(board);
+// console.log(controlBoard.moveCount)
 
-//Saved code for later
+// function sum(obj) {
+//   return Object.keys(obj).reduce((sum,key)=>sum+parseFloat(obj[key]||0),0);
+// }
+// let sample = { a: 1 , b: 2 , c:3 };
 
-//board[0].splice(board[(board[0].length- 1)],0,1) keep for future reference (might need splice later)
-//board[0].push(board[2][(board[2].length - 1)])
-
-  // if (typeof(start) !== 'number' || typeof(end) !== 'number') {
-  //    return console.log(`Please enter a valid peg number (Between 0 and 2)`)
-  // }
-
-  // if (start > 2 || start < 0 || end > 2 || end < 0 ) {
-  //   return console.log(`Please enter a valid peg number (Between 0 and 2)`)
-  // }
+// console.log(`sum:${sum(sample)}`);
