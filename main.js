ac68c41bc96682b1ac939874857c58ce466870a7
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-05-03 13:29:16 -0400

    all solved

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ var towersOfHanoi = function() {
     ['3', '2', '1'],
     [],
     []
-  ]
+  ];
 
-  var counter = 0
+  var counter = 0;
 
   var moveDisc = function(startPeg, endPeg) {
     // if the disc on the endPeg is greater than the disc you're moving, OR if the endPeg is empty
@@ -15,25 +15,33 @@ var towersOfHanoi = function() {
     // push starting disc to the top of the endPeg
     board[endPeg].push(board[startPeg][board[startPeg].length -1]);
     // then delete the starting disc in its original location
-    board[startPeg].splice(board[startPeg].length -1)
+    board[startPeg].splice(board[startPeg].length -1);
     // add 1 to the counter
-    counter ++
+    counter ++;
     // check to see if the game is over
-    checkWinner()
+    checkWinner();
     } else {
-    throw "cannot move a bigger disc onto a smaller disc"
+    throw "cannot move a bigger disc onto a smaller disc";
     }
-  }
+  };
 
   var possibleMoves = function(startPeg) {
-    //take a disc
-    //look at all pegs on the board that have a larger number
-    var possiblePegs = board.find(function(peg) {
-      return (peg[0] === undefined) || (board[startPeg][board[startPeg].length-1] > peg[0])
-      // (board[startPeg][board[startPeg].length-1] > peg[0])
-    })
-    console.log(board.indexOf(possiblePegs));
-  }
+    //if the startPeg isn't empty
+    if (board[startPeg][0] !== undefined) {
+      console.log('Your disc can be moved to these peg(s):');
+      //return array of discs the startPeg disc can be moved to
+      possiblePegs = board.filter(function(peg) {
+        return (board[startPeg][board[startPeg].length-1] < peg[0] || peg[0] === undefined);
+      });
+      // find and report what pegs those discs are on, but only if startPeg isn't one of those pegs
+      possiblePegs.forEach(function(peg) {
+        if (board.indexOf(peg) !== startPeg) {
+          console.log(board.indexOf(peg));
+        }
+      });
+      //if the startPeg is empty, say so
+    } else console.log("There's no disc on that peg");
+  };
 
   var printBoard = function() {
     // return board without commas
@@ -56,7 +64,8 @@ var towersOfHanoi = function() {
     // if true, declare winner and reset board
     if (isItAWin === true) {
       console.log("Now that's a winner! You completed the game in " + counter + " moves.")
-      console.log("Play again?")
+      console.log("Here's your winning board. Play again?")
+      console.log(printBoard())
       board = [['3', '2', '1'],[],[]]
     }
   }
@@ -70,119 +79,10 @@ var towersOfHanoi = function() {
 
 var action = towersOfHanoi()
 
-action.possibleMoves(0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////////////
-// board = [
-//   ['3', '2', '1'],
-//   [],
-//   []
-// ]
-//
-// var boardControls = {
-//   counter: 0,
-//
-//   possibleMoves: function(startPeg) {
-//     //take a disc
-//     //look at all pegs on the board that have a larger number
-//     var possiblePegs = board.find(function(peg) {
-//       return (peg[0] === undefined) || (board[startPeg][board[startPeg].length-1] > peg[0])
-//       // (board[startPeg][board[startPeg].length-1] > peg[0])
-//     })
-//     console.log(board[possiblePegs]);
-//   },
-//
-//   moveDisc: function(startPeg, endPeg) {
-//     // if the disc on the endPeg is greater than the disc you're moving, OR if the endPeg is empty
-//     if ((board[endPeg][0] > board[startPeg][board[startPeg].length -1]) || (board[endPeg][0] === undefined)){
-//     // push starting disc to the top of the endPeg
-//     board[endPeg].push(board[startPeg][board[startPeg].length -1]);
-//     // then delete the starting disc in its original location
-//     board[startPeg].splice(board[startPeg].length -1)
-//     // add 1 to the counter
-//     boardControls.counter ++
-//     // check to see if the game is over
-//     boardControls.checkWinner()
-//     } else {
-//     throw "cannot move a bigger disc onto a smaller disc"
-//     }
-//
-//   },
-//   checkWinner: function() {
-//     // set reduce function = isItAWing
-//     var isItAWin = board.reduce(function(accumulator, peg, currentIndex) {
-//       // after first loop, if a peg is full, return true
-//       if ((currentIndex >= 1) && (peg.length === 3)) {
-//         accumulator = true;
-//         }
-//       return accumulator
-//     },false)
-//     // if true, declare winner and reset board
-//     if (isItAWin === true) {
-//       console.log("Now that's a winner! You completed the game in " + boardControls.counter + " moves.")
-//       console.log("Play again?")
-//       board = [['3', '2', '1'],[],[]]
-//     }
-//   },
-//   printBoard: function(peg) {
-//     // return board without commas
-//     var printedBoard = board.map(function(num) {
-//       return "---" + num.join(" ")
-//     })
-//     // print board
-//     console.log(printedBoard);
-//   }
-// }
-//
-// boardControls.moveDisc(0, 2);
-// boardControls.moveDisc(0, 1);
-// boardControls.moveDisc(2, 1);
-// boardControls.moveDisc(0, 2);
-// boardControls.moveDisc(1, 0);
-// boardControls.moveDisc(1, 2);
-// boardControls.moveDisc(0, 2);
-//
-//
-// boardControls.printBoard();
-// boardControls.possibleMoves(0);
-
-// boardControls.moveDisc(0, 1);
-// boardControls.moveDisc(0, 2);
-// boardControls.moveDisc(1, 2);
-// boardControls.moveDisc(0, 1);
-// boardControls.moveDisc(2, 0);
-// boardControls.moveDisc(2, 1);
-// boardControls.moveDisc(0, 1);
-
-// console.log(board)
-
-
-
-
-//take board[x].length and move it to board[y].length
-// if board[y][0] is smaller than board[x][length]
-//if peg 1 and 3 or peg 1 and 2 are empty, the user
-
-//start with board
-//two arrays should be empty
-//
+action.moveDisc(0,2)
+action.moveDisc(0,1)
+action.moveDisc(2,1)
+action.moveDisc(0,2)
+action.moveDisc(1,0)
+action.printBoard()
+action.possibleMoves(1)
