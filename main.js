507d4aa832b71fe0a41d2602fcc2e12e7c41a245
MCola <mcolapietro502@gmail.com>
2020-05-03 17:35:28 -0400

    created working moveOptions function

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ This game is designed to be played in the console.
 */
 
 let board = [
-  [5, 4, 3, 2, 1],
-  [],
+  [5, 4, 3, 2],
+  [1],
   []
 ];
 
@@ -24,13 +24,14 @@ const controlBoard = {
 
   //object to hold most messages for readability
   messageBoard: {
-    errorOne: `INVALID MOVE: Please enter a valid peg number (Between 1 and 3). The board is still:`,
-    errorTwo: `INVALID MOVE: There are no more moves for that peg, please refer to board printout. The board is still:`,
-    errorThree: `INVALID MOVE: You can not move a larger disc on top of a smaller disc. The board is still:`,
+    errorInvalidPeg: `INVALID MOVE: Please enter a valid peg number (Between 1 and 3). The board is still:`,
+    errorNoDiscs: `INVALID MOVE: There are no more moves for that peg, please refer to board printout. The board is still:`,
+    errorNoDiscs2: `INVALID ENTRY: There are no discs on that peg to check`,
+    errorDiscSize: `INVALID MOVE: You can not move a larger disc on top of a smaller disc. The board is still:`,
     validMove: `The move was successful. The board is now:`,
   },
 
-  isValidPeg (moveValue) { 
+  isValidPeg(moveValue) {
     return (moveValue >= 1) && (moveValue <= board.length)
   },
 
@@ -38,33 +39,33 @@ const controlBoard = {
     //The code for referencing the arrays is adjusted accordingly with (-1) add to start and end
     //initial validation for correct inputs
     if (!this.isValidPeg(start) || !this.isValidPeg(end)) {
-      console.log(this.messageBoard.errorOne)
+      console.log(this.messageBoard.errorInvalidPeg)
       this.printBoard();
       return;
     }
-  
-    if(board[start - 1].length === 0) {
-      console.log(this.messageBoard.errorTwo)
+
+    if (board[start - 1].length === 0) {
+      console.log(this.messageBoard.errorNoDiscs)
       this.printBoard();
       return;
     }
-    
+
     //variables for moving the discs based on user input
     let startDisc = board[start - 1][board[start - 1].length - 1];
     let endDisc = board[end - 1][board[end - 1].length - 1]
-    
-    if(startDisc > endDisc) {
-      console.log(this.messageBoard.errorThree)
+
+    if (startDisc > endDisc) {
+      console.log(this.messageBoard.errorDiscSize)
       this.printBoard();
       return;
     }
-    
+
     //moving the discs begins below (if constraints are passed above)
-    board[end - 1].push(board[start -1][board[start - 1].length - 1]);
+    board[end - 1].push(board[start - 1][board[start - 1].length - 1]);
     board[start - 1].pop();
-    
-    this.moveCount = this.moveCount + 1
-    
+
+    this.moveCount += 1;
+
     //after successful move, board is printed and checked for a winning state
     console.log(this.messageBoard.validMove)
     this.printBoard();
@@ -72,30 +73,32 @@ const controlBoard = {
   },
 
   moveOptions(pegNumber) {
-     
+    //ensuring the moveOption entry has a disc on it
+    if (board[pegNumber].length === 0) {
+      console.log(this.messageBoard.errorNoDiscs2)
+      return;
+    }
+    //creating deep clone of board
     let newBoard = JSON.parse(JSON.stringify(board));
-    
+    //adding Peg# for return use
     newBoard.forEach((boards) => {
-      boards.unshift(('Peg' + ' ' + newBoard.indexOf(boards)))
+      boards.unshift(('Peg' + ' ' + (newBoard.indexOf(boards) + 1)))
     });
 
-    // console.log(newBoard);
-    // console.log('break')
-    // console.log(newBoard[0])
-    // console.log(newBoard[1])
-    // console.log(newBoard[2])
-
-    let filter = newBoard.filter((pegs) => {  
-      if ((pegs.slice(-1)[0]) > newBoard[pegNumber].slice(-1)[0] || pegs.slice(-1)[0] === typeof(string)) {
-      console.log(pegs.slice(-1)[0])
-        //console.log(`Peg: ${board.indexOf(pegs)} is an available move`);
-       
-      }
-    });
+    //console.log(newBoard);
     
-    return filter
+    //filtering out only pegs that have a higher number from input or only contains the newly created string
+    let filter = newBoard.filter((pegs) => {
+      
+      let slicedPegs = pegs.slice(-1)
+      let pegsType = typeof(slicedPegs[0])
+      
+      if ((slicedPegs[0]) > newBoard[pegNumber].slice(-1)[0] || pegsType === 'string')
+      
+      return true
+    });
 
-  
+    return console.log(filter)
   },
 
   printBoard() {
@@ -108,9 +111,9 @@ const controlBoard = {
   checkWinner() {
     //reduce helper methods that transforms the board into objects.
     //object keys equal array indexes board and the values are the sum of the nested arrays
-    let boardCalc =  board.reduce((accumulator, boards) => {
+    let boardCalc = board.reduce((accumulator, boards) => {
       let id = board.indexOf(boards);
-      let pegCalc = boards.reduce((accumulator, peg) => (accumulator += peg),0)
+      let pegCalc = boards.reduce((accumulator, peg) => (accumulator += peg), 0)
 
       accumulator[id] = pegCalc;
 
@@ -129,7 +132,11 @@ const controlBoard = {
 
   resetBoard() {
     this.moveCount = 0
-    board = [[5,4,3,2,1],[],[]]
+    board = [
+      [5, 4, 3, 2, 1],
+      [],
+      []
+    ]
     return this.printBoard();
   }
 }
@@ -144,17 +151,3 @@ console.log(controlBoard.moveOptions(1))
 // controlBoard.moveDisc(3,2)
 // controlBoard.moveDisc(1,3)
 // controlBoard.moveDisc(3,2)
-
-
-
-
-//saved just in case new filter function does not work
-
-  // let filter = board.filter((pegs) => {  
-  //   if ((pegs.slice(-1)[0]) > board[pegNumber].slice(-1)[0] || pegs.slice(-1)[0] === undefined) {
-  //   console.log(`Peg: ${board.indexOf(pegs)} is an available move`);
-  //     return true;
-  //   }
-  //  });
-  
-  // return filter
