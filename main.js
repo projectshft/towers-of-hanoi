44571ba86810ce9d45f5c5ee19de8fbb26e31a2d
Daniel Renteria <d.renteria_a@yahoo.com>
2020-08-16 13:24:24 -0400

    add checkWinner and gameOver functionality

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let startingBoard = [
     [],
     []
 ];
+let playingBoard;
 
 const map = function(boardArray) {
     let spacing = ''
@@ -14,6 +15,7 @@ const map = function(boardArray) {
     for (let i = 0; i < boardArray.length; i++) {
         if(boardArray === startingBoard) {
             boardArray[i].reverse();
+            playingBoard = JSON.parse(JSON.stringify(startingBoard));
         }
         if(i >= 1) {
             spacing += '   '
@@ -31,108 +33,77 @@ const map = function(boardArray) {
     console.log(pegs)
     console.log(spacing)
 }
-map(startingBoard)
-
-//Install lodash to get this to work
-/*
-let playingBoard = _.cloneDeep(startingBoard);
-console.log(playingBoard)
-*/
+map(startingBoard);
 
 const Board = function() {
-    //let playingBoard = _.cloneDeep(startingBoard);
-
     let moveDisc = function(startPeg, endPeg) {
-        let start = playingBoard[startPeg - 1].shift();
-        playingBoard[endPeg - 1].unshift(start);
-        map(playingBoard);
-        moves++;
-        if(gameOver) {
-            console.log(`Congratulations! You win!`)
-            playingBoard = Array.from(startingBoard)
+        let startPegIndex = startPeg - 1;
+        let endPegIndex = endPeg - 1;
+
+        if(Math.min(...playingBoard[endPegIndex]) < Math.min(...playingBoard[startPegIndex])) {
+            console.log('That is an invalid move!')
+        } else {
+            let start = playingBoard[startPegIndex].shift();
+            playingBoard[endPegIndex].unshift(start);
+            map(playingBoard);
+            moves++;
+            checkWinner();
         }
+
+        if(gameOver) {
+            console.log(`Congratulations! You win!`);
+            console.log(`Number of moves: ${moves}`)
+            startingBoard[0].reverse();
+            playingBoard = startingBoard;
+        }
+    };
+
+    let moveCheck = function(peg) {
+        let pegIndex = peg -1
+        let pegMin = Math.min(...playingBoard[pegIndex]);
+        
+        if(playingBoard[pegIndex].length == 0) {
+            console.log('There is no disc to move on this peg');
+        } else {
+            for (let i = 0; i < playingBoard.length; i++) {
+                if(Math.min(...playingBoard[i]) == Infinity || pegMin < Math.min(...playingBoard[i])) {
+                    console.log(`you can move a disc to peg ${i + 1}`)
+                } else {
+                    console.log(`you cannot move a disc to peg ${i + 1}`)
+                }
+            }
+        }
+    };
+
+    let checkWinner = function() {
+        let winningPeg;
+        playingBoard.forEach(element => {
+            if (element.length > 0) {
+                let reducer = (accumulator, currentValue) => accumulator + currentValue;
+                winningPeg = element.reduce(reducer);
+                if (winningPeg.length === startingBoard[0].length) {
+                    return gameOver = true
+                }
+            }
+
+        })
     }
+
     return {
-        moveDisc: moveDisc
+        moveDisc: moveDisc,
+        moveCheck: moveCheck
     }
-
-
 }
-// board = Board();
-// board.moveDisc(1, 2);
-// board.moveDisc(1, 3);
-// board.moveDisc(2, 3);
-// board.moveDisc(1, 2);
-// board.moveDisc(3, 1);
-// board.moveDisc(3, 2);
-// gameOver = true
-// board.moveDisc(1, 2);
 
-// console.log(startingBoard);
+board = Board();
+board.moveDisc(1, 2);
+board.moveDisc(1, 3);
+board.moveDisc(2, 3);
+board.moveDisc(1, 2);
+board.moveDisc(3, 1);
+board.moveDisc(3, 2);
+board.moveDisc(1, 2);
 
-//Right now game still needs functionality for determining a gameOver.
-//Need to deep clone startingBoard to playingBoard in order to be able to reset the game
-//Bullet point 6 on the project website!!!
+//console.log(startingBoard);
+//console.log(playingBoard);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const board = function() {
-
-//     let startingBoard = [
-//         [3, 2, 1 ],
-//         [],
-//         [],
-//     ];
-    
-//     // let boardState = {
-//     //     numOfMoves: moves
-//     // }
-//     //let moveDisc = Board().moveDisc
-
-//     //console.log(startingBoard);
-    
-//     return startingBoard
-// }
-// //board.moveDisc();
-
-// const map = function(boardArray) {
-//     boardArray.forEach((outerElement) => {
-//         //console.log(outerElement)
-//         outerElement.reverse().forEach((innerElement) => {
-//             console.log(innerElement)
-//         })
-//     })
-    
-//     // let boardPegs = '|'
-//     // for(let i = 1; i < boardArray.length; i++) {
-//     //     boardPegs += '  |';
-//     // }
-//     // console.log(boardPegs)
-// }
-// //map(board());
-
-// const Board = function() {
-//     let moveDisc = (startPeg, endPeg) => {
-//       console.log('hello')
-//     };
-//     console.log('hero')
-//     // if(gameOver) {
-//     //     console.log(boardState.numOfMoves)
-//     // }
-//     return {
-//         moveDisc: moveDisc
-//     }
-// };
-// Board().moveDisc();
