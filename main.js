e7b1508b2014784014a0ccff5b60b5548ba02da9
PJ Foley <pjfoley@gmail.com>
2020-08-16 16:54:36 -0700

    working and stable. making nicer

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
 // We'll want to be able to print the board horizontally. 
 // You MUST utilize a map function at least once to accomplish this part of the assignment.
-let boardState = [   // a basic 3x3 board
+let boardState = [   // a basic 3x3 board 
   ['3', '2', '1'],
   [],
   []
 ];
-let freshBoard = boardState;  // to reset a new game 
 let startingPeg = 1;  // peg we start on (1 in human-readable form, 0 in index)
-
+let winSum;
 var BoardController = function () {
   var startRound = function () {
     // console.log('startRound() called');
     if (!moves){
-      let winSum = 0;
+      winSum = 0;
+      boardState = [   // a basic 3x3 board // is this the best way to reset the game?
+        ['3', '2', '1'],
+        [],
+        []
+      ];
+      
       console.log('Welcome. Here is the game start configuration.');
       console.log(outputBoard(boardState));       
       for (let i = 0; i < boardState.length; i++) {
         winSum = (boardState[i].reduce(function(sum, element){return sum + parseInt(element)}, 0));
-        if (winSum > 0) {
-          console.log('winSum = ' + winSum);
+        if (winSum > 0) {  // assumes valid board, so returns once it gets a good count
+          //console.log('winSum = ' + winSum);
           return winSum;
         }
       }
     } else {
       console.log('Moves taken: ' + moves);
-      console.log(outputBoard(boardState));
+      console.log(outputBoard(boardState));  // need to determine if I'm sending the array to the function, or if it's just getting it itself
     }
    
   }
+
   var outputBoard = function(boardArray) {    // REQs = use MAP at least once **achieved**
     // console.log('outputBoard() called');
     let output = '';  // in scope of boardArray()
@@ -43,7 +49,7 @@ var BoardController = function () {
         output += '\n'; // add a newline at end of peg with discs
       }
       }
-    return output;
+    return output;  
   }
   var moveDisc = function(start, dest) {
     // console.log('calling testMove with start peg ('+start+') returns-->' + testMove(start));
@@ -60,68 +66,81 @@ var BoardController = function () {
     if (testMove(start).includes(dest)) {   // expecting array of valid pegs returned
       let movingDisc = boardState[start - 1].pop();  // store disc from start peg in var
       boardState[dest - 1].push(movingDisc);  // push disc onto board 
-      board.incrementMoves();
-      //console.table(boardState);
-      checkWinner(boardState);
+      incrementMoves();
+      if (checkWinner(boardState)) {
+        outputBoard(boardState); 
+        resetGame();
+        return;
+      }
+      startRound(boardState);
+      return;
     }else{
       console.log('Sorry, that move is not allowed. Rules is rules.');
       // I don't know if this counts as a move, so for now, it doesn't
       // board.incrementMoves();
+      return;
     }
   }
 
   var testMove = function(start) {  // REQs: use FILTER
     let currentDiscSize = boardState[start-1][(boardState[start-1].length)-1];
-    console.log(currentDiscSize + ' : currentDiscSize detected');
-    let response = [];  // test each row one at a time, if it's valid, add to valid array
-    let validMoves = [];  // ultimately return this, where it gets tested by .includes()
-    for (let boardIndex = 0; boardIndex < boardState.length; boardIndex++) {
-      if ( start-1 === boardIndex) { // moving to current peg not valid
-        validMoves.push(0);  // could be '' or anything not a peg number
+    //console.log(currentDiscSize + ' : currentDiscSize detected');
+    let validMoves = [];  // return this filled with valid pegs to move to
+    for (let boardIndex = 0; boardIndex < boardState.length; boardIndex++) {  // loop through main array
+      if ( start-1 === boardIndex) { // moving to current peg not valid, so skip it
+        validMoves.push(0);  // could be '' or anything not a peg number, or skip it :-)
+        /* next line goes into the second dimension of the board, looks at last entry, compares
+           to currentDiscSize. If larger or empty peg, allows and pushes peg to output array */
       } else if (boardState[boardIndex][boardState[boardIndex].length-1] > currentDiscSize || !boardState[boardIndex][boardState[boardIndex].length-1]) {
         validMoves.push(boardIndex+1); // +1 because human readable peg number
-      } else {
+      } else { // peg not allowed due to disc in play being larger than destination disc
         validMoves.push(0);  // could be '' or anything not a peg number
       }
-     
     }
     return validMoves;
   }
+
   var incrementMoves = function() {
-    console.log('incrementMoves() called');
-    console.log('moves made : ' + moves + 1);
+    // console.log('incrementMoves() called');
     return moves++; // easy enough, it seems
   }
+
   var checkWinner = function(boardArray) {
-    console.log('checkWinner() called');  // REQ's = use REDUCE at least once **achieved**
+    //console.log('checkWinner() called');  // REQ's = use REDUCE at least once **achieved**
     for (let i = 0; i < boardArray.length; i++) {  //common loop through elements in array - could be double .map I think
       //console.log('reducing to ' + boardArray[i].reduce(function(sum, element){return sum + element}, 0));
-      if (boardArray[i].reduce(function(sum, element){return sum + parseInt(element)}, 0) === winSum) {
-        if (startingPeg-1 != i) {
-          console.log('WINNER IN ROW ' + [i+1]);
-          resetGame(); 
-        }  else {
-          console.log('Sorry, no win yet.');
-        }
-      }
-        
       
-    // legal moves should already be accounted for, so...
-    // I think we can check just for total number of discs
-    // and see that they are all on one index
-    // AND that index is not the same as the starting peg
-    // for now, we'll cheat with defining the starting peg rather than detecting it
-    // NOTE subtract 1 from peg to equal index of array
-  }
-}
+      /* a repeat of one of the setup functions. Find the sum of all disc sizes 
+        on each peg that is not the starting peg. If the sum === what we had at the
+        beginning of the game, we have winner. */
+      // testing for number, disc size numbers are stored as strings. use parseInt
+      // -1 to turn human-friendly peg to index number
+      //  console.log('testing if' + boardArray[i].reduce(function(sum, element){return sum + parseInt(element)}, 0) + '=' + winSum);
+      if (boardArray[i].reduce(function(sum, element){return sum + parseInt(element)}, 0) === winSum && ((startingPeg-1) != i )) {    
+        console.log('WINNER IN ROW ' + [i+1]);
+          return true;
+        }  else {
+          // console.log('Sorry, no win yet.');  // is this obvious enough?
+        }
+      }  
+      return false;
+    }
+    /* legal moves should already be accounted for, so...
+       I think we can check just for total number of discs
+       and see that they are all on one index
+       AND that index is not the same as the starting peg
+       starting peg defined manually at game start = startingPeg
+       NOTE subtract 1 from peg to equal index of array */
+  
+
   var resetGame = function() {
-    console.log('resetGame() called');
-    boardState = freshBoard;  // return the board to starting configuration
-    moves = 0; // reset the move counter
+    // console.log('resetGame() called');
+    moves = 0; // reset the move counter -- this is the real magic to this function
     winSum = 0; // will be reset by startRound(), but just covering all bases
     startRound();
-    return; // needed?
+    return; 
   }
+  // send all functions in Object to global
   return {
     startRound : startRound,
     outputBoard : outputBoard,
@@ -132,15 +151,10 @@ var BoardController = function () {
     resetGame : resetGame
   }      
 }
-
- 
-
-
     
 var board = BoardController();
 let moves = 0;
-let winSum = board.startRound();  // pull winSum into global scope. Use to test all discs on a peg.
-board.incrementMoves();
+board.startRound();  // set up game first run
 board.moveDisc(1,2); // put test here
 board.moveDisc(1,3);
 board.moveDisc(2,3);
@@ -148,8 +162,8 @@ board.moveDisc(1,2);
 board.moveDisc(3,1);
 board.moveDisc(3,2);
 board.moveDisc(1,2); // put test here
-console.log(board.outputBoard(boardState));
-board.checkWinner(boardState);  // this is going to need to be called by moveDisc()
+//console.log(board.outputBoard(boardState));
+//board.checkWinner(boardState);  // this is going to need to be called by moveDisc()
  
 // now we should be ready for console commands
 
