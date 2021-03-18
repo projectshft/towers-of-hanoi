e3764de9d5f755a31a4690e6d1f5fe4e50ee7ae2
unknown <joshkamrass@gmail.com>
2021-03-18 15:54:22 -0400

    adds generate board

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 const TowersOfHanoiEngine = function () {
+  this.board = [];
 
+  //Creates the intial game board
+  this.generateBoard = function (pegs, discs) {
+    var initialBoard = [];
+    for (i = 0; i < pegs; i++) {
+      initialBoard.push([])
+    }
+    for (i = discs; i >= 1; i--) {
+      initialBoard[0].push(i.toString());
+    }
+    this.board = initialBoard;
+  }
 }
+
+var game = new TowersOfHanoiEngine();
+game.generateBoard(4,9);
+console.log(game.board);
