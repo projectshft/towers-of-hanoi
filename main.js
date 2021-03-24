9f29c6cf01d712960c24b64f2178d4ab58760144
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-03-24 14:57:01 -0400

    board, printBoard fxn, start of moveDisc fxn

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //build game board for towers of hanoi
 //1. board has to be 2D array
 //2. must utilize MAP to print board HORIZONTALLY at least once
+
 //3. player submits moves to game --> function called moveDisc(sp, ep)
   // and updates board if allowed
 //4. an object is responsible for maintaining the board
@@ -11,12 +12,52 @@
   //must use array helper methods except forEach
 
 
+
+
+//var board = towersOfHanoi.board;
+
 var towersOfHanoi = {
-  board: [['5', '4', '3', '2', '1'],
+  board: [
+    [5, 4, 3, 2, 1],
     [],
-    []]
+    []
+  ],
+
+  //prints board to console horizontally
+  printBoard: function() {
+    var gameBoard = this['board'].map(function (pegs) {
+      return console.log('--- ' + pegs.join(' '))  
+    });
+  
+    return gameBoard; 
+  },
+
+  moveDisc: function(startPeg, endPeg) {
+    //adjusts parameters so they can act as indexes
+    startPeg -= 1;
+    endPeg -= 1; 
+    
+    var board = this.board; 
+  
+    if(board[endPeg].length === 0) {
+      board[endPeg].push(board[startPeg].pop());
+  
+    };
+  
+    towersOfHanoi.printBoard();
+  },
+
 };
 
-console.log(towersOfHanoi.board)
+
+
+towersOfHanoi.moveDisc(1, 3);
+
+
+
+
+
+
+  
  
 
