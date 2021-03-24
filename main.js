a96cada20ed8b72c19195f0f8651840480a564d0
Lauren Nicholls <lauren.nicholls1@gmail.com>
2021-03-23 20:28:11 -0400

    Added ability to change number of discs and pegs

--- main.js
+++ main.js
@@ -1,33 +1,46 @@
 var board = {
-  // Initialize starting board for game
-  startingPegBoard: [
-                      [5, 4, 3, 2, 1],
-                      [],
-                      []
-                    ],
-  
-  // Initialize game board
-  currentPegBoard: [
-                    [5, 4, 3, 2, 1],
-                    [],
-                    []
-                   ],
+// Start a game with directions
+  directions: function() {
+    var gameDirections = [`TOWERS OF HANOI`, 
+                      `Directions: Towers of Hanoi is an ancient puzzle played with three stacks and five differently-sized discs. At the start, all the discs are in the leftmost stack, with the largest disc on the bottom and the smallest on the top. The object is to get all the discs, stacked from biggest to smallest over to the right most peg with the following rules:`, 
+                      `- You can only move one disc per move, and you can only move the top disc on a stack.`, 
+                      `- You can't move a larger disc on top of a smaller disc. `, 
+                      `- Discs can only be moved to empty pegs or on top of larger discs.`, 
+                      `- To start the game, type in board.startGame(numPegs, numDiscs) and hit enter. numPegs represents the numbers of pegs you would like the game to have. numDiscs represents how many discs you would like your game to have.`
+                    ];
 
-  // Start a game with directions
-  startGame: function() {
-    console.log(`TOWERS OF HANOI`);
-    console.log(`Directions: Towers of Hanoi is an ancient puzzle played with three stacks and five differently-sized discs. At the start, all the discs are in the leftmost stack, with the largest disc on the bottom and the smallest on the top. The object is to get all the discs, stacked from biggest to smallest over to the right most peg with the following rules:`);
-    console.log(`- You can only move one disc per move, and you can only move the top disc on a stack.`);
-    console.log(`- You can't move a larger disc on top of a smaller disc. `);
-    console.log(`- Discs can only be moved to empty pegs or on top of larger discs.`); 
-    console.log(`- To move a disc, type into the console board.moveDisc(numberOne, numberTwo). numberOne is the peg you want to move from. numberTwo is disc you want to move the peg to. For example, board.moveDisc(1, 2) would move a disc from peg 1 to peg 2. Good luck!`);
-    this.printBoard(this.currentPegBoard);
+    gameDirections.map((direction) => {
+      console.log(direction);
+    });
   },
+
+  // Builds game board based on user input
+  startGame: function(numPegs, numDiscs) {
+    if (numPegs < 3 || numDiscs < 1) {
+      console.log('The number of pegs must be greater than or equal to 3 and the number of discs must be greater than 0! Please try again!');
+      return;
+    };
+
+    var pegBoard = Array(numPegs).fill().map(() => Array()); 
+
+    for (let discs = numDiscs; discs > 0; discs--) {
+      pegBoard[0].push(discs);
+    };
+
+    console.log(`- To move a disc, type into the console board.moveDisc(numberOne, numberTwo). numberOne is the peg you want to move from. numberTwo is disc you want to move the peg to. For example, board.moveDisc(1, 2) would move a disc from peg 1 to peg 2. Good luck!`);
+    this.printBoard(pegBoard);
+    board.startingPegBoard = pegBoard;
+    board.currentPegBoard = JSON.parse(JSON.stringify(pegBoard));
+  },
+
+  // Placeholders for game boards
+  startingPegBoard: [],
+  currentPegBoard: [],
 
   // Moves discs between pegs
   moveDisc: function (startPeg, endPeg) {
     // Checks that startPeg and endPeg are between 1 - 3
-    if (startPeg < 1 || startPeg > 3 || endPeg < 1 || endPeg > 3) {
+    if (startPeg < 1 || startPeg > this.numPegs || endPeg < 1 || endPeg > this.numPegs) {
       console.log('Please enter a startPeg and/or endPeg value between 1 and 3.');
       this.printBoard(this.currentPegBoard); 
       return;
@@ -79,24 +92,24 @@ var board = {
 
   // checks if the player has won
   checkWinner: function(currentPegBoard) {
-    let winningStack = [5, 4, 3, 2, 1];
+    var winningStack = this.startingPegBoard[0];
+    console.log('winningStack: ', winningStack);
 
     // checks if the right most stack has the winning order
     if (winningStack.every(function(element, index) {
       return element === currentPegBoard[currentPegBoard.length - 1][index]; 
       })) {
         console.log('Congratulations! You are a Tower of Hanoi MASTER!!! Play again?');
-        board.currentPegBoard = board.startingPegBoard;
-        this.printBoard(this.currentPegBoard);
+        board.directions();
         return;
     };
 
     // Reprints the board to continue game play
-    if (this.currentPegBoard[1].length !== 5 || this.currentPegBoard[2].length !== 5) {
+    if (this.currentPegBoard[1].length !== winningStack.length || this.currentPegBoard[2].length !== winningStack.length) {
       console.log('That move was successful, board is now:');
       this.printBoard(currentPegBoard);
     };
   }   
 };
 
-board.startGame();
+board.directions();
