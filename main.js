b2ef863d537b5251498f43669268fd01870a60a6
Jeremy Wheless <whelessjw@gmail.com>
2021-03-24 22:13:10 -0400

    completed print board method

--- main.js
+++ main.js
@@ -5,24 +5,25 @@ const gameBoard = [["5", "4", "3", "2", "1"],
 const board = {
   printBoard() {
     gameBoard.map(peg => {
-     if (!peg.includes('---')) peg.unshift('---');
-     console.log(...peg);
-   });
+      console.log('---', ...peg);
+    })
  },
   moveDisk(start, end) {
     const startPeg = gameBoard[start - 1];
     const endPeg = gameBoard[end - 1];
     const movingDisk = startPeg[startPeg.length - 1];
     const destinationDisk = endPeg[endPeg.length - 1];
-console.log(endPeg.length)
-console.log(gameBoard)
+
     if (movingDisk < destinationDisk || endPeg.length === 0) {
       endPeg.push(startPeg.pop());
       console.log(`That move was successful, board is now:`);
       this.printBoard();
     }
 
-    //if (movingDisk < des)
+    if (movingDisk > destinationDisk) {
+      console.log(`You cannot move a larger disc on top of a smaller one, board is still:`);
+      this.printBoard();
+    }
   },
   checkWinner() {
 
@@ -31,6 +32,7 @@ console.log(gameBoard)
 
   }
 };
-
+//board.printBoard();
 board.moveDisk(1, 2);
 board.moveDisk(1, 3);
+board.moveDisk(1, 2);
