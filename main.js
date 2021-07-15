1d4ec0b7960f1ea9c0aac643ace8ff3204c74b15
Benjamin Pryor <bscott23@gmail.com>
2021-07-15 11:50:20 -0400

    add move check and simplfied some

--- main.js
+++ main.js
@@ -2,7 +2,16 @@ var board = [["5", "4", "3", "2", "1"],
 [],
 []]
 var moveDisc = function (start, end) {
-  board[end-1].push(board[start-1][board[start-1].length-1]);
-  board[start-1][board[start-1].pop()];
-  console.log(board); 
+  var startDisc = board[start-1][board[start-1].length-1];
+  var endPosition = board[end-1][board[end-1].length-1];
+  debugger
+  if (endPosition > startDisc || board[end-1].length === 0) {
+    board[end-1].push(startDisc);
+    board[start-1][board[start-1].pop()];
+    console.log('Successful move! Your board now looks like');
+    console.log(board); 
+  } else {
+    console.log('Your move was not vaild, try a different move.');
+    console.log(board);
+  }
 }
