11c9d33e25ca2083ed7650a2b397a208e7c0fd5a
Otto Frankel <ottofrankel@comcast.net>
2021-07-16 14:10:44 -0700

    Wrapped game in an object

--- main.js
+++ main.js
@@ -1,71 +1,74 @@
-var board = []
 
-var newGame = function () {
-  board[0] = ["5", "4", "3", "2", "1"];
-  board[1] = [];
-  board[2] = [];
-  console.log("New game started!");
-  printBoard(board);
-}
+var game = {
+  board: [],
 
-var printBoard = function (arr) {
-  var printArr = arr.map(function (peg) {
-    var pegLine = "---";
-    var newArr = [pegLine].concat(peg);
-    return newArr;
-  })
+  newGame: function () {
+    this.board[0] = ["5", "4", "3", "2", "1"];
+    this.board[1] = [];
+    this.board[2] = [];
+    console.log("New game started!");
+    this.printBoard();
+  },
 
-  console.log(printArr[0].join(' ') + '\n' + printArr[1].join(' ') + '\n' + printArr[2].join(' '));
-}
+  printBoard: function () {
+    var printArr = this.board.map(function (peg) {
+      var pegLine = "---";
+      var newArr = [pegLine].concat(peg);
+      return newArr;
+    })
+  
+    console.log(printArr[0].join(' ') + '\n' + printArr[1].join(' ') + '\n' + printArr[2].join(' '));
+  },
 
-Array.prototype.moveDisc = function (peg1, peg2) {
-  peg1--;
-  peg2--;
-  var sourcePeg = board[peg1];
-  var destPeg = board[peg2];
+  moveDisc: function (peg1, peg2) {
+    peg1--;
+    peg2--;
+    var sourcePeg = this.board[peg1];
+    var destPeg = this.board[peg2];
 
-  if (peg1 < board.length && peg2 < board.length) {
-    
-    var topDisc1 = sourcePeg[sourcePeg.length - 1];
-    var topDisc2 = destPeg[destPeg.length - 1];
+    if (peg1 < this.board.length && peg2 < this.board.length) {
+      
+      var topDisc1 = sourcePeg[sourcePeg.length - 1];
+      var topDisc2 = destPeg[destPeg.length - 1];
 
-    if (topDisc2 > topDisc1 || !topDisc2) {
-      destPeg.push(topDisc1);
-      sourcePeg.pop();
-      console.log("Move successful, board is now: ");
-    } else {
-      console.log("You cannot move a larger disc on top of a smaller one, board is still: ");
-    }
-
-  } else {
-    console.log("There aren't that many pegs!");
-  }
-
-  printBoard(board);
-
-  if (checkWinner(destPeg)) {
-    console.log("********** \n Winner! \n**********");
-    newGame();
-  }
-}
-
-var checkWinner = function (peg) {
-  if (peg != 0 && peg.length == 5) {
-
-    var isWinner = peg.reduce(function (hasWon, curr, index) {
-      if (index > 0) {
-        var previous = peg[index - 1];
-
-        if (curr != previous - 1) {
-          hasWon = false;
-        }
+      if (topDisc2 > topDisc1 || !topDisc2) {
+        destPeg.push(topDisc1);
+        sourcePeg.pop();
+        console.log("Move successful, board is now: ");
+      } else {
+        console.log("You cannot move a larger disc on top of a smaller one, board is still: ");
       }
-      return hasWon;
-    }, true);
+    
+      this.printBoard();
 
-    return isWinner;
+      if (this.checkWinner(destPeg)) {
+        console.log("********** \n Winner! \n**********");
+        this.newGame();
+      }
+      
+    } else {
+      console.log("There aren't that many pegs!");
+    }
+  },
+
+  checkWinner: function (peg) {
+    if (peg != 0 && peg.length == 5) {
+  
+      var isWinner = peg.reduce(function (hasWon, curr, index) {
+        if (index > 0) {
+          var previous = peg[index - 1];
+  
+          if (curr != previous - 1) {
+            hasWon = false;
+          }
+        }
+        return hasWon;
+      }, true);
+  
+      return isWinner;
+    }
+    return false;
   }
-  return false;
 }
 
-newGame();
+game.newGame();
