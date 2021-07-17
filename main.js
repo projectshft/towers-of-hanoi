96cfdac982d11e5039d8c5462dd05b1c1381ea17
Benjamin Pryor <bscott23@gmail.com>
2021-07-17 10:32:17 -0400

    added logBoard function

--- main.js
+++ main.js
@@ -20,20 +20,26 @@ var Board = function () {
   if (endPosition > startDisc || this.towers[end-1].length === 0) {
     this.towers[end-1].push(startDisc);
     this.towers[start-1][this.towers[start-1].pop()];
-    console.log('Successful move! Your this.towers now looks like');
-    console.log('---',this.towers[0].toString(),'\n','---',this.towers[1].toString(),'\n','---',this.towers[2].toString()); 
+    console.log('Successful move! Your towers now looks like');
+    this.logBoard();
     if( this.checkWin()) {
       console.log('You win!');
     }
   } else {
     console.log('Your move was not vaild, try a different move.');
-    console.log('---',this.towers[0].toString(),'\n','---',this.towers[1].toString(),'\n','---',this.towers[2].toString());
+    this.logBoard();
   }
 };
 
 Board.prototype.checkWin = function () {
   return (this.towers[1].length === 5 || this.towers[2].length === 5) 
 }
+
+Board.prototype.logBoard = function () {
+  this.towers.map(function (tower, index) {
+    console.log('---' + board.towers[index].toString());
+  })
+};
 
 var board = new Board;
 
