c771b7bed58929f8330aa41f76c9c127ba1fa611
Philip Krynicki <philipkrynicki@Philips-MacBook-Air.local>
2021-07-17 12:18:47 -0500

    Finished with javascript code

--- main.js
+++ main.js
@@ -1,46 +1,41 @@
 
 
-const initialState = [["5", "4", "3", "2", "1"],
+const initialState = [
+    ["5", "4", "3", "2", "1"],
     [],
     [],
-];  // watch out for off-by-one errors
+];  
 
-
-const board = {
-    gameState: [...initialState],
+const game = {
+    board: [...initialState],
+    displayPegs: () => {
+      console.log("gameboard:");
+      game.board.map((item) => {
+        console.log(item);
+      })
+    },
     moveDisc: (initialPeg, targetPeg) => {
-        // ... all your game logic
-            // top is the last item in the initalPeg
-            // check that the peg in question is smaller than where it's headed
-                // handle null/empty state
-            // if peg is validated, then update the target peg
-          initalPeg -= 1;
+      game.displayPegs()
+        
+          initialPeg -= 1;
           targetPeg -= 1;
 
-          var board = this.board;
-          var begin = board[initialPeg];
-          var end = board[targetPeg];
-
-          // logic for moveDisc to differentiate between legal and illegal moves
-
-
-
+          var start = game.board[initialPeg];  
+          var end = game.board[targetPeg];      
+  
+        if (end.length === 0 || start[start.length - 1] < end[end.length - 1]) {
+          end.push(start.pop());  
+        };
         // after every move, you want to check for a winner
-        this.checkWinner()
+        game.checkWinner()
+        game.displayPegs();
     },
     checkWinner: () => {
         // const winner = ...some logic to check for the winner
-        var win = board.find (function () {
-          return board[1].length === 5 || board[2].length === 5;
-        });
-        if (winner) {
-            console.log("You won!!")
-            // reset to initial state
-          var resetGame = function () {
-            // line 41 has a problem and needs to be re-written
-            return board.gameState;
-          }
-
+        
+        var hasWon = (game.board[1].length === 5 || game.board[2].length === 5);
+        if (hasWon) {
+            console.log("You won!!")            
         }
     }
 }
@@ -57,34 +52,34 @@ const board = {
 
 
 //  Code to use to check for a win in game
-// board.moveDisc(1, 3)
-// board.moveDisc(1, 2)
-// board.moveDisc(3, 2)
-// board.moveDisc(1, 3)
-// board.moveDisc(2, 1)
-// board.moveDisc(2, 3)
-// board.moveDisc(1, 3)
-// board.moveDisc(1, 2)
-// board.moveDisc(3, 2)
-// board.moveDisc(3, 1)
-// board.moveDisc(2, 1)
-// board.moveDisc(3, 2)
-// board.moveDisc(1, 3)
-// board.moveDisc(1, 2)
-// board.moveDisc(3, 2)
-// board.moveDisc(1, 3)
-// board.moveDisc(2, 1)
-// board.moveDisc(2, 3)
-// board.moveDisc(1, 3)
-// board.moveDisc(2, 1)
-// board.moveDisc(3, 2)
-// board.moveDisc(3, 1)
-// board.moveDisc(2, 1)
-// board.moveDisc(2, 3)
-// board.moveDisc(1, 3)
-// board.moveDisc(1, 2)
-// board.moveDisc(3, 2)
-// board.moveDisc(1, 3)
-// board.moveDisc(2, 1)
-// board.moveDisc(2, 3)
-// board.moveDisc(1, 3)
+game.moveDisc(1, 3)
+game.moveDisc(1, 2)
+game.moveDisc(3, 2)
+game.moveDisc(1, 3)
+game.moveDisc(2, 1)
+game.moveDisc(2, 3)
+game.moveDisc(1, 3)
+game.moveDisc(1, 2)
+game.moveDisc(3, 2)
+game.moveDisc(3, 1)
+game.moveDisc(2, 1)
+game.moveDisc(3, 2)
+game.moveDisc(1, 3)
+game.moveDisc(1, 2)
+game.moveDisc(3, 2)
+game.moveDisc(1, 3)
+game.moveDisc(2, 1)
+game.moveDisc(2, 3)
+game.moveDisc(1, 3)
+game.moveDisc(2, 1)
+game.moveDisc(3, 2)
+game.moveDisc(3, 1)
+game.moveDisc(2, 1)
+game.moveDisc(2, 3)
+game.moveDisc(1, 3)
+game.moveDisc(1, 2)
+game.moveDisc(3, 2)
+game.moveDisc(1, 3)
+game.moveDisc(2, 1)
+game.moveDisc(2, 3)
+game.moveDisc(1, 3)
