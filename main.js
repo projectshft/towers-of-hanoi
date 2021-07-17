6b662dff74eb43cdf3d1f6b565b39ece152c86a6
Benjamin Pryor <bscott23@gmail.com>
2021-07-17 11:13:55 -0400

    final before extensions

--- main.js
+++ main.js
@@ -2,18 +2,6 @@ var Board = function () {
   this.towers = [['5','4','3','2','1'],[],[]];
  };
 
-// Board.prototype.newBoard = function (towers, discs) {
-//   var towers = towers;
-//   var discs = discs;
-//   this.towers.map(function (towers, discs) {
-//     this.towers.length = towers;
-//     this.towers.fill([0]);
-//     this.towers[0].length = discs;
-//     console.log(this.towers);
-//     debugger;
-//   }) 
-// };
-
  Board.prototype.moveDisc = function (start, end) {
   var startDisc = this.towers[start-1][this.towers[start-1].length-1];
   var endPosition = this.towers[end-1][this.towers[end-1].length-1];
