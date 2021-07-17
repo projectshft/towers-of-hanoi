1f22be7880a82f9235de85ffaea0b0317ced8268
Benjamin Pryor <bscott23@gmail.com>
2021-07-16 20:40:21 -0400

    configured display board

--- main.js
+++ main.js
@@ -1,68 +1,40 @@
-var Game = function () {
-  this.board = [['5','4','3','2','1'],[],[]];
+var Board = function () {
+  this.towers = [['5','4','3','2','1'],[],[]];
  };
 
-// Game.prototype.newGame = function (towers, discs) {
-//   this.this.board.map(function (towers, discs) {
-//     this.this.board.length = towers;
-//     this.this.board.fill([]);
-//     this.this.board[0].length = discs;
-//     console.log(this.this.board);
+// Board.prototype.newBoard = function (towers, discs) {
+//   var towers = towers;
+//   var discs = discs;
+//   this.towers.map(function (towers, discs) {
+//     this.towers.length = towers;
+//     this.towers.fill([0]);
+//     this.towers[0].length = discs;
+//     console.log(this.towers);
 //     debugger;
 //   }) 
 // };
 
- Game.prototype.moveDisc = function (start, end) {
-  var startDisc = this.board[start-1][this.board[start-1].length-1];
-  var endPosition = this.board[end-1][this.board[end-1].length-1];
-  if (endPosition > startDisc || this.board[end-1].length === 0) {
-    this.board[end-1].push(startDisc);
-    this.board[start-1][this.board[start-1].pop()];
-    console.log('Successful move! Your this.board now looks like');
-    console.log(this.board); 
+ Board.prototype.moveDisc = function (start, end) {
+  var startDisc = this.towers[start-1][this.towers[start-1].length-1];
+  var endPosition = this.towers[end-1][this.towers[end-1].length-1];
+  if (endPosition > startDisc || this.towers[end-1].length === 0) {
+    this.towers[end-1].push(startDisc);
+    this.towers[start-1][this.towers[start-1].pop()];
+    console.log('Successful move! Your this.towers now looks like');
+    console.log('---',this.towers[0].toString(),'\n','---',this.towers[1].toString(),'\n','---',this.towers[2].toString()); 
     if( this.checkWin()) {
       console.log('You win!');
     }
   } else {
     console.log('Your move was not vaild, try a different move.');
-    console.log(this.board);
+    console.log('---',this.towers[0].toString(),'\n','---',this.towers[1].toString(),'\n','---',this.towers[2].toString());
   }
 };
 
-Game.prototype.checkWin = function () {
-  return (this.board[1].length === 5 || this.board[2].length === 5) 
+Board.prototype.checkWin = function () {
+  return (this.towers[1].length === 5 || this.towers[2].length === 5) 
 }
 
-var game = new Game;
+var board = new Board;
 
-game.moveDisc(1, 3)
-game.moveDisc(1, 2)
-game.moveDisc(3, 2)
-game.moveDisc(1, 3)
-game.moveDisc(2, 1)
-game.moveDisc(2, 3)
-game.moveDisc(1, 3)
-game.moveDisc(1, 2)
-game.moveDisc(3, 2)
-game.moveDisc(3, 1)
-game.moveDisc(2, 1)
-game.moveDisc(3, 2)
-game.moveDisc(1, 3)
-game.moveDisc(1, 2)
-game.moveDisc(3, 2)
-game.moveDisc(1, 3)
-game.moveDisc(2, 1)
-game.moveDisc(2, 3)
-game.moveDisc(1, 3)
-game.moveDisc(2, 1)
-game.moveDisc(3, 2)
-game.moveDisc(3, 1)
-game.moveDisc(2, 1)
-game.moveDisc(2, 3)
-game.moveDisc(1, 3)
-game.moveDisc(1, 2)
-game.moveDisc(3, 2)
-game.moveDisc(1, 3)
-game.moveDisc(2, 1)
-game.moveDisc(2, 3)
-game.moveDisc(1, 3)
+board.moveDisc(1,2);
