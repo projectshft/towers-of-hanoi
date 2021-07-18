3c8030f997146710d1ddc0486092166af8cffaeb
Philip Krynicki <philipkrynicki@Philips-MacBook-Air.local>
2021-07-17 23:04:01 -0500

    Explanation added

--- main.js
+++ main.js
@@ -7,25 +7,27 @@ const initialState = [            //  declaring an array of arrays with "initial
 ];                                //  Closing the array of arrays
 
 const game = {                         // declaring an object of game
-    board: [...initialState],          // key of board, the array ...initialState makes a copy of object or  array content.  "Put these in there"
+    board: [...initialState],          // key of board, the array ...initialState makes a copy of an object or an array content.  "Put these in there"
     displayPegs: () => {               //  Create a function called displayPegs, arrow function format
-      console.log("gameboard:");       //  Console log of "gameboard" to see if it's working
-      game.board.map((item) => {       //  game.board.map  -- used to CHANGE TO NEW VALUES.  Item is argument
-        console.log(item);             //  console.log the argument of item
+      console.log("gameboard:");       //  Console log of "gameboard" to indicate what the arrays represent
+      game.board.map((item) => {       //  game.board.map  -- used to CHANGE TO NEW VALUES.  "item" is an argument
+        console.log(item);             //  console.log the argument of item (from game.board.map)
       })
     },
-    moveDisc: (initialPeg, targetPeg) => {    // Create a moveDisk function that takes in the initial and target page
+    moveDisc: (initialPeg, targetPeg) => {    // Create a moveDisk function that takes in the initial and target pegs
       game.displayPegs()                      // Call (from the object of game) the displayPegs function
         
           initialPeg -= 1;                    // Array is 0, 1, 2 but player sees pegs 1, 2, 3
           targetPeg -= 1;                     // Array is 0, 1, 2 but player sees pets 1, 2 ,3
 
-          var start = game.board[initialPeg];  //  declare a variable of start.  It equals the game.board initalPeg
-          var end = game.board[targetPeg];     //  declare a variable of end.  It equals the game.board target.Peg
+          var start = game.board[initialPeg];  //  declare a variable of start.  It equals the game.board initialPeg
+          var end = game.board[targetPeg];     //  declare a variable of end.  It equals the game.board targetPeg
   
         if (end.length === 0 || start[start.length - 1] < end[end.length - 1]) {
           end.push(start.pop());  
-        };
+        };                                     // If the targetPeg array equals 0 or the initialPeg array value is less than the targetPeg value, then do the following code
+                                               // Remove the value (pop) from ititialPeg and "push" value to targetPeg
+        
                                                // after every move, you want to check for a winner
         game.checkWinner()                     // call the game.CheckWinner function
         game.displayPegs();                    // call the game.displayPegs function
@@ -34,7 +36,7 @@ const game = {                         // declaring an object of game
         
         
         var hasWon = (game.board[1].length === 5 || game.board[2].length === 5);
-        //  this is the logic to check if the player has one the game.  The conditions are 5 discs on either  Peg 2 or Peg 3
+        //  this is the logic to check if the player has one the game.  The conditions are 5 discs on either Peg 2 or Peg 3
         if (hasWon) {                           // check to see if var hasWon is true
             console.log("You won!!")            // indicate to the player that they have won 
         }
