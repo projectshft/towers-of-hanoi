be023c83de5b6a3b0e3a2443ae6dd169d2ddcc8b
Stephen Husmann <stephenhusmann@Stephens-MacBook-Pro-3.local>
2021-07-28 10:30:15 -0600

    Smoothing out formatting and easier gameplay

--- main.js
+++ main.js
@@ -6,19 +6,19 @@ var board = {
     this.numMoves = 0;
     this.gameBoard = [];
     if (numPegs < 3) {
-      console.log("The board must have at least 3 pegs, so we defaulted to 3 pegs.");
+      alert("The board must have at least 3 pegs, so we defaulted to 3 pegs.");
       numPegs = 3;
     };
-    if (numPegs > 10) {
-      console.log("The board may not have more than 10 pegs, so we defaulted to 10 pegs.");
-      numPegs = 10;
+    if (numPegs > 9) {
+      alert("The board may not have more than 9 pegs, so we defaulted to 9 pegs.");
+      numPegs = 9;
     };
     if (numDiscs < 1) {
-      console.log("The board must have at least 1 disc, so we defaulted to 1 disc.");
-      numDiscs = 3;
+      alert("The board must have at least 1 disc, so we defaulted to 1 disc.");
+      numDiscs = 1;
     };
     if (numDiscs > 15) {
-      console.log("The board may not have more than 15 discs, so we defaulted to 15 discs.");
+      alert("The board may not have more than 15 discs, so we defaulted to 15 discs.");
       numDiscs = 15;
     };    
     for (var i = 0; i < numPegs; i++) {
@@ -29,7 +29,7 @@ var board = {
     }
     this.winningPeg = [...this.gameBoard[0]];
     this.print();
-    console.log('\nNow you are ready to start playing!\n \nTo make a move, use this format:\n\n   board.move(pegChoice1, pegChoice2);\n\nThis will move the top disc from [pegChoice1] to [pegChoice2], as long as you are not stacking a larger disc on top of a smaller disk!');
+    console.log('\nNow you are ready to start playing! To make a move, use this format:\n\n   move(pegChoice1, pegChoice2);\n\nThis will move the top disc from [pegChoice1] to [pegChoice2], as long as you are not stacking a larger disc on top of a smaller disk!');
   },
   print: function(){
     var pegNum = 0;
@@ -38,11 +38,11 @@ var board = {
       return ('Peg #' + pegNum + ' ---- ') + peg.join(' ');
     });
     if (this.numMoves === 0) {
-      console.log('\nHERE IS YOUR BOARD BEFORE MAKING ANY MOVES:\n\n');
+      console.log('*******************************************\n\nHERE IS YOUR BOARD BEFORE MAKING ANY MOVES:\n\n*******************************************\n\n');
     } else if (this.numMoves === 1) {
-      console.log('\nHERE IS YOUR BOARD AFTER MAKING YOUR FIRST MOVE:\n\n');
+      console.log('************************************************\n\nHERE IS YOUR BOARD AFTER MAKING YOUR FIRST MOVE:\n\n************************************************\n\n');
     } else {
-      console.log('\nHERE IS YOUR BOARD AFTER ' + this.numMoves + ' MOVES:\n\n');
+      console.log('**********************************\n\nHERE IS YOUR BOARD AFTER ' + this.numMoves + ' MOVES:\n\n**********************************\n\n');
     };
     console.log(printBoard.join('\n'));
   },
@@ -69,8 +69,7 @@ var board = {
     var lastDiscOnPeg2 = this.gameBoard[pegChoice2Index][indexOfLastDiscOnPeg2];
     var discThatMoves = this.gameBoard[pegChoice1Index][indexOfLastDiscOnPeg1];
     if(this.gameBoard[pegChoice1Index].length === 0) {
-      console.log('\nInvalid Move: There are no discs on Peg #' + pegChoice1 + '. Try Again\n');
-      this.print();
+      alert('\nInvalid Move: There are no discs on Peg #' + pegChoice1 + '. Please try again\n');
       return;
     };
     if(lastDiscOnPeg2 > discThatMoves || lastDiscOnPeg2 === undefined) {
@@ -79,20 +78,26 @@ var board = {
       this.numMoves++;
       this.print();
       if(this.checkWinner(pegChoice2Index)) {
-        console.log('CONGRATULATIONS! You successfully moved all of the discs to a new peg!');
-        console.log('\n \nTo play again, create another board using this format:\n\n   board.create(numPegs, numDiscs);\n\nYour board may have 3-10 pegs, and 1-15 discs.\n');
+        alert('CONGRATULATIONS! You successfully moved all of the discs to a new peg!');
+        console.log('\n \nTo play again, create another board using this format:\n\n   create(numPegs, numDiscs);\n\nYour board may have 3-10 pegs, and 1-15 discs.\n');
         return;
       } else {
-          console.log('\nNow make another move, using this format:\n\n   board.move(pegChoice1, pegChoice2)');
+          console.log('\nNow make another move, using this format:\n\n   move(pegChoice1, pegChoice2)');
       };
     } else {
-      console.log('\nInvalid Move: You are not allowed to move a disc that is worth ' + discThatMoves + ' on top of a disc that is worth ' + lastDiscOnPeg2 + '. Try Again\n');
-      this.print();
+      alert('\nInvalid Move: You are not allowed to move a disc that is worth ' + discThatMoves + ' on top of a disc that is worth ' + lastDiscOnPeg2 + '. Please try again.\n');
     };
 
   },
 };
 
-//This is where the console game is played
-console.log('Welcome to Towers of Husmann!\n \nTo begin, create a board using this format:\n\n   board.create(numPegs, numDiscs);\n\nYour board may have 3-10 pegs, and 1-15 discs.\n');
-board.create(10,1);
+var move = function (pegChoice1, pegChoice2) {
+  board.move(pegChoice1,pegChoice2);
+}
+
+var create = function (numPegs, numDiscs) {
+  board.create(numPegs, numDiscs);
+}
+
+//Gameplay Begins
+console.log('Welcome to Towers of Husmann!\n \nTo begin, create a board using this format:\n\n   create(numPegs, numDiscs);\n\nYour board may have 3-9 pegs, and 1-15 discs.\n');
