b418c6cb7371fbe3a7b1cee3053582d8aa84a44c
Ian Holmes <iqholmes@gmail.com>
2022-01-21 13:49:51 -0500

    working print board function

--- main.js
+++ main.js
@@ -1,35 +1,12 @@
-var board = {
-  pegs: [
-    [5, 4, 3, 2, 1],
-    [],
-    []
-  ],
-  moveDisc: function (startPeg, endPeg) {
-    startPeg -= 1;
-    endPeg -= 1;
-    if (this.pegs[endPeg] > 0 && this.pegs[endPeg] < this.pegs[startPeg]){
-      console.log(`You cannot move a larger disc on top of a smaller one, board is still: `)
-    } else {
-      startPeg = this.pegs[startPeg].pop();
-      endPeg = this.pegs[endPeg].push(startPeg);
-      printBoard;
-      this.checkWinner();
-    } 
-    var printBoard = this.pegs.map(function (peg){
-      console.log(peg);
-    });    
-  },
-  checkWinner: function() {
-    var startPos = 5;
-    if (this.pegs[2].length === startPos || this.pegs[1].length === startPos){
-      console.log(`winner!`);
-    } else {
-      console.log(`That move was successful, board is now:`)
-    }
-  },
-  
-};
+var board = [["5", "4", "3", "2", "1"],
+[],
+[]]
 
+var printBoard = function() {
+  var boardString = board.map(function(positions){
+    return '--- ' + positions.toString() ;
+  })
+  console.log(boardString.join("\n"))
+}
 
-
-
+printBoard();
