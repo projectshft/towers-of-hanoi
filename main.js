a6004c794608151f85e737360f1e4fa00c9cc85c
Samuel Zimel <samuel.c.zimel@gmail.com>
2022-01-22 16:12:44 -0600

    moving pegs w/ no checks

--- main.js
+++ main.js
@@ -1,51 +1,44 @@
-// A board representing 3 pegs, and 5 discs on the first peg in ascending order.  This is a normal starting position for the game.
-[["5", "4", "3", "2", "1"],
-[],
-[]]
+
+let actualBoard = [
+  [5, 4 ,3 ,2 ,1],
+  [],
+  []
+];
+
+
+const move = function(disc, spot) {
+  if (disc > 3 || disc < 0 || spot > 3 || spot < 0) {
+    return console.log('Not a valid peg');
+  }
+  //Code for counting moves
+  const moveCounter = function() {
+    let moves = 0;
+    moves++;
+    return console.log(`Number of Moves: ${moves}`);
+  }
+
+
+  //Code for cutting the disc from peg
+  let pegDisc = actualBoard[disc - 1];
+  let push = pegDisc.pop();
+  actualBoard[disc -1] = pegDisc;
+
+
+  //Code for putting disc in peg
+  let pegSpot = actualBoard[spot - 1];
+  pegSpot.push(push);
+  actualBoard[spot -1] = pegSpot;
+
+  console.log(actualBoard)
+  moveCounter();
 
 
   
-//Board setup 
-const board = []
-const row = 3
-const col = 5
-for(i = 0; i < row; i++) {
-  board[i] = [];
-  for(j = 0; j < col; j++) {
-    board[i][j] = [null];
-  }
-};
-
-//board pieces
-const pieces = {
-  A: 5,
-  B: 4,
-  C: 3,
-  D: 2,
-  E: 1
 }
 
-//Board start setup
-function start(idk) {
-  board[0][0] = pieces.A;
-  board[0][1] = pieces.B;
-  board[0][2] = pieces.C;
-  board[0][3] = pieces.D;
-  board[0][4] = pieces.E;
-}
-start();
-console.log(board)
 
-//need to have a function that has (piece, position) that selects the piece and moves it to a new spot. 
 
-function move(piece, position_x, position_y) {
 
-  //Checks for a correct piece input
-  var check = pieces.hasOwnProperty(piece)
-  if(!check) {
-    console.log('Not a valid game piece.');
-  }
 
-  
-}
-move('F', null, null);
+
+
