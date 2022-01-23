cb61bc5e43b1dbb90fc1dcc6755fda86e20474fa
Michael Grumbine <mgrumbine3@gmail.com>
2022-01-23 15:13:02 -0800

    finished moveIsLegal

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ var board = [
 
 var rowIndicator = '-';
 
+var boardState = {
+  playing: true,
+  illegalMove: ''
+};
+
 
 var printBoard = function () {
   var rowsAsStrings = board.map(function (row) {
@@ -20,17 +25,37 @@ var printBoard = function () {
   console.log(fullString);
 };
 
+var moveIsLegal = function (firstPeg, lastPeg) {
+  var startPegDiscs = board[firstPeg - 1];
+  var endPegDiscs = board[lastPeg - 1];
+  var topDisc = startPegDiscs[startPegDiscs.length - 1]
+  var bottomDisc = endPegDiscs[0];
+  var rowHasDiscs = startPegDiscs.length;
+  var acceptableDiscSize = topDisc < bottomDisc || bottomDisc === undefined;
+  if (!rowHasDiscs) {
+    boardState.illegalMove = 'There are no discs to move on that peg!';
+  }
+  if (!acceptableDiscSize) {
+    boardState.illegalMove = 'You cannot place a larger disc on top of a smaller.';
+  }
+  
+  return rowHasDiscs && acceptableDiscSize;
+};
+
+
+
 var moveDisc = function (startPeg, endPeg) {
   // note: the pegs are not the same as the index: so peg 1 is index 0 in its row
   var startPegDiscs = board[startPeg - 1];
   var endPegDiscs = board[endPeg - 1];
 
-  if (moveIsLegal(startPeg, endPeg) {
+  if (moveIsLegal(startPeg, endPeg)) {
     endPegDiscs.push(startPegDiscs.pop())
     console.log('That move was successful!');
     printBoard();
   } else {
-    console.log(`You cannot move a larger disc onto a smaller one.\nBoard remains unchanged.`)
+    console.log(boardState.illegalMove);
+    console.log(`Board remains unchanged.`)
     printBoard();
   }
 };
