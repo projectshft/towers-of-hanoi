19017a5a08f1ec84106062284129beb071a195c0
Dio <cdebon97@outlook.com>
2022-01-22 22:38:54 -0600

    Base game complete except for starting board display

--- main.js
+++ main.js
@@ -3,31 +3,66 @@ var boardArray = [["5", "4", "3", "2", "1"],
 []];
 
 
-var printBoard = boardArray.map(function (element) {
-  return "--- " + element;
-});
-
-function display(params) {
-  console.log(printBoard[0].replaceAll(","," "),`\n${printBoard[1].replaceAll(","," ")}`,`\n${printBoard[2].replaceAll(","," ")}`);
-} 
-
-
 var board = {
   moveDisc: function (fromPeg, toPeg) {
     var startPeg = boardArray[fromPeg - 1];
     var endPeg = boardArray[toPeg - 1];
-    var travelDisc = startPeg.find(function (disc) {
-      return startPeg.indexOf(disc) + 1 == startPeg.length; 
-    });
-    endPeg.push(travelDisc);
-    startPeg.pop(travelDisc);
+    var lowerDisc;
+
+    //Creating two new variables to helpe me log the updated board to the console.
+    var newBoard;
+    var printBoard;
     
-    var printBoard = boardArray.map(function (element) {
+    //Finding the disc the user wants to move.
+    var travelDisc = startPeg.find(function (disc) {
+      return startPeg.indexOf(disc) + 1 == startPeg.length;
+    });
+
+    //Determining if a disc already exists on destination peg and what size disc it is.
+    if (endPeg.length == 0) {
+      lowerDisc = null;
+    } else {
+      lowerDisc = endPeg.find(function (disc) {
+        return endPeg.indexOf(disc) + 1 == endPeg.length;
+      })
+    };
+
+    if (lowerDisc == null || travelDisc < lowerDisc) {
+      endPeg.push(travelDisc);
+      startPeg.pop(travelDisc);
+      console.log('That move was successful. Board is now:')
+    } else {
+      console.log('You cannot move a larger disc on top of a smaller one, board is still:')
+    }
+    newBoard = boardArray.map(function (element) {
       return "--- " + element;
     });
-    console.log(printBoard[0].replaceAll(","," "),`\n${printBoard[1].replaceAll(","," ")}`,`\n${printBoard[2].replaceAll(","," ")}`);
-    //console.log(printBoard[0],`\n${printBoard[1]}`,`\n${printBoard[2]}`);
-  }
+    printBoard = `${newBoard[0].replaceAll(","," ")}\n${newBoard[1].replaceAll(","," ")}\n${newBoard[2].replaceAll(","," ")}`;
+    console.log(printBoard);
+    this.checkWinner;
+  },
+  checkWinner: function () {
+    if (boardArray[0].length) {
+      if (boardArray[1].length == 5 || boardArray[2].length == 5) {
+        console.log('Congratulations! You win!!')
+      }
+    }
+
+  },
 };
 
-board.moveDisc(1,2);
+board.moveDisc(1, 2);
+board.moveDisc(1, 3);
+board.moveDisc(2, 3);
+board.moveDisc(1, 2);
+board.moveDisc(3, 1);
+board.moveDisc(3, 2);
+board.moveDisc(1, 2);
+board.moveDisc(1, 3);
+board.moveDisc(1, 3);
+board.moveDisc(1, 3);
+board.moveDisc(1, 2);
+board.moveDisc(3, 2);
+board.moveDisc(3, 2);
+board.moveDisc(3, 2);
+board.moveDisc(1, 3);
