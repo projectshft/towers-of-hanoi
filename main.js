202fd41d0c4991decee8497975dda6c78a947ab5
Emily Freeman <freeman.ecf@gmail.com>
2022-01-26 19:52:39 -0500

    create printBoard func

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let board = {
   []
 ],
 
-  moveDisc: function(start, end) {
+  moveDisc: function (start, end) {
    let startPeg = this.gameBoard[start - 1];
    let endPeg = this.gameBoard[end - 1];
    let lastDiscOnStartPeg = startPeg[startPeg.length - 1];
@@ -13,10 +13,26 @@ let board = {
    
    if (lastDiscOnStartPeg > lastDiscOnEndPeg) {
      console.log('You cannot move a larger disc on top of a smaller one, board is still:');
-     printBoard();
+     this.printBoard();
    } else {
      endPeg.push(lastDiscOnStartPeg);
      startPeg.pop(lastDiscOnStartPeg);
+     console.log('That move was successful, board is now:')
+     this.printBoard()
    }
   },
+ 
+  printBoard: function () {
+    let rows = function(arr) {
+    let string = arr.join(' ');
+    return ` --- ${string}`;
+  };
+    let endBoard = this.gameBoard.map(function(arr) {
+    return rows(arr);
+  });
+
+  console.log(endBoard[0]);
+  console.log(endBoard[1]);
+  console.log(endBoard[2]);
+}
 }
