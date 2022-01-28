42f0820dad9f1dc389c77ac5aaecc2bb13c552fc
Emily Freeman <freeman.ecf@gmail.com>
2022-01-28 18:01:45 -0500

    can change amount of pegs and discs

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-let originalBoard = [
+const originalBoard = [
   ['5', '4', '3', '2','1'],
   [],
   []
 ];
 
-let board = {
+const board = {
   gameBoard: [
-  ['5', '4', '3', '2','1'],
   [],
-  []
-],
+  ['5', '4', '3', '2'],
+  ['1']
+  ],
 
   moveDisc: function (start, end) {
-   let startPeg = this.gameBoard[start - 1];
-   let endPeg = this.gameBoard[end - 1];
-   let lastDiscOnStartPeg = startPeg[startPeg.length - 1];
-   let lastDiscOnEndPeg = endPeg[endPeg.length - 1];
+   const startPeg = this.gameBoard[start - 1];
+   const endPeg = this.gameBoard[end - 1];
+   const lastDiscOnStartPeg = startPeg[startPeg.length - 1];
+   const lastDiscOnEndPeg = endPeg[endPeg.length - 1];
    
    if (lastDiscOnStartPeg > lastDiscOnEndPeg) {
      console.log('You cannot move a larger disc on top of a smaller one, board is still:');
@@ -23,7 +23,6 @@ let board = {
    } else {
      endPeg.push(lastDiscOnStartPeg);
      startPeg.pop(lastDiscOnStartPeg);
-     
      console.log('That move was successful, board is now:');
      this.printBoard();
      this.checkWinner();
@@ -31,11 +30,11 @@ let board = {
   },
  
   printBoard: function () {
-    let rows = function(arr) {
-    let string = arr.join(' ');
+    const rows = function(arr) {
+    const string = arr.join(' ');
     return ` --- ${string}`;
   };
-    let endBoard = this.gameBoard.map(function(arr) {
+    const endBoard = this.gameBoard.map(function(arr) {
     return rows(arr);
   });
     endBoard.forEach(function(arr) {
@@ -43,12 +42,11 @@ let board = {
     })
   },
  
-
   checkWinner: function () {
-    let winningPeg = ['5', '4', '3', '2', '1'].toString();
-    let peg1 = this.gameBoard[0].toString();
-    let gameBoardString = this.gameBoard.toString();
-    let win = gameBoardString.includes(winningPeg);
+    const winningPeg = ['5', '4', '3', '2', '1'].toString();
+    const peg1 = this.gameBoard[0].toString();
+    const gameBoardString = this.gameBoard.toString();
+    const win = gameBoardString.includes(winningPeg);
 
     if (win && !peg1) {
       console.log('You Won!!!!');
@@ -58,5 +56,18 @@ let board = {
 
   resetGame: function () {
   this.gameBoard = originalBoard;
+  },
+
+  amountOfPegs: function (pegs) {
+    for (let i = 0; i < pegs - 3; i++) {
+      this.gameBoard.push([]);
+    }
+  },
+
+  amountOfDiscs: function (discs) {
+    this.gameBoard[0] = [];
+    for (let i = discs; i > 0; i--) {
+      this.gameBoard[0].push(i.toString());
+    }
   }
 }
