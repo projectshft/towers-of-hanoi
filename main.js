c756a1b89bd0893013a80e7c99977cae6654fb40
sbistransin <48774173+sbistransin@users.noreply.github.com>
2022-01-28 10:48:33 -0500

    convert ToH from function to class

--- main.js
+++ main.js
@@ -1,121 +1,127 @@
 
 // class that defines number of pegs and discs for board
-var TowersOfHanoi = function(pegs = 3, discs = 5) {
-
-  var boardArr = createBoard(pegs, discs);
-  // private attributes only accessible with getter/setter
-  // and by moveDisc
-  var boardAttributes = {
-    numberOfDiscs : discs,
-    numberOfPegs : pegs,
-    //boardArr : [['5', '4', '3', '2', '1'], [], []]
-    boardArr : boardArr
-  }
-
-  // create initial board from pegs and discs
-  function createBoard(pegs, discs) {
-    var boardArr = [];
+class TowersOfHanoi {
   
-    // create pegs
-    for (let i = 0; i < pegs; i++) {
-      boardArr.push([]);
-    }
-  
-    // add discs to first peg
-    for (let i = discs; i > 0; i--) {
-      boardArr[0].push(i);
-    }
-      return boardArr;
-  }
+  constructor(pegs = 3, discs = 5) {
+    var boardArr = createBoard(pegs, discs);
+    
+    this.boardAttributes = {
+      numberOfDiscs : discs,
+      numberOfPegs : pegs,
+      //default boardArr : [['5', '4', '3', '2', '1'], [], []]
+      boardArr : boardArr
+    };
 
+    this.moveDisc = moveDisc;
+    this.printBoard = printBoard;
+    this.checkWinner = checkWinner;
+    this.resetGame = resetGame;
+    this.getAttribute = getAttribute;
+    this.setAttribute = setAttribute;
 
-  // moves disc from pegs if a valid move
-  function moveDisc(src, dest) {
-    var sourceArr = boardAttributes.boardArr[src - 1];
-    var destArr = boardAttributes.boardArr[dest - 1];
-    // assuming we never pick from an empty peg...
-    var sourceDisc = sourceArr[sourceArr.length - 1];
-    var destLength = destArr.length;
-
-    // move without testing if no discs of peg
-    if (destLength === 0) {
-      // move 
-      var disc = sourceArr.pop();
-      destArr.push(disc);
-      console.log("That move was successful, board is now:")
-    } else {
-      var lastDisc = destArr[destLength - 1];
-
-      if (lastDisc < sourceDisc){
-        // don't move
-        console.log("You cannot move a larger disc on top of a smaller one, board is still:");
-      } else {
-        // move 
-        var disc = sourceArr.pop();
-        destArr.push(disc);
-        console.log("That move was successful, board is now:");
-      }
-    }
-
-    printBoard();
-    if (checkWinner()){
-      console.log("You win!!");
-      resetGame();
-    } 
-  };
-
-  // function that prints board Array to console
-  function printBoard() {
-    boardAttributes.boardArr.map(function(peg) {
-  
-      var strPeg = peg.reduce(function(accumulator, peg) {
-        return accumulator + peg + " "; 
-      }, "--- " )
-      console.log(strPeg);
-      return peg;
-    })
-  };
-
-  // function that checks if game has been won
-  function checkWinner() {
-    var noneInFirstPeg = boardAttributes.boardArr[0].length === 0;
-    var allInOnePeg = boardAttributes.boardArr.some(function(peg) {
-      return peg.length == boardAttributes.numberOfDiscs;
-    });
-  
-    return noneInFirstPeg && allInOnePeg;
-  }
-  
-  // resets board back to initial setup
-  function resetGame() {
-    var newBoard = createBoard(boardAttributes.numberOfPegs, boardAttributes.numberOfDiscs); 
-    setAttribute('boardArr', newBoard);
-    console.log('Game reset. Play first move:')
-    printBoard();    
-  }
-
-  function getAttribute(attribute) {
-    if (boardAttributes.hasOwnProperty(attribute)){
-      return boardAttributes[attribute];
-    } 
-  };
-
-  function setAttribute(attribute, value) {
-    if (boardAttributes.hasOwnProperty(attribute)){
-      return boardAttributes[attribute] = value;
-    } 
-  };
-
-  return { 
-    moveDisc: moveDisc,
-    getAttribute: getAttribute,
-    setAttribute: setAttribute
   }
 }
 
+// create initial board from pegs and discs
+var createBoard = function(pegs, discs) {
+  var boardArr = [];
 
+  // create pegs
+  for (let i = 0; i < pegs; i++) {
+    boardArr.push([]);
+  }
+
+  // add discs to first peg
+  for (let i = discs; i > 0; i--) {
+    boardArr[0].push(i);
+  }
+    return boardArr;
+}
+
+// moves disc from pegs if a valid move
+var moveDisc = function(src, dest) {
+  var sourceArr = this.boardAttributes.boardArr[src - 1];
+  var destArr = this.boardAttributes.boardArr[dest - 1];
+  // assuming we never pick from an empty peg...
+  var sourceDisc = sourceArr[sourceArr.length - 1];
+  var destLength = destArr.length;
+
+  // move without testing if no discs of peg
+  if (destLength === 0) {
+    // move 
+    var disc = sourceArr.pop();
+    destArr.push(disc);
+    console.log("That move was successful, board is now:")
+  } else {
+    var lastDisc = destArr[destLength - 1];
+
+    if (lastDisc < sourceDisc){
+      // don't move
+      console.log("You cannot move a larger disc on top of a smaller one, board is still:");
+    } else {
+      // move 
+      var disc = sourceArr.pop();
+      destArr.push(disc);
+      console.log("That move was successful, board is now:");
+    }
+  }
+
+  this.printBoard();
+
+  if (this.checkWinner()){
+    console.log("You win!!");
+    this.resetGame();
+  } 
+};
+
+
+// function that prints board Array to console
+var printBoard = function() {
+  this.boardAttributes.boardArr.map(function(peg) {
+
+    var strPeg = peg.reduce(function(accumulator, peg) {
+      return accumulator + peg + " "; 
+    }, "--- " )
+    console.log(strPeg);
+    return peg;
+  })
+};
+
+// function that checks if game has been won
+var checkWinner = function() {
+
+  var noneInFirstPeg = this.boardAttributes.boardArr[0].length === 0;
+  var allInOnePeg = this.boardAttributes.boardArr.some((peg) => {
+    return peg.length == this.boardAttributes.numberOfDiscs;
+  });
+
+  return noneInFirstPeg && allInOnePeg;
+}
+
+// resets board back to initial setup
+var resetGame = function() {
+  var newBoard = createBoard(this.boardAttributes.numberOfPegs, this.boardAttributes.numberOfDiscs); 
+  this.setAttribute('boardArr', newBoard);
+  console.log('Game reset. Play first move:')
+  this.printBoard();    
+}
+
+var getAttribute = function(attribute) {
+  if (this.boardAttributes.hasOwnProperty(attribute)){
+    return this.boardAttributes[attribute];
+  } 
+};
+
+var setAttribute = function(attribute, value) {
+  if (this.boardAttributes.hasOwnProperty(attribute)){
+    return this.boardAttributes[attribute] = value;
+  } 
+};
+
+
+/*
+TESTS
 var board = new TowersOfHanoi(3, 5);
-// console.log(board.getAttribute('boardArr'));
 
 
 // how to win
@@ -160,6 +166,7 @@ board.moveDisc(1,2);
 // board.moveDisc(3,2);
 // board.moveDisc(1,2);
 // board.moveDisc(1,2);
+*/
 
 
 
