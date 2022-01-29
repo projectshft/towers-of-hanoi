e060c3351a0e1d52e01af3b5a38f6c4ad4430f6b
Emily Freeman <freeman.ecf@gmail.com>
2022-01-29 13:08:41 -0500

    fix checkWinner again

--- jasmine-standalone-4.0.0/src/main.js
+++ jasmine-standalone-4.0.0/src/main.js
@@ -16,8 +16,8 @@ const board = {
   amountOfPegs: function (pegs) {
     for (let i = 0; i < pegs - 3; i++) {
       this.gameBoard.push([]);
-  }
-},
+    }
+  },
 
   amountOfDiscs: function (discs) {
     this.gameBoard[0] = [];
@@ -25,8 +25,8 @@ const board = {
     for (let i = discs; i > 0; i--) {
       this.gameBoard[0].push(i.toString());
       winPeg.push(i.toString());
-  }
-},
+    }
+  },
 
   moveDisc: function (start, end) {
     const startPeg = this.gameBoard[start - 1];
@@ -59,25 +59,24 @@ const board = {
     endBoard.forEach(function(arr) {
       console.log(arr);
     })
-
      /* This is how I printed the board without using forEach, but I couldn't figure out how to make the game flexible (able to change # of discs and pegs) without using forEach
     console.log(endBoard[0]);
     console.log(endBoard[1]);
     console.log(endBoard[2]);
     */
-
   },
  
   checkWinner: function () {
     const winningPeg = winPeg.toString();
-    console.log(winningPeg);
     const peg1 = this.gameBoard[0].toString();
-    const gameBoardString = this.gameBoard.toString();
-    const win = gameBoardString.includes(winningPeg);
-    if (win && !peg1) {
+    this.gameBoard.forEach(function (arr) {
+      let eachString = arr.toString();
+      let win = eachString.includes(winningPeg);
+      if (win && !peg1) {
       console.log('You Won!!!!');
       this.resetGame();
-    }
+      }
+    })
   },
 
   resetGame: function () {
