6ae5cb56401f8e70ca69c2533b7c47fc472c7c69
HK Nelson <hknelson8@gmail.com>
2022-02-01 08:44:18 -0500

    refactor with printBoard method & remove null from pegs

--- main.js
+++ main.js
@@ -1,38 +1,34 @@
 const board = {
 
   pegs: [
-    null,
-    ["5", "4", "3", "2"],
+    ["5", "4", "3", "2", "1"],
     [],    
-    ["1"]
+    []
   ],
+
+  printBoard (str) {
+    console.log(str + '\n' + this.pegs.map(item => {
+      return `--- ${item.join("")}\n`;
+      }).join(""));
+  },
   
   moveDisc (startPeg, endPeg) {
+    startPeg = startPeg - 1;
+    endPeg = endPeg - 1;
   
     // LOG entered peg does not exist (invalid input)
     if (this.pegs.length - 1 < startPeg || this.pegs.length - 1 < endPeg) {
   
-      console.log('Peg(s) not in existence, board is still:\n' + 
-                  this.pegs.map(item => {
-                    if (item !== null) {
-                      return `--- ${item.join(" ")}\n`;
-                    }
-                  }).join(""));
+      this.printBoard('Peg(s) not in existence, board is still:');
   
       return;
   
-      // LOG starting peg is empty (invalid input)
+    // LOG starting peg is empty (invalid input)
     } else if (this.pegs[startPeg].length === 0) {
       
-      console.log('Your starting peg is empty, board is still:\n' + 
-                  this.pegs.map(item => {
-                    if (item !== null) {
-                      return `--- ${item.join(" ")}\n`;
-                    }
-                  }).join(""));
+      this.printBoard('Your starting peg is empty, board is still:');
   
       return;
-      
     }
   
     const disc = this.pegs[startPeg].pop();
@@ -42,50 +38,32 @@ const board = {
 
       this.pegs[startPeg].push(disc);
   
-      console.log('You can\'t move a larger disc on top of a smaller one, the board is still:\n' + 
-                  this.pegs.map(item => {
-                    if (item !== null) {
-                      return `--- ${item.join(" ")}\n`;
-                    }
-                  }).join(""));
-  
+      this.printBoard('You can\'t move a larger disc on top of a smaller one, the board is still:');
+
       return;
   
-      // LOG move was successful
+    // LOG move was successful
     } else if (this.pegs[startPeg].length >= 0) {
       
       this.pegs[endPeg].push(disc);
       
-      console.log('That move was successful, the board is now:\n' + 
-              this.pegs.map(item => {
-                if (item !== null) {
-                  return `--- ${item.join(" ")}\n`;
-                }
-              }).join(""));
+      this.printBoard('That move was successful, the board is now:');
 
       this.checkWinner();
   
       return this.pegs;
-      
     }
   },
 
   startRound () {
 
-    this.pegs[0] = null;
-    this.pegs[1] = ["5", "4", "3", "2", "1"];
+    this.pegs[0] = ["5", "4", "3", "2", "1"];
+    this.pegs[1] = [];
     this.pegs[2] = [];
-    this.pegs[3] = [];
 
-    console.log('Let\'s play! The board is:\n' +
-                this.pegs.map(item => {
-                  if (item !== null) {
-                    return `--- ${item.join(" ")}\n`;
-                  }
-                }).join(""));
+    this.printBoard('Let\'s play! The board is:')
 
     return this.pegs;
-    
   },
 
   checkWinner () {
@@ -96,7 +74,7 @@ const board = {
       return array[element].join("");
     };
 
-    if (checkOrder(this.pegs, 2) === winningOrder || checkOrder(this.pegs, 3) === winningOrder) {
+    if (checkOrder(this.pegs, 1) === winningOrder || checkOrder(this.pegs, 2) === winningOrder) {
       console.log('YOU WIN! Resetting for a new game...');
       setTimeout(() => {console.log('.')}, 2500);
       setTimeout(() => {console.log('.')}, 3000);
@@ -112,33 +90,33 @@ const board = {
 board.startRound();
 
 board.moveDisc(1, 2);
-board.moveDisc(1, 3);
-board.moveDisc(2, 3);
 board.moveDisc(1, 2);
-board.moveDisc(3, 1);
-board.moveDisc(3, 2);
-board.moveDisc(1, 2);
-board.moveDisc(1, 3);
-board.moveDisc(2, 3);
-board.moveDisc(2, 1);
-board.moveDisc(3, 1);
-board.moveDisc(2, 3);
-board.moveDisc(1, 2);
-board.moveDisc(1, 3);
-board.moveDisc(2, 3);
-board.moveDisc(1, 2);
-board.moveDisc(3, 1);
-board.moveDisc(3, 2);
-board.moveDisc(1, 2);
-board.moveDisc(3, 1);
-board.moveDisc(2, 3);
-board.moveDisc(2, 1);
-board.moveDisc(3, 1);
-board.moveDisc(3, 2);
-board.moveDisc(1, 2); 
-board.moveDisc(1, 3);
-board.moveDisc(2, 3);
-board.moveDisc(1, 2);
-board.moveDisc(3, 1);
-board.moveDisc(3, 2);
-board.moveDisc(1, 2);
+// board.moveDisc(2, 3);
+// board.moveDisc(1, 2);
+// board.moveDisc(3, 1);
+// board.moveDisc(3, 2);
+// board.moveDisc(1, 2);
+// board.moveDisc(1, 3);
+// board.moveDisc(2, 3);
+// board.moveDisc(2, 1);
+// board.moveDisc(3, 1);
+// board.moveDisc(2, 3);
+// board.moveDisc(1, 2);
+// board.moveDisc(1, 3);
+// board.moveDisc(2, 3);
+// board.moveDisc(1, 2);
+// board.moveDisc(3, 1);
+// board.moveDisc(3, 2);
+// board.moveDisc(1, 2);
+// board.moveDisc(3, 1);
+// board.moveDisc(2, 3);
+// board.moveDisc(2, 1);
+// board.moveDisc(3, 1);
+// board.moveDisc(3, 2);
+// board.moveDisc(1, 2); 
+// board.moveDisc(1, 3);
+// board.moveDisc(2, 3);
+// board.moveDisc(1, 2);
+// board.moveDisc(3, 1);
+// board.moveDisc(3, 2);
+// board.moveDisc(1, 2);
