968c4fe403b5186b1f9ec6ac2ffc29eea8719b7d
drkstn <86159096+drkstn@users.noreply.github.com>
2022-04-23 15:13:59 -0700

    initial gameboard and console display

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('Towers of Hanoi')
+// starting board
+const board = [
+  [5, 4, 3, 2, 1], 
+  [], 
+  []
+];
+
+// console display for gameboard
+// --- 5 4 3 2 1
+// ---
+// ---
+
+const display = array => {
+  return array
+    .map(peg => `--- ${peg.join(' ')}`)
+    .join('\n')
+}
+
+console.log(display(board))
