60d12effd29ab0d3cfa27e8c5d6a59e77357d30b
drkstn <86159096+drkstn@users.noreply.github.com>
2022-04-24 07:10:41 -0700

    add win condition and illegal move constraint

--- main.js
+++ main.js
@@ -13,10 +13,17 @@ const display = array => {
 
 const currentState = () => game.board.at(-1);
 
-const moveUpdate = () => game.moves = game.board.length - 1;
+const updateMoves = () => game.moves = game.board.length - 1;
 
 const nextState = (from, to) => {
   const disc = currentState()[from - 1].at(-1);
+
+  if (disc > currentState()[to - 1].at(-1)) {
+    console.log('ILLEGAL MOVE\nCannot move larger disc on top of smaller one.\nBoard remains:')
+    return currentState()
+  }
+
+  console.log(`SUCCESSFUL MOVE\nMoved disc ${disc} from peg ${from} to peg ${to}.\nBoard is now:`)
   return currentState().map((peg, index) => {
     return index === (from - 1) ? peg.slice(0, -1) :
            index === (to - 1)   ? peg.concat(disc) :
@@ -24,12 +31,23 @@ const nextState = (from, to) => {
   });
 }
 
+const checkWinner = array => {
+  return array.some((value, index) => {
+    return value.at(-1) === 1 && (value.length === 5) && index > 0
+  })
+}
+
 const moveDisc = (fromPeg, toPeg) => {
   const stateUpdate = nextState(fromPeg, toPeg);
 
-  game.board.push(stateUpdate);
-  moveUpdate();
-  console.log(`Moved disc from peg ${fromPeg} to peg ${toPeg}`)
+  if (stateUpdate !== currentState()) {
+    game.board.push(stateUpdate);
+    updateMoves();
+  }
+
+  if (checkWinner(currentState())) {
+    console.log(`YOU WIN!\nIt took you ${game.moves} moves.`);
+  }
 
   return currentState()
 }
@@ -38,11 +56,37 @@ console.log(display(game.board[0]))
 console.log(display(moveDisc(1, 2)));
 console.log(display(moveDisc(1, 3)));
 console.log(display(moveDisc(2, 3)));
-// console.log(display(moveDisc(1, 2)));
-// console.log(display(moveDisc(3, 2)));
-// console.log(display(moveDisc(3, 1)));
-// console.log(display(moveDisc(2, 3)));
-// console.log(display(moveDisc(1, 2)));
-// console.log(display(moveDisc(3, 2)));
-console.log(game.board)
+console.log(display(moveDisc(1, 2)));
+console.log(display(moveDisc(3, 2)));
+console.log(display(moveDisc(3, 1)));
+console.log(display(moveDisc(2, 3)));
+console.log(display(moveDisc(1, 2)));
+console.log(display(moveDisc(3, 2)));
+console.log(display(moveDisc(1, 3)));
+console.log(display(moveDisc(2, 3)));
+console.log(display(moveDisc(2, 1)));
+console.log(display(moveDisc(3, 1)));
+console.log(display(moveDisc(2, 3)));
+console.log(display(moveDisc(1, 2)));
+console.log(display(moveDisc(1, 3)));
+console.log(display(moveDisc(2, 3)));
+console.log(display(moveDisc(1, 2)));
+console.log(display(moveDisc(3, 1)));
+console.log(display(moveDisc(3, 2)));
+console.log(display(moveDisc(1, 2)));
+console.log(display(moveDisc(3, 1)));
+console.log(display(moveDisc(2, 3)));
+console.log(display(moveDisc(2, 1)));
+console.log(display(moveDisc(3, 1)));
+console.log(display(moveDisc(3, 2)));
+console.log(display(moveDisc(1, 3)));
+console.log(display(moveDisc(1, 2)));
+console.log(display(moveDisc(3, 1)));
+console.log(display(moveDisc(2, 3)));
+console.log(display(moveDisc(1, 3)));
+console.log(display(moveDisc(1, 2)));
+console.log(display(moveDisc(3, 1)));
+console.log(display(moveDisc(3, 2)));
+console.log(display(moveDisc(1, 2)));
+
 console.log(game.moves)
