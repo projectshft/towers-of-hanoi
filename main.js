c4beb27335f6fb22d364835c3237d3211c2b03ac
arcox80 <drew.r.cox@gmail.com>
2022-04-30 19:49:08 -0400

    Added and completed moveDisc function

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const board = [[5, 4, 3, 2, 1], [], []];
+const state = {
+  board: [[5, 4, 3, 2, 1], [], []]
+};
+
+//const board = [[5, 4, 3, 2, 1], [], []];
+
 const printBoard = function (boardArr) {
   return boardArr.forEach(function (peg) {
     if (peg.length === 0) {
@@ -9,4 +14,15 @@ const printBoard = function (boardArr) {
   });
 };
 
-printBoard(board);
+const moveDisc = function (startingPeg, endingPeg) {
+  const discStart = state.board[startingPeg - 1];
+  const discEnd = state.board[endingPeg - 1];
+  if (discStart[discStart.length - 1] > discEnd[discEnd.length - 1]) {
+    console.log('You cannot move a larger disc on top of a smaller one, board is still:');
+  } else {
+    discEnd.push(discStart.pop());
+  }
+  return printBoard(state.board);
+};
+
+printBoard(state.board);
