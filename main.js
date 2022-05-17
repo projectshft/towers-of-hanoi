62164bdb52c9eebfbfa9e7a8cc4f6444bb11270d
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-05-17 17:29:37 -0400

    can move one from another

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-var board = [
-  ['5', '4', '3', '2,', '1'],
+let board = [
+  ['5', '4', '3', '2','1'],
   [],
   []
 ];
+
+let firstPeg = board[0];
+let secondPeg = board[1];
+let thirdPeg = board[2];
+
+let move = firstPeg.pop();
+secondPeg.push(move);     
+
+firstPeg.splice()
+board.map(x => {
+  if (x.length < 1) {
+    console.log('---')
+  } else {
+    console.log(`---${x}`)
+  }
+})
