73dbcf799f1fceb01bf50837964df9272f2b0744
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-05-18 08:07:19 -0400

    created board boardState object

---
+++ main.js
@@ -0,0 +1,31 @@
+const boardState = {
+  pegOne: [5, 4, 3, 2, 1],
+  pegTwo: [],
+  pegThree: []
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//tester: no effects / returns a boolean
+
+//mover – affects state of board object
+
+//array creator 
+
+//console log
