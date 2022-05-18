c435c51159da2c9f3cd764ba022709336eb26afc
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-05-18 08:57:11 -0400

    added argument converter logic to mover

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const boardState = {
-  pegOne: [5, 4, 3, 2, 1],
-  pegTwo: [],
-  pegThree: []
+  pegOne: [1],
+  pegTwo: [2],
+  pegThree: [3]
 }
 
 const one = boardState.pegOne;
@@ -31,8 +31,42 @@ const tester = (from, to) => {
 }
 
 
+//create argument conv
+
 //mover – affects state of board object
 
 //array creator 
 
 //console log
+
+
+// const move = () => {
+  // if (from === 1) {
+  //   from = one;
+  // } else if (from === 2) {
+  //   from = two
+  // } else if (from === 3) {
+  //   from = three;
+  // } else {
+  //   console.log('Arguments must equal 1, 2, or 3.')
+  // }
+
+  // if (to === 1) {
+  //   to = one;
+  // } else if (to === 2) {
+  //   to = two
+  // } else if (to === 3) {
+  //   to = three;
+  // } else {
+  //   console.log('Arguments must equal 1, 2, or 3.')
+  // }
+
+  // tester(from, to);
+
+//   tester;
+
+//   //mover
+
+//   array creator 
+
+// }
