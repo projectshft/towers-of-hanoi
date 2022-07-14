9b122f63615d10a56940c8c6315796c017dba21a
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-14 16:03:27 -0400

    set up instructions

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+
+
 const boardState = [[5, 4, 3, 2, 1], [],[]]
 
 
 
+ /*
+
+ 1) object that will hold all of the methods, called gameMethods
+ 2) a function that pops the last index of an item from an array
+ 3) a function that places an index on the back of an array
+ 4) a function that controls with arrays withing boardState execute #s 2 & 3
+ 5) a function that renders the board state to the console
+ 6) a function that renders the game instructions to the console
+ 7) a function that checks whether a move was legal and chooses to rerender  the move
+
  
+ function for moving pegs
+ function for checking for valid moves
+ function for checking winner checkWinner
+ functino for resetting the game
+
+ */
