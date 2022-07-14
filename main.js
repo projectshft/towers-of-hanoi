3b654ea2e337e1521de5a1e2dd55dab7713ac278
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-14 16:21:02 -0400

    step 3 complete

--- main.js
+++ main.js
@@ -5,16 +5,21 @@ testArray = [1, 2, 3]
 const gameMethods = {
   removeTopDisc (array) {
     array.pop()
+  },
+  addDiscToTop (array, property) {
+    array.push(property);
   }
 }
 
 
 
+console.log(testArray)
+
  /*
 
  $$$ 1) object that will hold all of the methods, called gameMethods
  $$$ 2) a function that pops the last index of an item from an array
- 3) a function that places an index on the back of an array
+ $$$ 3) a function that places an index on the back of an array
  4) a function that controls with arrays withing boardState execute #s 2 & 3
  5) a function that renders the board state to the console
  6) a function that renders the game instructions to the console
