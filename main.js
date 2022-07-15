b65acd8764ffb8a13a62dcdaf3796b6dabff7ad6
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-14 21:08:45 -0400

    checked off number 5

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ console.log(gameMethods.renderBoardState())
  $$$ 2) a function that pops the last index of an item from an array
  $$$ 3) a function that places an index on the back of an array
  $$$ 4) a function that controls which arrays within boardState execute #s 2 & 3
- 5) a function that renders the board state to the console
+ $$$ 5) a function that renders the board state to the console
  6) a function that renders the game instructions to the console
  7) a function that checks whether a move was legal and chooses to rerender  the move
   a) whether the peg has a place to move
