3040494cb8d6a499fc328fd1fdc58de690d27299
nathanjpak <nathan.j.pak@gmail.com>
2022-07-23 03:06:33 -0700

    add function to change number of pegs and discs

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ var board = {
                     //clear board
                     this.state.forEach(function(peg) {
                         while (peg[peg.length-1]) {
-                            peg.pop(peg[peg.length-1]);
+                            peg.pop();
                         }
                     });
                     //"clone" starting peg onto the first peg of the board state
@@ -43,9 +43,9 @@ var board = {
             };
 
 var moveDisc = function(pos1, pos2) {
-    var startingPeg = board.state[pos1-1];
+    var startPeg = board.state[pos1-1];
     var targetPeg = board.state[pos2-1];
-    var disc = startingPeg[startingPeg.length - 1];
+    var disc = startPeg[startPeg.length - 1];
 
     if (!disc) {
         console.log("Move was unsuccessful. There's no disc there!");
@@ -53,11 +53,33 @@ var moveDisc = function(pos1, pos2) {
         console.log("Move was unsuccessful. You can't move a disc onto a smaller disc.")
     } else {
         targetPeg.push(disc);
-        startingPeg.pop(disc);
+        startPeg.pop(disc);
     }
 
     board.printBoard();
     board.checkWinner();
+}
+
+//let's plan to use an html form to limit user input
+var setupGame = function(discs, pegs) {
+    if (discs > board.startingPeg.length) {
+        for (i=board.startingPeg.length+1; i <= discs; i++) {
+            board.startingPeg.unshift(i);
+        }
+    } else if (discs < board.startingPeg.length) {
+        while (board.startingPeg.length > discs) {
+            board.startingPeg.shift();
+        }
+    }
+    
+    while (board.state.length < pegs) {
+        board.state.push([]);
+    }
+    while (board.state.length > pegs) {
+        board.state.pop();
+    }
+
+    board.startGame();
 }
 
 board.startGame();
