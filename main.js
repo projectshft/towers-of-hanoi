b5671c62ffa1eea6e600aa9f784b89def14d7690
Rangel Rosado <rangelrosado@rangels-mbp.attlocal.net>
2022-07-29 20:12:03 -0500

    Move function is done. Need to figure out how to start the board so there are place holders in the empty pegs

--- main.js
+++ main.js
@@ -3,29 +3,38 @@
 var game = {
 board: [
 [5, 4, 3, 2, 1],
-[3,1],
-[5,7]
+[null],
+[null]
 ],
 move: function(from, to) {
   var fromDisc = this.board[from - 1][this.board[from - 1].length - 1];
   var toDisc = this.board[to - 1][this.board[to - 1].length - 1];
- console.log(fromDisc)
- console.log(toDisc)
+  if(fromDisc > toDisc) {
+    console.log('You Cannot move a larger disc on top of a smaller one, board is still:');
+    this.boardStatus();
+  } else if(fromDisc < toDisc) {
+    this.board[to - 1].push(fromDisc);
+    this.board[from - 1].pop();
+    console.log('That move was successful, board is now:');
+    this.boardStatus()
+  }
+},
+  boardStatus: function() {
+    var pegOne = game.board[0].map(function(disc) {
+      return disc;
+     });
+     var pegTwo = game.board[1].map(function(disc) {
+       return disc;
+      });
+      var pegThree = game.board[2].map(function(disc) {
+       return disc;
+      });
+      console.log('--- ' + pegOne.join(' '));
+      console.log('--- ' + pegTwo.join(' '));
+      console.log('--- ' + pegThree.join(' '));
+  },
+  start: function() {
+    console.log('Welcome to the Towers of Hanoi game! the board looks like:');
+    this.boardStatus();
+  }
 }
-}
-
-// var fromDisc = game.board[0][game.board[0].length - 1];
-
-var pegOne = game.board[0].map(function(disc) {
- return disc;
-});
-var pegTwo = game.board[1].map(function(disc) {
-  return disc;
- });
- var pegThree = game.board[2].map(function(disc) {
-  return disc;
- });
-
-console.log('--- ' + pegOne.join(' '));
-console.log('--- ' + pegTwo.join(' '));
-console.log('--- ' + pegThree.join(' '));
