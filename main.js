b760042019086e4e879504fac1e2a551df1a62f0
Ryan <ryancaimano@gmail.com>
2022-08-01 00:51:31 -0500

    formatted main.js with eslint enabled

--- main.js
+++ main.js
@@ -23,61 +23,61 @@
 //test dozens of moveDisc pairs
 
 var reset = function () {
-  return [[5, 4, 3, 2, 1], [], []];
+	return [[5, 4, 3, 2, 1], [], []];
 };
 
 var board = reset();
 
 var checkWinner = function () {
-  if (board[0].length == 0 && board[1].length == 0) {
-    console.log(
-      "You have conquered the Towers of Hanoi, and hereby are declared a winner. According to Brahmin priests, the world will end now. But if remain in existence, try the game again and see if you can win by using no more than 31 moves."
-    );
-    board = reset();
-    printUpdate();
-    return true;
-  } else {
-    return false;
-  }
+	if (board[0].length == 0 && board[1].length == 0) {
+		console.log(
+			'You have conquered the Towers of Hanoi, and hereby are declared a winner. According to Brahmin priests, the world will end now. But if remain in existence, try the game again and see if you can win by using no more than 31 moves.'
+		);
+		board = reset();
+		printUpdate();
+		return true;
+	} else {
+		return false;
+	}
 };
 
 var printUpdate = function () {
-  board.map(function (peg) {
-    let line = "--- ";
-    peg.forEach(function (disc) {
-      line += disc + " ";
-    });
-    return console.log(line);
-  });
+	board.map(function (peg) {
+		let line = '--- ';
+		peg.forEach(function (disc) {
+			line += disc + ' ';
+		});
+		return console.log(line);
+	});
 };
 printUpdate();
 
 var getTopDisc = function (peg) {
-  var pegLength = board[peg - 1].length;
-  return board[peg - 1][pegLength - 1];
+	var pegLength = board[peg - 1].length;
+	return board[peg - 1][pegLength - 1];
 };
 
 var moveDisc = function (fromPeg, toPeg) {
-  //arguments must be in the interval I= [1,3]
-  if (fromPeg <= 0 || toPeg <= 0 || fromPeg > 3 || toPeg > 3) {
-    console.log("Please choose peg numbers between 1 and 3, inclusive");
-    return printUpdate();
-  }
+	//arguments must be in the interval I= [1,3]
+	if (fromPeg <= 0 || toPeg <= 0 || fromPeg > 3 || toPeg > 3) {
+		console.log('Please choose peg numbers between 1 and 3, inclusive');
+		return printUpdate();
+	}
 
-  if (
-    getTopDisc(toPeg) === undefined ||
+	if (
+		getTopDisc(toPeg) === undefined ||
     getTopDisc(fromPeg) < getTopDisc(toPeg)
-  ) {
-    let disc = board[fromPeg - 1].pop();
-    board[toPeg - 1].push(disc);
-    console.log("That move was successful, board is now: ");
-  } else {
-    console.log(
-      "You cannot move a larger disc on top of a smaller one, board is still: "
-    );
-  }
+	) {
+		let disc = board[fromPeg - 1].pop();
+		board[toPeg - 1].push(disc);
+		console.log('That move was successful, board is now: ');
+	} else {
+		console.log(
+			'You cannot move a larger disc on top of a smaller one, board is still: '
+		);
+	}
 
-  printUpdate();
-  checkWinner();
-  return;
+	printUpdate();
+	checkWinner();
+	return;
 };
