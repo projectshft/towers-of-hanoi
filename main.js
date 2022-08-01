feeb2e16ea9aff568774341fc2c67cf9707d187c
Justin Johnson <jumejohn@live.com>
2022-08-01 14:05:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 //initial 2d Array of the 'board'
 var Game = function(board){
   this.board = board;
+  this.discs = discs;
   };
   this.board = [[1, 2, 3, 4, 5], [], []];
   var moveCount = 0;
+  var discs = 5;
+  var toTower = null;
   
   //create a horizontal display of the current board
   Game.prototype.displayBoard = function(){
@@ -33,6 +36,7 @@ var Game = function(board){
     var fromValue = this.board[moveFrom -1][fromIndex];
     var toIndex = (this.board[moveTo -1].length -1);
     var toValue = this.board[moveTo -1][toIndex];
+    toTower = moveTo -1;
       if(fromValue === undefined){
         console.log(`That is not a valid move. Please move one of the discs to a new tower.`);
         return this.displayBoard();
@@ -42,8 +46,8 @@ var Game = function(board){
         return this.displayBoard();
       } else {
         this.board[moveTo -1].push(this.board[moveFrom -1].pop());
-        this.moveCount++;
-        console.log(`current number of moves: ${this.moveCount}`);
+        moveCount++;
+        console.log(`current number of moves: ${moveCount}`);
         this.winCheck();
         console.log(`That move was successful. Here is what the board looks like now:`);
         return this.displayBoard();
@@ -51,20 +55,57 @@ var Game = function(board){
   };
       
   Game.prototype.winCheck = function(){
-    if(this.board[1].length === 5 || this.board[2].length === 5)
-        {this.displayBoard();
-        console.log(`current number of moves: ${this.moveCount}`)
-        alert `You have won the game in ${this.moveCount} moves! Way to go! Game will now reset.`;
-        return this.resetGame()
-        };
-      };
-     
+    if(this.board[toTower].length === this.discs && toTower !== 0) {
+            this.displayBoard();
+            console.log(`current number of moves: ${this.moveCount}`)
+            alert `You have won the game in ${this.moveCount} moves! Way to go! Game will now reset.`;
+            return this.resetGame()
+            };
+    };
+       
   Game.prototype.resetGame = function(){
-    this.board = [[1, 2, 3, 4, 5], [], []];
+   this.changeTowers();
+   this.changeDiscs();
     this.moveCount = 0;
     console.log(`current number of moves: ${this.moveCount}`);
     return this.displayBoard();
   };
+  
+  Game.prototype.changeTowers = function(userInputTowers){
+  userInputTowers = prompt `Please enter the number of towers you would like to play with:`
+  while(this.board.length < userInputTowers){
+    this.board.push([]);
+    if(this.board.length === userInputTowers){
+      break;
+    }
+  };
+  while(this.board.length > userInputTowers){
+    this.board.pop();
+    if(this.board.length === userInputTowers){
+      break;
+    }
+  }
+  return this.displayBoard();
+  };
+  
+  Game.prototype.changeDiscs = function(userInputDiscs){
+    userInputDiscs = prompt `Please enter the number of discs you would like to play with:`
+    while(this.board[0].length < userInputDiscs){
+      this.board[0].push(this.board[0].length +1);
+      if(this.board[0].length === userInputDiscs){
+        break;
+      }
+    };
+    while(this.board[0].length > userInputDiscs){
+      this.board[0].shift();
+      if(this.board[0].length === userInputDiscs){
+        break;
+      }
+    }
+    this.discs = userInputDiscs; 
+    console.log('this.discs' + this.discs);
+    return this.displayBoard();
+  }
   
   var myGame = new Game(board);
   
