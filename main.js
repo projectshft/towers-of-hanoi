caa6eb88642af139e37f4bbfc022cc4a48fe63e5
unknown <dgoodenough@proton.me>
2022-10-22 18:41:40 -0700

    Pseudo code phase 1

--- main.js
+++ main.js
@@ -22,3 +22,54 @@
   // There should be a checkWinner function
    // You win the game by putting all the discs back in the original order but on a new peg.
   //  Once a player wins, the game should automatically end by announcing the winner (through a console log) and reset for a new game.
+
+  let gameboard = [
+      [5, 4, 3, 2, 1],
+      [],
+      []
+    ];
+  
+  function displayBoard(board) {
+    //accepts the current state of the board and logs it in a readable form
+    //map the gameboard to something like this:
+      //----array1
+      //----array2
+      //----array3
+  }
+
+  function isMoveValid(from, to, board) {
+    //accepts the move that was made and the current state of the board.
+    //return true/false
+
+    //make sure that the value of the last item in the array that is coming FROM is smaller than the array its going TO
+      //if true return true
+      //else return false
+  }
+
+  function resetBoard() {
+    //sets the gameboard back to the initial value
+  }
+
+  function checkWinner(board) {
+    //if won
+      //console.log("Yay you win");
+      //resetBoard();
+    //else do nothing? return false?
+  }
+
+  function moveDisk(from, to) {
+    if(isMoveValid(from, to, board)){
+      //console.log any important messages ie success
+      updateBoard(from, to, board);
+      checkWinner(board);
+    } else {
+      console.log("Invalid Move");
+      displayBoard(board)
+    }
+  }
+
+//constuctor function to put it all into and make instances of the game
+
+function NewGameToH () {
+  
+}
