eb23f89b3edb0b045307f06e8642577d2c97cab1
Jack Mullinkosson <jackmullinkosson@Jacks-MacBook-Pro.local>
2022-10-26 15:03:22 -0500

    this should be the final version"

--- main.js
+++ main.js
@@ -4,18 +4,13 @@ var board = [
     [],
 ]
 
-
-
-
-
-
 function moveDisc(disc, peg){
 if(isNaN(peg) || peg<1 || peg>3){
     console.log('Peg must be a value between 1-3.')
     logGame()
     return
 }
-if(isNaN(disc) || disc<1 || disc>6){
+else if(isNaN(disc) || disc<1 || disc>5){
     console.log('Disc must be a value between 1-5.')
     logGame()
     return
@@ -38,18 +33,21 @@ discArr=board[discArrIndex]
 }
 findDiscArr()
 if (disc !== discArr[discArr.length-1]){
-    console.log('you can only move the last ring on each peg')
+    console.log('You can only move the last ring on each peg.')
+    logGame()
     return
 }
 if(topDisc===undefined || topDisc>disc){
     pegArr.push(disc)
     discArr.splice(discArr.length-1,1)
+    console.log('Succesful move. The board is now:')
     logGame()
+    checkWinner()
     return
 }
 
 else if(topDisc<=disc){
-    console.log('You cannot move a larger disc on top of a smaller one, board is still:')
+    console.log('You cannot move a larger disc on top of a smaller one.')
     logGame()
     return
 }
@@ -65,7 +63,17 @@ function logGame(){
 var viz = board.map((a)=>{
     return '---' + a
 })
-console.log(viz)
+console.table(viz)
 }
+function checkWinner(){
+    if(board[2].length === 5 || board[1].length === 4){
+        alert('You won! Omg so good! Wow..')
+        location.reload()
+    }
+}
+
+
+console.log('Welcome to the Towers of Hanoi. Use the moveDisc(disc, peg) function to play. Good luck.. ')
+logGame();
 
 
