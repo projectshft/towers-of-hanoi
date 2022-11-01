8d10bef13100657400076574784c013db6199e41
Nils Landsberg <nils.landsberg78@gmail.com>
2022-11-01 17:04:46 -0400

    updated variable names

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var board = [
 var stringBoard = board.map(element => {
   return "--- " + element.join(' ');
 });
-
+console.log(stringBoard);
 
 var showBoard = function() {
   return stringBoard.join("\n")
@@ -23,9 +23,9 @@ var currentBoard = {
 
 function moveDisc(disc, peg) {
   // locate peg in array (index of board)
-  var pegToMove = board[peg - 1]; 
+  var destinationPeg = board[peg - 1]; 
   // identify the current disc on top of the peg
-  var topDiscOnPeg = pegToMove[pegToMove.length - 1]
+  var topDiscOnPeg = destinationPeg[destinationPeg.length - 1]
   // check if disc is larger than the current disc at the top of the peg - if true, log error
   if (disc > topDiscOnPeg) {
     console.log("You cannot move a larger disc on top of a smaller one, board is still:\n" + showBoard());
@@ -40,15 +40,15 @@ function moveDisc(disc, peg) {
         currentPeg.pop();
       } 
     }
-  } 
-  // push disc to pegToMove
+     // push disc to pegToMove
   pegToMove.push(disc);
   console.log("That move was successful, board is now:\n" + showBoard());
+  } 
 }
-console.log(board);
-console.log(showBoard());
-console.log(moveDisc(1, 3));
 
+console.log(moveDisc(1, 3));
+console.log(moveDisc(2, 2));
+console.log(board);
 
 // function checkWinneer(board){
 //   // check board to see if [5, 4, 3, 2, 1] is on peg 2 or 3
