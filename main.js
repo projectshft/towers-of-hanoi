f3213c0bc28ded08d4ca3231218701b590db3b0a
Nils Landsberg <nils.landsberg78@gmail.com>
2022-11-01 15:12:13 -0400

    moveDisc function works. Board array is modified correctly. However, showBoard does not reflect the change

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 var board = [
   [5, 4, 3, 2, 1],
   [],
-  [1]
+  []
 ];
-
-
 
 // we want to be able to print the board horizontally.  You MUST use a map function at least once to accomplish this part of the assignment.
 var stringBoard = board.map(element => {
   return "--- " + element.join(' ');
-})
+});
 
 
 var showBoard = function() {
@@ -19,33 +17,37 @@ var showBoard = function() {
 var currentBoard = {
   pegOne: board[0],
   pegTwo: board[1],
-  pegThree: board[2]
+  pegThree: board[2],
+  hasWinner: false
 }
 
 function moveDisc(disc, peg) {
-
+  // locate peg in array (index of board)
   var pegToMove = board[peg - 1]; 
+  // identify the current disc on top of the peg
   var topDiscOnPeg = pegToMove[pegToMove.length - 1]
   // check if disc is larger than the current disc at the top of the peg - if true, log error
   if (disc > topDiscOnPeg) {
     console.log("You cannot move a larger disc on top of a smaller one, board is still:\n" + showBoard());
-   
-  // } else {
-    // push disc to pegToMove
-  //   pegToMove.push(disc);
-    // remove disc from peg that has its value
-
-    // console.log "That move was successful, board is now: " + printBoard
-
-  }
- 
+  } else {
+    // find disc on board
+    for (var i = 0; i < board.length; i++) {
+      var currentPeg = board[i];
+      var topDisc = currentPeg[currentPeg.length - 1];
+      // if disc is equal to top disc on peg
+      if (disc === topDisc) {
+      // remove 
+        currentPeg.pop();
+      } 
+    }
+  } 
+  // push disc to pegToMove
+  pegToMove.push(disc);
+  console.log("That move was successful, board is now:\n" + showBoard());
 }
-
-console.log(moveDisc(3, 3))
-
-//   // otherwise, push disc to peg array and remove it from the array it was originally located
-//   // console.log "That move was successful, board is now:" + show board
-// }
+console.log(board);
+console.log(showBoard());
+console.log(moveDisc(1, 3));
 
 
 // function checkWinneer(board){
