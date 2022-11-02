c02e27da1515510b905ab1bf2ecdd05bbe92bec1
Nils Landsberg <nils.landsberg78@gmail.com>
2022-11-01 22:33:17 -0400

    SOLVED

--- main.js
+++ main.js
@@ -5,19 +5,17 @@ var board = [
 ];
 
 // we want to be able to print the board horizontally.  You MUST use a map function at least once to accomplish this part of the assignment.
-var stringBoard = board.map(element => {
-  return "--- " + element.join(' ');
-});
-console.log(stringBoard);
+function stringBoard(board) {
+  return board.map(row => {
+    return "--- " + row.join(' ');
+  });
+}
 
-var showBoard = function() {
-  return stringBoard.join("\n")
+function showBoard() {
+  return stringBoard(board).join("\n")
 }
 
 var currentBoard = {
-  pegOne: board[0],
-  pegTwo: board[1],
-  pegThree: board[2],
   hasWinner: false
 }
 
@@ -25,31 +23,39 @@ function moveDisc(disc, peg) {
   // locate peg in array (index of board)
   var destinationPeg = board[peg - 1]; 
   // identify the current disc on top of the peg
-  var topDiscOnPeg = destinationPeg[destinationPeg.length - 1]
+  var lastDiscOnDestinationPeg = destinationPeg[destinationPeg.length - 1]
   // check if disc is larger than the current disc at the top of the peg - if true, log error
-  if (disc > topDiscOnPeg) {
+  if (disc > lastDiscOnDestinationPeg) {
     console.log("You cannot move a larger disc on top of a smaller one, board is still:\n" + showBoard());
   } else {
     // find disc on board
     for (var i = 0; i < board.length; i++) {
       var currentPeg = board[i];
       var topDisc = currentPeg[currentPeg.length - 1];
-      // if disc is equal to top disc on peg
-      if (disc === topDisc) {
-      // remove 
-        currentPeg.pop();
-      } 
+      // if disc is not equal to top disc on peg
+      if (disc === topDisc) { 
+        // remove disc/topDisc from current
+        // push disc to destinationPeg
+        destinationPeg.push(currentPeg.pop());
+        console.log("That move was successful, board is now:\n" + showBoard());
+        break;
+      } else {
+        console.log("You cannot move that disc.  It is not at the end of the peg.")
+        break;
+      }
     }
-     // push disc to pegToMove
-  pegToMove.push(disc);
-  console.log("That move was successful, board is now:\n" + showBoard());
   } 
 }
 
-console.log(moveDisc(1, 3));
-console.log(moveDisc(2, 2));
-console.log(board);
 
-// function checkWinneer(board){
+
+
+function checkWinner() {
 //   // check board to see if [5, 4, 3, 2, 1] is on peg 2 or 3
-// }
+  if (board[1].length === 5 || board[2].length === 5) {
+    currentBoard.hasWinner = true;
+    console.log("Victory!");
+  } else {
+    console.log("The game has not been won, keep trying!")
+  }
+}
