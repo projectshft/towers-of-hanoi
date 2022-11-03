716b111df484375e30930bce995795fa76b330e5
Johnny Wright <johnnywrightiv@gmail.com>
2022-11-03 14:23:09 -0500

    create starter variables & funcs

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('ready to play?')
+var moveOutcome = '[undefinded]';
+
+// Have to use .map() at least once to set up board?
+var board = {
+  peg1: [5, 4, 3, 2, 1],
+  peg2: ['-','-','-','-','-'],
+  peg3: ['-','-','-','-','-'],
+};
+
+var moveDisc = function (dic, peg) {}; // use helper methods, not for loops
+
+var checkWinner = function () {}; // if true, announce (clog) winner and reset game
+
+console.log(`That move was ${moveOutcome}. Board is now:`)
+// console.log(board);
+console.log(board.peg1);
+console.log(board.peg2);
+console.log(board.peg3);
+
+console.log(moveDisc(1, 2)); // test function
