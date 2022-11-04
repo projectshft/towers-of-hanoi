b27e23dbfc5f0fb99acb6ccf2da182faa3317176
Johnny Wright <johnnywrightiv@gmail.com>
2022-11-04 13:03:50 -0500

    create gameState object & move vars inside

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-var moveOutcome = '[undefinded]';
+// object responsible for maintaining the state of the board
+var gameState = {
+  moveOutcome: '<empty>',
 
-// Have to use .map() at least once to set up board?
-var board = {
-  peg1: [5, 4, 3, 2, 1],
-  peg2: ['-','-','-','-','-'],
-  peg3: ['-','-','-','-','-'],
+  // Have to use .map() at least once to set up board?
+  board: [
+    ['---', 5, 4, 3, 2, 1],
+    ['---'  ],
+    ['---'],
+  ],
+  
+  moveDisc: function (disc, peg) {}, // a way to move discs from one peg to another (use array helper methods, not for loops when iterating on arrays)
+  
+  checkWinner: function () {}, // check if player won game, if true, announce (clog) winner and reset game
 };
 
-var moveDisc = function (dic, peg) {}; // use helper methods, not for loops
 
-var checkWinner = function () {}; // if true, announce (clog) winner and reset game
 
-console.log(`That move was ${moveOutcome}. Board is now:`)
-// console.log(board);
-console.log(board.peg1);
-console.log(board.peg2);
-console.log(board.peg3);
+console.log(`That move was ${gameState.moveOutcome}. Board is now:`)
+// console.log(gameState.board);
+console.log(gameState.board[0]); // peg1
+console.log(gameState.board[1]); // peg2
+console.log(gameState.board[2]); // peg3
 
-console.log(moveDisc(1, 2)); // test function
+console.log(gameState.moveDisc(1, 2)); // test function
