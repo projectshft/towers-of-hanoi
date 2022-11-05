44a5079a17e59692966c7147a98376e723b9b560
Johnny Wright <johnnywrightiv@gmail.com>
2022-11-05 12:47:59 -0700

    Fix moveDisc logic error

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // object responsible for maintaining the state of the board
 var gameState = {
   moveOutcome: '',
+  errorMsg: '',
 
   // Have to use .map() at least once to set up board?
   board: [
@@ -9,40 +10,44 @@ var gameState = {
     ['---'],
   ],
   
+  testBoard: [
+    ['---', 5, 4],
+    ['---', 3,],
+    ['---', 2, 1],
+  ],
+  
   // move discs from one peg to another (use array helper methods, not for loops)
   moveDisc: function (disc, peg) {
     // check for valid move
-    gameState.board[peg].forEach(function (element) {
+    gameState.board[peg - 1].forEach(function (element) {
       // -- valid move --
       if (typeof(element) === 'number') {
         gameState.moveOutcome = 'valid';
-        if (peg < element) {
-          // -- valid move --
-            // SOME COME FOR MOVING DISC HERE...
+        if (element > disc) {  // if true, add element to array and remove first index
+          gameState.board[peg - 1].push(disc);
+          gameState.board[peg - 1].shift();
           } else { // -- invalid move, disc can't go on top of bigger disc --
             gameState.moveOutcome = 'invalid';
-            console.log('sorry, can\'t move disc here');
-          }
+            gameState.errorMsg = 'Disc can\'t go on top of smaller disc.';
+          };
           
-      } else { // -- invalid move, disc can't go on base of peg --
+      } else { // -- invalid move, disc can't go on base of peg (type mismatch) --
         gameState.moveOutcome = 'invalid';
-        console.log('sorry, can\'t move disc here');
-      }
+        gameState.errorMsg = 'Disc can\'t go on base of peg (type mismatch).';
+      };
       
     });
-    console.log(`That move was ${gameState.moveOutcome}. Board is now:`)
+    console.log(`That move was ${gameState.moveOutcome}. ${this.errorMsg} Board is now:`);
     gameState.moveOutcome = '';
+    // console.log(gameState.board);
+    console.log(gameState.board[0]); // peg1
+    console.log(gameState.board[1]); // peg2
+    console.log(gameState.board[2]); // peg3
   },
   
   // check if player won game, if true, announce (clog) winner and reset game
   checkWinner: function () {},
 };
-
-
-// console.log(gameState.board);
-console.log(gameState.board[0]); // peg1
-console.log(gameState.board[1]); // peg2
-console.log(gameState.board[2]); // peg3
 
  // test function
 console.log("Function Test: ", gameState.moveDisc(1, 2));
