7ed0a4f799b2a898cbf7048b867fa41cacecfa0e
Johnny Wright <johnnywrightiv@gmail.com>
2022-11-09 15:08:46 -0600

    Fix moveDisc conditionals and clean up

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-/* NEXT STEPS: 
-- remove else logic on line 32-36
-- fix if logic on line 24
-*/
-
-
 // object responsible for maintaining the state of the board
 var gameState = {
   moveOutcome: '',
@@ -12,45 +6,39 @@ var gameState = {
 
   // Have to use .map() at least once to set up board?
   board: [
-    [5, 4, 3, 2, 1],
-    [],
-    [],
+    [5, 4, 3],
+    [1],
+    [2],
   ],
   
   // move discs from one peg to another (use array helper methods, not for loops)
   moveDisc: function (disc, peg) {
-    gameState.board[peg - 1].forEach(function (element) { // check for valid move
-      console.log("el: " + element);
-      if (typeof(element) != 'number') { // -- valid move --
-        if (element > disc) {  // if true, add element to end of peg array
+    if (gameState.board[peg - 1].length === 0) { // check if array is empty. if true, move is valid -> move disc
+      gameState.moveOutcome = 'valid';
+      gameState.board[peg - 1].push(disc);
+    } else {
+      gameState.board[peg - 1].forEach(function (element) { // check each element (disc) on a given peg
+        if (element > disc) {  // check if disc will fit on peg. if true, move is valid -> add element to end of peg array
           gameState.moveOutcome = 'valid';
           gameState.board[peg - 1].push(disc);
           } else { // -- invalid move --
             gameState.moveOutcome = 'invalid';
             gameState.errorMsg = 'Disc can\'t go on top of smaller disc.';
           };
-          
-      } else { // -- invalid move, disc can't go on base of peg --
-        gameState.moveOutcome = 'invalid';
-        gameState.errorMsg = 'Disc can\'t go on base of peg.';
-      };
-      
-    });
-    console.log(`That move was ${gameState.moveOutcome}. ${this.errorMsg} Board is now:`);
+      });
+    };
+    
+    console.log(`That move was ${this.moveOutcome}. ${this.errorMsg} Board is now:`);
+    // reset props after evaluating conditionals
     gameState.moveOutcome = '';
+    gameState.errorMsg = '';
 
     // Use .map() to setup/return board?
     var testOutputMap = gameState.board.map(function (element) {
-      return element;
+      console.log(`--- ${element.join(' ')}`)
     });
-    console.log(testOutputMap);
-    
-    // console.log(gameState.board);
-    // console.log(`--- ${gameState.board[0].join(' ')}`); // peg1
-    // console.log(`--- ${gameState.board[1].join(' ')}`); // peg2
-    // console.log(`--- ${gameState.board[2].join(' ')}`); // peg3
   },
 };
 
 // test function
-console.log("Function Test: ", gameState.moveDisc(1, 2));
+console.log("Function Test: ", gameState.moveDisc(3, 2));
