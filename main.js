5d5ddbb41f38bb0b7ee1e812274d2963bae78baf
Nils Landsberg <nils.landsberg78@gmail.com>
2022-11-09 21:51:43 -0500

    fixed parameters/logic of moveDisc; added edge cases for parameters of moveDisc; updated comments

--- main.js
+++ main.js
@@ -67,73 +67,79 @@ function showBoard() {
   return stringBoard(board).join("\n")
 }
 
-
 var stateOfBoard = {
   numberOfPegs: 3,
   numberOfDiscs: 5,
   hasGameStarted: false,
-  hasWinner: false
+  hasWinner: false,
+  successfulMove: false
 }
 
+function moveDisc(origin, destination) {
+  // !!!!! created this successfulMove property for testing - not sure if I need this
+  stateOfBoard.successfulMove = false;
+  // if origin or destination does not exist on the board, log message and showBoard
+  if ((origin > board.length || destination > board.length) || (origin <= 0 || destination <= 0)) {
+    console.log("You can only move discs to/from pegs that exist on the board.  Please try again.  The board is still:\n" + showBoard());
+    return; 
+  }
+  // if origin or destination is not a number, log message and showBoard
+  if (typeof(origin) !== 'number' || typeof(destination) !== 'number') {
+    console.log(typeof(origin));
+    console.log('Pegs can only be identified by their numeral value - please resubmit your request')
+    return;
+  }
 
-function moveDisc(disc, peg) {
-  stateOfBoard.hasGameStarted = true;
-  // locate peg in array (index of board)
-  var destinationPeg = board[peg - 1]; 
-  // identify the current disc on top of the peg
-  var lastDiscOnDestinationPeg = destinationPeg[destinationPeg.length - 1]
-  // check if disc is larger than the current disc at the top of the peg - if true, log error and show the board
-  if (disc > lastDiscOnDestinationPeg) {
-    console.log("You cannot move a larger disc on top of a smaller one, board is still:\n" + showBoard());
+  var originPeg = board[origin - 1];
+  var destinationPeg = board[destination - 1];
+  // identify last disc on originPeg
+  var originPegDisc = originPeg[originPeg.length - 1];
+  // identify last disc on the destinationPeg
+  var destinationPegDisc = destinationPeg[destinationPeg.length - 1]; 
+  // if originPeg is empty, log message and showBoard
+  if (originPeg.length === 0) {
+    console.log("There are no discs on this peg.  Please try again.\n" + showBoard());
+  // otherwise, if originPegDisc is larger that destinationPegDisc, log message and showBoard
+  } else if (originPegDisc > destinationPegDisc) {
+      console.log("You cannot move a larger disc on top of a smaller one, board is still:\n" + showBoard());
   } else {
-    var discMoved = false;
-    board.forEach(peg => {
-      var topDisc = peg[peg.length - 1];
-      // if disc is not equal to top disc on peg return no value
-      if (discMoved) {
-        return;
-      } else if (disc === topDisc) {
-        // remove the topDisc and push it to the destinationPeg
-        destinationPeg.push(peg.pop());
-        // log that the move was successful and show the board
-        console.log("That move was successful, board is now:\n" + showBoard());
-        // toggle discMoved to true
-        discMoved = true;
-        return;
-      } 
-      });
-    }
-
-    checkWinner();
-
-    if (stateOfBoard.hasWinner) {
-      // if stateOfBoard.hasWinner = true, log victory message and reset board and stateOfBoard values to starting values
-      console.log("Victory! Congratulations, the board has now reset.")
-      resetBoard();
-    }
+    destinationPeg.push(originPeg.pop());
+    // log that the move was successful and show the board
+    console.log("That move was successful, board is now:\n" + showBoard());
+    stateOfBoard.successfulMove = true;
+    stateOfBoard.hasGameStarted = true;
   }
-
-  function resetBoard() {
-    board = [
-      [5, 4, 3, 2, 1],
-      [],
-      []
-    ];
-    stateOfBoard.numberOfPegs = 3;
-    stateOfBoard.numberOfDiscs = 5;
-    stateOfBoard.hasGameStarted = false;
-    stateOfBoard.hasWinner = false;
-  }
-
-  function checkWinner() {
-    // check board to see if a complete array of discs is on a peg that is not board[0]
-      board.forEach((peg, index) => {
-        // edge case to eliminate checking board[0]
-        if (index === 0) return;
-        // if the length of the peg is equal to number of discs in the game, toggle value of .hasWinner to true
-        if (peg.length === stateOfBoard.numberOfDiscs) {
-        stateOfBoard.hasWinner = true;
-      } 
-      });
-    }
     
+  checkWinner();
+
+  if (stateOfBoard.hasWinner) {
+    // if stateOfBoard.hasWinner = true, log victory message and reset board and stateOfBoard values to starting values
+    console.log("Victory! Congratulations, the board has now reset.")
+    resetBoard();
+  }
+}
+
+function resetBoard() {
+  board = [
+    [5, 4, 3, 2, 1],
+    [],
+    []
+  ];
+  stateOfBoard.numberOfPegs = 3;
+  stateOfBoard.numberOfDiscs = 5;
+  stateOfBoard.hasGameStarted = false;
+  stateOfBoard.hasWinner = false;
+}
+
+function checkWinner() {
+   // check board to see if a complete array of discs is on a peg that is not board[0]
+  board.forEach((peg, index) => {
+    // edge case to eliminate checking board[0]
+   if (index === 0) return;
+    // if the length of the peg is equal to number of discs in the game, toggle value of .hasWinner to true
+    if (peg.length === stateOfBoard.numberOfDiscs) {
+      stateOfBoard.hasWinner = true;
+    } 
+  });
+}
+
