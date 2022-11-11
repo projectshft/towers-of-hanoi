e93f7ba5939cc986e3457932e5e14777ec32a9b2
Johnny Wright <johnnywrightiv@gmail.com>
2022-11-11 17:18:24 -0600

    Create playable game state users can interact with in console

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+console.log("Welcome to the Towers of Hanoi! To start a new game type 'startGame()' below.")
+
 // object responsible for maintaining the state of the board
 var gameState = {
   moveOutcome: '',
@@ -11,12 +13,8 @@ var gameState = {
   // check if player won game, if true, announce winner and reset game
   checkWinner: function () {
     if (gameState.board[1].length === 5 || gameState.board[2].length === 5 ) {
-      console.log('Congratulations, you win!')
-      gameState.board = [
-        [5, 4, 3, 2, 1],
-        [],
-        [],
-      ]
+      console.log("🎉 Congratulations, you win! 🎉")
+      startGame();
     }
   },
   
@@ -58,41 +56,62 @@ var gameState = {
       var testOutputMap = gameState.board.map(function (element) {
         console.log(`--- ${element.join(' ')}`)
       });
-
+      
       gameState.checkWinner();
+      // return testOutputMap
     },
 };
 
-// test function
-console.log(gameState.checkMove(1, 2), "Next Move?");
-console.log(gameState.checkMove(1, 3), "Next Move?");
-console.log(gameState.checkMove(2, 3), "Next Move?");
-console.log(gameState.checkMove(1, 2), "Next Move?");
-// console.log(gameState.checkMove(1, 2), "Next Move?"); // error works correctly
-console.log(gameState.checkMove(3, 1), "Next Move?");
-console.log(gameState.checkMove(3, 2), "Next Move?");
-console.log(gameState.checkMove(1, 2), "Next Move?");
-console.log(gameState.checkMove(1, 3), "Next Move?");
-console.log(gameState.checkMove(2, 3), "Next Move?");
-console.log(gameState.checkMove(2, 1), "Next Move?");
-console.log(gameState.checkMove(3, 1), "Next Move?");
-console.log(gameState.checkMove(2, 3), "Next Move?");
-console.log(gameState.checkMove(1, 2), "Next Move?");
-console.log(gameState.checkMove(1, 3), "Next Move?");
-console.log(gameState.checkMove(2, 3), "Next Move?");
-console.log(gameState.checkMove(1, 2), "Next Move?");
-console.log(gameState.checkMove(3, 1), "Next Move?");
-console.log(gameState.checkMove(3, 2), "Next Move?");
-console.log(gameState.checkMove(1, 2), "Next Move?");
-console.log(gameState.checkMove(3, 1), "Next Move?");
-console.log(gameState.checkMove(2, 3), "Next Move?");
-console.log(gameState.checkMove(2, 1), "Next Move?");
-console.log(gameState.checkMove(3, 1), "Next Move?");
-console.log(gameState.checkMove(3, 2), "Next Move?");
-console.log(gameState.checkMove(1, 2), "Next Move?");
-console.log(gameState.checkMove(1, 3), "Next Move?");
-console.log(gameState.checkMove(2, 3), "Next Move?");
-console.log(gameState.checkMove(1, 2), "Next Move?");
-console.log(gameState.checkMove(3, 1), "Next Move?");
-console.log(gameState.checkMove(3, 2), "Next Move?");
-console.log(gameState.checkMove(1, 2));
+// var for user to easily interact with
+var move = gameState.checkMove;
+
+// use to start new game and to reset game after win
+var startGame = function () {
+  gameState.board = [
+    [5, 4, 3, 2, 1],
+    [],
+    [],
+  ]
+  console.log("---------- NEW GAME ----------")
+  console.log("** OBJECTIVE: The goal of the game is to move a set of discs of different sizes (5, 4, 3, 2, 1) from one peg to another while only moving the topmost disc off each peg and not putting a larger disc on top of a smaller one. ")
+  console.log("** INSTRUCTIONS: in order to make a move, please type 'move(X,Y)' below, where X is the peg to move from and Y is the peg to move to. Good luck!")
+  console.log(`--- ${gameState.board[0].join(' ')}`) // oops I hard coded this because it was easier 🫣
+  console.log(`--- ${gameState.board[1].join(' ')}`)
+  console.log(`--- ${gameState.board[2].join(' ')}`)
+};
+
+
+// test game
+// console.log(gameState.checkMove(1, 2), "Next Move?");
+// console.log(gameState.checkMove(1, 2), "Next Move?");
+// console.log(gameState.checkMove(1, 3), "Next Move?");
+// console.log(gameState.checkMove(2, 3), "Next Move?");
+// console.log(gameState.checkMove(1, 2), "Next Move?");
+// // console.log(gameState.checkMove(1, 2), "Next Move?"); // error works correctly
+// console.log(gameState.checkMove(3, 1), "Next Move?");
+// console.log(gameState.checkMove(3, 2), "Next Move?");
+// console.log(gameState.checkMove(1, 2), "Next Move?");
+// console.log(gameState.checkMove(1, 3), "Next Move?");
+// console.log(gameState.checkMove(2, 3), "Next Move?");
+// console.log(gameState.checkMove(2, 1), "Next Move?");
+// console.log(gameState.checkMove(3, 1), "Next Move?");
+// console.log(gameState.checkMove(2, 3), "Next Move?");
+// console.log(gameState.checkMove(1, 2), "Next Move?");
+// console.log(gameState.checkMove(1, 3), "Next Move?");
+// console.log(gameState.checkMove(2, 3), "Next Move?");
+// console.log(gameState.checkMove(1, 2), "Next Move?");
+// console.log(gameState.checkMove(3, 1), "Next Move?");
+// console.log(gameState.checkMove(3, 2), "Next Move?");
+// console.log(gameState.checkMove(1, 2), "Next Move?");
+// console.log(gameState.checkMove(3, 1), "Next Move?");
+// console.log(gameState.checkMove(2, 3), "Next Move?");
+// console.log(gameState.checkMove(2, 1), "Next Move?");
+// console.log(gameState.checkMove(3, 1), "Next Move?");
+// console.log(gameState.checkMove(3, 2), "Next Move?");
+// console.log(gameState.checkMove(1, 2), "Next Move?");
+// console.log(gameState.checkMove(1, 3), "Next Move?");
+// console.log(gameState.checkMove(2, 3), "Next Move?");
+// console.log(gameState.checkMove(1, 2), "Next Move?");
+// console.log(gameState.checkMove(3, 1), "Next Move?");
+// console.log(gameState.checkMove(3, 2), "Next Move?");
+// console.log(gameState.checkMove(1, 2));
