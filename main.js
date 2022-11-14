5085fa6d88c3c2dee7ff400d614115e28190f29f
Alex Van Dahm <avdahm9727@gmail.com>
2022-11-13 20:16:59 -0600

    second commit

--- main.js
+++ main.js
@@ -5,33 +5,36 @@ const gameBoard = {
     [], // peg 3
   ],
   isWon: function () {
-    // check if the second or third peg has all the disks
-   if ( this.board.peg1 === 0 && this.board.peg3 ===0)
-   return win?
+    
+   if ( this.board[1].length === 5 || this.board[2].length === 5)
+   return console.log ('you win!!')
   },
   print: function () {
    this.board.map((peg) => {
-      // use map to print each peg
-      return peg.println 
+      
+      console.log(`--- ${peg.join(' ')}\n`);
     });
   },
   move: function (from, to) {
-    // accept two arguments, from and to
 
-    // check if the move is valid
-    if (this.board.disc < //the number of the disc its going on top of)
+    console.log(this.board[to - 1][this.board[to - 1].length] < this.board[from - 1][this.board[from - 1].length])
+
+    if (this.board[to - 1][this.board[to - 1].length] > this.board[from - 1][this.board[from - 1].length]) {
+      this.board[to - 1].push(this.board[from -1].pop());
+      } else {
+        console.log(`you can't move the things`)
+    }
     
-    // if valid, move the disk
-    .push??
-    // print the board
-    println
-    // check if the game is won
-    if (this.board.peg2 === 5)
-    return console.log(You Win!!)
+    
+    this.print()
+
+    this.isWon()
+    
+    
+    
     
   }
 }
 
 // test in browser console by running gameBoard.move(1, 2)
 
-console.log(gameBoard)
