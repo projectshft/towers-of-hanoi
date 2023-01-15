d2d0b11a88c94f52a60776767933daa85fdd465a
lukegarst <lukegarst@icloud.com>
2023-01-15 11:38:05 -0800

    added game reset functionality

--- main.js
+++ main.js
@@ -1,48 +1,61 @@
 //CONTROLS: Use 'moveDisk(from, to)' and use 'peg1', 'peg2', or 'peg3' as arguments to move the top disk of one peg to another peg
 //Added the option to add on more pegs and/or disks but the addDisk() function only adds more disks with the value '6' :(
 
-  var boardArr = [[5, 4, 3, 2, 1], [], []];
+var boardArr = [[5, 4, 3, 2, 1], [], []];
 
-  var peg1 = boardArr[0];
-  var peg2 = boardArr[1];
-  var peg3 = boardArr[2];
+var peg1 = boardArr[0];
+var peg2 = boardArr[1];
+var peg3 = boardArr[2];
   
-  var checkWinner = function () {
-    if (peg2.length === 5 || peg3.length === 5) {
-      console.log('You win!');
-    }
-  };
-  
-  var printBoard = function (arr) {
-    var gameBoard = arr.map(function (peg) {
-      console.log('---' + peg) 
-      return gameBoard;
-    })
+var checkWinner = function () {
+  if (peg2.length === 5 || peg3.length === 5) {
+    console.log('You win!');
   }
+};
   
-  var moveDisk = (from, to) => {
-    let disk;
+gameReset = () => {
+  boardArr = [[5, 4, 3, 2, 1], [], []];
   
-    if (to.length === 0) {
-      disk = from.pop([]);
-      to.push(disk);
-    } else if (to[to.length - 1] > from[from.length - 1]) {
-      disk = from.pop([]);
-      to.push(disk);
-    } else if (to[to.length - 1] < from[from.length - 1]) {
-      console.log('INVALID MOVE');
-    }
-  checkWinner();
+  peg1 = boardArr[0];
+  peg2 = boardArr[1];
+  peg3 = boardArr[2];
+
   printBoard(boardArr);
-  };
+
+}
+
+var printBoard = function (arr) {
+  var gameBoard = arr.map(function (peg) {
+    console.log('---' + peg) 
+    return gameBoard;
+  })
+}
   
+var moveDisk = (from, to) => {
+  let disk;
+  
+  if (to.length === 0) {
+    disk = from.pop([]);
+    to.push(disk);
+  } else if (to[to.length - 1] > from[from.length - 1]) {
+    disk = from.pop([]);
+    to.push(disk);
+  } else if (to[to.length - 1] < from[from.length - 1]) {
+    console.log('INVALID MOVE');
+  }
+checkWinner();
+printBoard(boardArr);
+};
+  
+printBoard(boardArr);
+  
+var addPeg = function () {
+  boardArr.push([]);
   printBoard(boardArr);
+}
   
-  var addPeg = function () {
-    boardArr.push([]);
-    printBoard(boardArr);
-  }
+var addDisk = function () {
+   peg1.unshift(6)
+}
+
   
-  var addDisk = function () {
-     peg1.unshift(6)
-  }
