e18e8f8574f71f45b5f2dd464f5986f9371d23b1
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-01-25 15:27:37 -0500

    added the board

--- main.js
+++ main.js
@@ -13,16 +13,34 @@
 //once game is won the game should automatically end by announcing the winner(though console.log) and reset for a new game
 //BONUS:  any tiem you iterate through the array, try and refrain from using for or while loops - try using helper methods
 
-var line1 = ['-', '-', '-', 5, 4, 3, 2, 1];
-var line2 = ['-', '-', '-'];
-var line3 = ['-', '-', '-'];
+// var line1 = ['-', '-', '-', 5, 4, 3, 2, 1];
+// var line2 = ['-', '-', '-'];
+// var line3 = ['-', '-', '-'];
 
-console.log(line1, line2, line3);
+// console.log(line1, line2, line3);
 
-var board = {
-  line1: ['-', '-', '-', 5, 4, 3, 2, 1],
-  line2: ['-', '-', '-'],
-  line3: ['-', '-', '-']
-};
+// var gameBoard = [
+//   { title: "---", five: 5, four: 4, three: 3, two: 2, one: 1},
+//   { title: "---", five: number, four: number, three: number, two: number, one: number },
+//   { title: "---", five: number, four: number, three: number, two: number, one: number},
+  
+// ];
 
-console.log(board);
+// console.log(map.gameBoard())
+
+var gameBoard2 = [
+  { title: "---", line: [5, 4, 3, 2, 1]},
+  { title: "---", line: new Array(5)},
+  { title: "---", line: new Array(5)},
+  
+];
+
+var officialBoard = gameBoard2.map(function (key) {
+  return key.title && key.line;
+
+})
+
+console.table(officialBoard)
+
+
+// Array.apply(null, Array(5)).map(function () {})
