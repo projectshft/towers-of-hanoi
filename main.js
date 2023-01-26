11348dbe106df6ddbf72660018dc5f7cfc0ea7fc
josephl37 <josephliow2000@gmail.com>
2023-01-25 23:03:43 -0600

    started moveDisc function

--- main.js
+++ main.js
@@ -4,3 +4,22 @@ let board = {
   tower3: []
 }
 
+const moveDisc = (oldTower) => {
+  // need code to set arguemnts to arrays in board (maybe use switch statement?)
+  if (oldTower == 1) {
+    oldTower = board.tower1;
+    console.log(oldTower);
+  } else if (oldTower == 2) {
+    oldTower = board.tower2;
+    console.log(oldTower);
+  } else if (oldTower == 3) {
+    oldTower = board.tower3
+    console.log(oldTower);
+  } else {
+    console.log(`Invalid Tower. Pick a number between 1 and 3.`)
+  }
+  // conditional to check if move can be done
+  // if ()
+}
+
+console.log(moveDisc(2));
