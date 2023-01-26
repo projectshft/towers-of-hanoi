170d05688d7c18999ec91d537affac4f5ae7ca01
josephl37 <josephliow2000@gmail.com>
2023-01-26 09:17:13 -0600

    assigned arugments to towers

--- main.js
+++ main.js
@@ -1,25 +1,50 @@
 let board = {
   tower1: [5, 4, 3, 2, 1],
-  tower2: [],
+  tower2: [2, 3],
   tower3: []
 }
 
-const moveDisc = (oldTower) => {
+
+const moveDisc = (oldTower, newTower) => {
   // need code to set arguemnts to arrays in board (maybe use switch statement?)
-  if (oldTower == 1) {
-    oldTower = board.tower1;
-    console.log(oldTower);
-  } else if (oldTower == 2) {
-    oldTower = board.tower2;
-    console.log(oldTower);
-  } else if (oldTower == 3) {
-    oldTower = board.tower3
-    console.log(oldTower);
-  } else {
-    console.log(`Invalid Tower. Pick a number between 1 and 3.`)
+  switch (oldTower) {
+    case 1:
+      oldTower = board.tower1;
+      break;
+    case 2:
+      oldTower = board.tower2;
+      break;
+    case 3:
+      oldTower = board.tower3;
+      break;
+    default:
+      console.log(`Invalid First Tower. Pick a number between 1 and 3.`)
+      break;
   }
+
+  switch (newTower) {
+    case 1:
+      newTower = board.tower1;
+      break;
+    case 2:
+      newTower = board.tower2;
+      break;
+    case 3:
+      newTower = board.tower3;
+      break;
+    default:
+      console.log(`Invalid Second Tower. Pick a number between 1 and 3.`)
+      break;
+  }
+
+
+  
+
+
   // conditional to check if move can be done
-  // if ()
+  oldTower.forEach(function (disc) {
+    console.log(disc);
+  })
 }
 
-console.log(moveDisc(2));
+console.log(moveDisc(1, 2));
