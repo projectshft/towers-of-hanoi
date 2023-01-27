03c88c25700a9d1add1448bc142c0f808e14ed22
cwjohnson7 <cliffj1230@gmail.com>
2023-01-27 13:19:52 -0500

    Create Starting Board

---
+++ main.js
@@ -0,0 +1,9 @@
+var startingBoardArray   = [[5, 4, 3, 2, 1], [], []];
+
+var startBoard = function () {
+  console.log('--- ', startingBoardArray[0]);
+  console.log('--- ', startingBoardArray[1]);
+  console.log('--- ', startingBoardArray[2]);
+}
+
+startBoard();
