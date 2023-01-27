47976e66b9a2bdd551aa0949317b6417642760da
cwjohnson7 <cliffj1230@gmail.com>
2023-01-27 15:13:04 -0500

    Starting Board

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var startingBoardArray   = [[5, 4, 3, 2, 1], [], []];
+var board   = [[5, 4, 3, 2, 1], [], []];
 
-var startBoard = function () {
-  console.log('--- ', startingBoardArray[0]);
-  console.log('--- ', startingBoardArray[1]);
-  console.log('--- ', startingBoardArray[2]);
-}
+var startingBoard = function () {
+  return board.map(function (pegs) {
+  return console.log(`--- ${pegs.join(' ')}`);
+})};
 
-startBoard();
+startingBoard();
+
