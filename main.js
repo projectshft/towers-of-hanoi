3acdec58616f8217b029478f74bde450c06d907d
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-01-27 14:06:42 -0500

    deleted unneeded code and cleaned up removed item from to DO

--- main.js
+++ main.js
@@ -3,44 +3,11 @@
 
 //Must utilize a map function at least once to accomplish this part of the assignment.  The starting board will log the 2D array to the console like:
 
-// --- 5 4 3 2 1
-// ---
-// ---
 
-//There should be an object maintaining the state of the board
-//There shoul be a way to move discs form one peg to another
 //There should be a behckWinner function that checks to see if the player has won the game(disc in original order but on different peg)
 //once game is won the game should automatically end by announcing the winner(though console.log) and reset for a new game
 //BONUS:  any tiem you iterate through the array, try and refrain from using for or while loops - try using helper methods
 
-// var line1 = ['-', '-', '-', 5, 4, 3, 2, 1];
-// var line2 = ['-', '-', '-'];
-// var line3 = ['-', '-', '-'];
-
-// console.log(line1, line2, line3);
-
-// var gameBoard = [
-//   { title: "---", five: 5, four: 4, three: 3, two: 2, one: 1},
-//   { title: "---", five: number, four: number, three: number, two: number, one: number },
-//   { title: "---", five: number, four: number, three: number, two: number, one: number},
-  
-// ];
-
-// console.log(map.gameBoard())
-
-// var gameBoard2 = [
-//   {  line: [5, 4, 3, 2, 1]},
-//   {  line: new Array(5)},
-//   {  line: new Array(5)},
-  
-// ];
-
-// // var officialBoard = gameBoard2.map(function (key) {
-// //   console.table(key.title && key.line);
-
-// var mapped = gameBoard2.map(function(select) {
-//  return select.line;
-// });
 
 //I need 3 array
 var line1 = [5, 4, 3, 2, 1];
@@ -51,25 +18,15 @@ var winningArray = [5, 4, 3, 2, 1];
 
 var board = [line1, line2, line3];
 
-// var game = board.map(function (array) {
-//   return array;
-// });
 
 console.table(board);
-// console.table(game[0]);
-
-// var moveFunction = function(array, index, )
-
-// game[1].push(game[0].pop(1));
 
 //when mod args that are sent insdie func you can have uninted side effect could change board
 //arr and obj are pass by reference
 
 //Function to create board, move piece and throw error if a larger number is placed on top of another
-// var orgiginalBoard = [...arr];
-function arrMove(arr, oldIndex, newIndex) {
 
-  
+function arrMove(arr, oldIndex, newIndex) {
   var newBoard = arr.map( function (someVar){
     return someVar.map(function(arr2) {
       return arr2;
@@ -86,11 +43,11 @@ function arrMove(arr, oldIndex, newIndex) {
   } else {
     moveRule();
   };
-  // console.table(newBoard[newIndex])
-  board  = newBoard;
 
+  board  = newBoard;
 };
 
+//Winning Moves
 arrMove(board, 0, 2)
 arrMove(board, 0, 1);
 arrMove(board, 2, 1)
