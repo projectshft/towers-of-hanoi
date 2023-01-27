e0b9739aad563c6f26a00a0b7037db86af86c92c
cwjohnson7 <cliffj1230@gmail.com>
2023-01-27 18:40:01 -0500

    Move Disc function (without conditionals)

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ var startingBoard = function () {
 
 startingBoard();
 
+var moveDisc = function (pegA, pegB) {
+  var peg1 = pegA - 1;
+  var peg2 = pegB - 1;
+
+  board[peg2].push(board[peg1].pop());
+  console.log('That move was successful, board is now: ');
+  startingBoard();
+}
