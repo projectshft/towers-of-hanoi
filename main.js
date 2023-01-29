78fb3b4812f860a0335f48ebdac784230edb6f46
FenrirIsFree <nickscape96@gmail.com>
2023-01-29 15:35:37 -0500

    Added the checkWinner to moveDisc

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // We'll want to be able to print the board horizontally. You MUST utilize a map function at least once
 
-const board = {peg1: [5, 4, 3, 2, 1],
-peg2: [],
+const board = {peg1: [1],
+peg2: [5, 4, 3, 2],
 peg3: []};
 
 const moveDisc = function (start, end) {
@@ -23,19 +23,15 @@ const moveDisc = function (start, end) {
   console.log(`--- ${board.peg1.map(x => x).join(' ')}`);
   console.log(`--- ${board.peg2.map(x => x).join(' ')}`);
   console.log(`--- ${board.peg3.map(x => x).join(' ')}`);
-}
-
-const checkWinner = function () {
- 
-  if(board['peg2'].length === 5 || board['peg3'].length === 5){
+//Checking if the you have won everytime you move a disc, using setTimeout just to make sure it doesn't go to the top of the stack
+  setTimeout( () => {
+    if(board['peg2'].length === 5 || board['peg3'].length === 5){
 
     console.log('All discs are moved from peg1 to peg2 or peg3. You win and the game has been reset.');
 
     board['peg1'] = [5, 4, 3, 2, 1]; 
     board['peg2'] = [];
     board['peg3'] = [];
-  } else {
-
-    console.log('You have not move all the disks to either peg2 or peg3.')
-  }
+    }
+  }, 0);
 }
