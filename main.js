24eb46e3624904203751ffd12d60f3b3ed707a83
Jill Arnold <jillarnold2913@gmail.com>
2023-04-06 13:29:11 -0400

    Refactor of original eval

--- main.js
+++ main.js
@@ -1,98 +1,99 @@
-// //original start to project 
-// //First of all it will need a board. We'll utilize a 2D array to do this:
-let array = [
-  [5, 4, 3, 2, 1],
-  [],
-  []
-  ];
+//OBJECT WITH ARRAYS CAN BE CONSIDERED AS 2D ARRAYS
+let gameBoard = {
+  peg1: [5, 4, 3, 2, 1],
+  peg2: [],
+  peg3: [],
+}
 
-// // transform arrays to object
-function fromArrToObj(array) {
-  var board = {};
- 
- for (var i = 0; i < array.length; i++) {
-   const element = array[i];
+console.log(gameBoard)
 
-   element[0];
-   element[1];
+//Message to player with game rules.
+console.log('Game starts here! The goal is to stack the discs onto another peg, making sure that larger discs are not placed on smaller ones.  Good luck!!')
 
-   board[element[0]] = element[1];
- }
- return board;
- }
+//TRY WITH MAP - WILL WORK AS OBJECT?  CHECK INTO "ARRAY-LIKE"
+showBoard = () => {
+console.log('Peg1:  ' + '---' + gameBoard.peg1.join(' '));
+console.log('Peg2:  ' + '---' + gameBoard.peg2.join(' '));
+console.log('Peg3:  ' + '---' + gameBoard.peg3.join(' '));
+}
 
-// // set up format for peg display
-let gameBoard; 
+showBoard();
 
-// //define # of discs on each peg
-
-const Peg = class {
-  constructor(length) {
-    this.length = length;
+resetBoard = () => {
+  console.log('Lets play again!')
+  gameBoard = {
+    peg1: [5, 4, 3, 2, 1],
+    peg2: [],
+    peg3: [],
   }
+  showBoard();
+  
 };
 
-let peg1 = new Peg(5);
-let peg2 = new Peg(5);
-let peg3 = new Peg(5);
-
-
-// // redefine starting position for each peg 
-gameBoard = [
-  peg1 = [5, 4, 3, 2, 1],
-  peg2 = [],
-  peg3 = [],
-  ];
- 
-  
-
-// // // assign peg name to each object key 
-gameBoard['peg1'] = gameBoard['0'];
-gameBoard['peg2'] = gameBoard['1'];
-gameBoard['peg3'] = gameBoard['2'];
-delete gameBoard['0'];
-delete gameBoard['1'];
-delete gameBoard['2'];
-
-let getPlayerChoice = function (fromPeg, toPeg) {
-  if (toPeg.length ===0 || toPeg.length-1 > fromPeg.length-1) {
-     toPeg.push(fromPeg.pop()), console.log("Good move! Board is now:") 
-     console.log(`--- ${peg1.join(' ')}`);
-     console.log(`--- ${peg2.join(' ')}`);
-     console.log(`--- ${peg3.join(' ')}`);
- } else if (fromPeg.length === 0 || toPeg.length-1 < fromPeg.length-1 || toPeg === fromPeg) {
-     console.log("Move cannot be made. Try again");
- }
- return gameBoard
-  };
-     
-getPlayerChoice(peg1, peg3);
-
-let playGame = function(getPlayerChoice) {
-  return gameBoard;
-
-}
-console.log(playGame(getPlayerChoice));
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-// const checkWinner = function () {
-//   if gameBoard = peg2[5,4,3,2,1] || peg3[5,4,3,2,1] {
-//     console.log("You won the game!");
+// checkWinner = (toPeg) => {
+//   if (toPeg,length ===5) {
+//     console.log("Hurray!  You won!");
+//     resetBoard();
 //   }
-// }
+// };
 
+moveDisc = (fromPeg, toPeg) => {
+  if (fromPeg.length === 0) {
+    console.log("No disc to move!  Try another peg");
+    return showBoard();
+  } else if (fromPeg === toPeg) {
+    console.log("You must move your piece to a different peg - try again!")
+    return showBoard();
+  } else if (toPeg.length !==0 && toPeg[toPeg.length-1] < fromPeg[fromPeg.length-1]) {
+    console.log("Larger discs cannot be placed on smaller discs.  Try again.");
+    return showBoard();
+  } else {
+    let disc = fromPeg.pop();
+    toPeg.push(disc);
+    
+    if (toPeg.length === 5) {
+      console.log('AWESOME move - YOU WON!!');
+      showBoard();
+      return resetBoard();
+            
+    } else {
+    console.log("Good move!  Board is now:")
+    return showBoard();
+  }
+}
+}
+//ROUND ONE
+moveDisc(gameBoard.peg1, gameBoard.peg3)
+moveDisc(gameBoard.peg1, gameBoard.peg2)
+moveDisc(gameBoard.peg3, gameBoard.peg2)
+moveDisc(gameBoard.peg1, gameBoard.peg3)
+moveDisc(gameBoard.peg2, gameBoard.peg1)
+moveDisc(gameBoard.peg2, gameBoard.peg3)
+moveDisc(gameBoard.peg1, gameBoard.peg3)
+moveDisc(gameBoard.peg1, gameBoard.peg2)
+moveDisc(gameBoard.peg3, gameBoard.peg2)
+moveDisc(gameBoard.peg3, gameBoard.peg1)
+moveDisc(gameBoard.peg2, gameBoard.peg1)
+moveDisc(gameBoard.peg3, gameBoard.peg2)
+moveDisc(gameBoard.peg1, gameBoard.peg3)
+moveDisc(gameBoard.peg1, gameBoard.peg2)
+moveDisc(gameBoard.peg3, gameBoard.peg2)
+moveDisc(gameBoard.peg1, gameBoard.peg3)
+moveDisc(gameBoard.peg2, gameBoard.peg1)
+moveDisc(gameBoard.peg2, gameBoard.peg3)
+moveDisc(gameBoard.peg1, gameBoard.peg3)
+moveDisc(gameBoard.peg2, gameBoard.peg1)
+moveDisc(gameBoard.peg3, gameBoard.peg2)
+moveDisc(gameBoard.peg3, gameBoard.peg1)
+moveDisc(gameBoard.peg2, gameBoard.peg1)
+moveDisc(gameBoard.peg2, gameBoard.peg3)
+moveDisc(gameBoard.peg1, gameBoard.peg3)
+moveDisc(gameBoard.peg1, gameBoard.peg2)
+moveDisc(gameBoard.peg3, gameBoard.peg2)
+moveDisc(gameBoard.peg1, gameBoard.peg3)
+moveDisc(gameBoard.peg2, gameBoard.peg1)
+moveDisc(gameBoard.peg2, gameBoard.peg3)
+moveDisc(gameBoard.peg1, gameBoard.peg3)
+//ROUND TWO
+moveDisc(gameBoard.peg1, gameBoard.peg3)
 
- 
