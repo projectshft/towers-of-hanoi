8a66e1e92e88fdc60769b8cc0c782eeedec51697
etiry <7997357+etiry@users.noreply.github.com>
2023-07-25 11:20:20 -0400

    Fix moveDisc function

--- main.js
+++ main.js
@@ -31,28 +31,85 @@ Board.prototype.moveDisc = function (pegMoveFrom, pegMoveTo) {
     console.log('The peg you want to move a disc from doesn\'t exist! Board is still:');
     console.log(this.displayBoard());
   } else if (!this.board[pegMoveTo - 1]) {
-    console.log('The peg you want to move a peg to doesn\'t exist! Board is still:');
+    console.log('The peg you want to move a disc to doesn\'t exist! Board is still:');
     console.log(this.displayBoard());
   } else if (this.board[pegMoveFrom - 1].length === 0) {
     console.log('There aren\'t any discs on that peg! Board is still:');
     console.log(this.displayBoard());
-  } else if (this.board[pegMoveTo - 1].length !== 0) {
-    if (this.board[pegMoveFrom - 1].slice(-1) > this.board[pegMoveTo - 1].slice(-1)) { 
-      console.log('You cannot move a larger disc on top of a smaller one. Board is still:');
-      console.log(this.displayBoard());
-    }
-  } else {
+  } 
+  
+  if (this.board[pegMoveTo - 1].length === 0) {
     var discToMove = this.board[pegMoveFrom - 1].pop();
     this.board[pegMoveTo - 1].push(discToMove);
     console.log('That move was successful. Board is now:');
     console.log(this.displayBoard());
+  } else {
+    if (this.board[pegMoveFrom - 1].slice(-1) > this.board[pegMoveTo - 1].slice(-1)) { 
+      console.log('You cannot move a larger disc on top of a smaller one. Board is still:');
+      console.log(this.displayBoard());
+    } else {
+      var discToMove = this.board[pegMoveFrom - 1].pop();
+      this.board[pegMoveTo - 1].push(discToMove);
+
+      if (this.checkWinner()) {
+        console.log('You win! Here is the final board:');
+        console.log(this.displayBoard());
+      } else {
+        console.log('That move was successful. Board is now:');
+        console.log(this.displayBoard());
+      }
+    }
   }
+}
+
+Board.prototype.checkWinner = function () {
+  var winner = false;
+  var pegsToCheck = this.board.slice(1);
+
+  pegsToCheck.forEach(function (peg) {
+    winner = peg.reduce(function (acc, disc) {
+      if (acc - disc === 1) {
+        return true;
+      } else {
+        return false;
+      }
+    }, this.discs + 1);
+  })
+
+  return winner;
 }
 
 var board = new Board(3, 5);
 
 board.startNewGame();
 board.moveDisc(1, 2);
+board.moveDisc(1, 3);
+board.moveDisc(2, 3);
+board.moveDisc(1, 2);
+board.moveDisc(3, 1);
+board.moveDisc(3, 2);
 board.moveDisc(1, 2);
 board.moveDisc(1, 3);
-board.moveDisc(1, 4);
+board.moveDisc(2, 3);
+board.moveDisc(2, 1);
+board.moveDisc(3, 1);
+board.moveDisc(2, 3);
+board.moveDisc(1, 2);
+board.moveDisc(1, 3);
+board.moveDisc(2, 3);
+board.moveDisc(1, 2);
+board.moveDisc(3, 1);
+board.moveDisc(3, 2);
+board.moveDisc(1, 2);
+board.moveDisc(3, 1);
+board.moveDisc(2, 3);
+board.moveDisc(2, 1);
+board.moveDisc(3, 1);
+board.moveDisc(3, 2);
+board.moveDisc(1, 2);
+board.moveDisc(1, 3);
+board.moveDisc(2, 3);
+board.moveDisc(1, 2);
+board.moveDisc(3, 1);
+board.moveDisc(3, 2);
+board.moveDisc(1, 2);
