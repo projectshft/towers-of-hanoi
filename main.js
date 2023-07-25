6344c32a88a4565a6b38340b64ee8ab53db0e58a
etiry <7997357+etiry@users.noreply.github.com>
2023-07-25 13:07:24 -0400

    Comment out solution

--- main.js
+++ main.js
@@ -89,40 +89,38 @@ Board.prototype.checkWinner = function () {
   return winner;
 }
 
-
-
-
 var board = new Board(3, 5);
-
 board.startNewGame();
-board.moveDisc(1, 2);
-board.moveDisc(1, 3);
-board.moveDisc(2, 3);
-board.moveDisc(1, 2);
-board.moveDisc(3, 1);
-board.moveDisc(3, 2);
-board.moveDisc(1, 2);
-board.moveDisc(1, 3);
-board.moveDisc(2, 3);
-board.moveDisc(2, 1);
-board.moveDisc(3, 1);
-board.moveDisc(2, 3);
-board.moveDisc(1, 2);
-board.moveDisc(1, 3);
-board.moveDisc(2, 3);
-board.moveDisc(1, 2);
-board.moveDisc(3, 1);
-board.moveDisc(3, 2);
-board.moveDisc(1, 2);
-board.moveDisc(3, 1);
-board.moveDisc(2, 3);
-board.moveDisc(2, 1);
-board.moveDisc(3, 1);
-board.moveDisc(3, 2);
-board.moveDisc(1, 2);
-board.moveDisc(1, 3);
-board.moveDisc(2, 3);
-board.moveDisc(1, 2);
-board.moveDisc(3, 1);
-board.moveDisc(3, 2);
-board.moveDisc(1, 2);
+
+// Solution
+// board.moveDisc(1, 2);
+// board.moveDisc(1, 3);
+// board.moveDisc(2, 3);
+// board.moveDisc(1, 2);
+// board.moveDisc(3, 1);
+// board.moveDisc(3, 2);
+// board.moveDisc(1, 2);
+// board.moveDisc(1, 3);
+// board.moveDisc(2, 3);
+// board.moveDisc(2, 1);
+// board.moveDisc(3, 1);
+// board.moveDisc(2, 3);
+// board.moveDisc(1, 2);
+// board.moveDisc(1, 3);
+// board.moveDisc(2, 3);
+// board.moveDisc(1, 2);
+// board.moveDisc(3, 1);
+// board.moveDisc(3, 2);
+// board.moveDisc(1, 2);
+// board.moveDisc(3, 1);
+// board.moveDisc(2, 3);
+// board.moveDisc(2, 1);
+// board.moveDisc(3, 1);
+// board.moveDisc(3, 2);
+// board.moveDisc(1, 2);
+// board.moveDisc(1, 3);
+// board.moveDisc(2, 3);
+// board.moveDisc(1, 2);
+// board.moveDisc(3, 1);
+// board.moveDisc(3, 2);
+// board.moveDisc(1, 2);
