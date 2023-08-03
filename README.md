532682b33ac92d267511d81b1c21bea7043bc982
Jordan Cox <jordancox747@outlook.com>
2023-08-03 14:53:48 -0600

    initial + moveDisc beginning to work

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Towers of Hanoi
 
-This project has been created by a student at Parsity, an online software engineering course. The work in this repository is wholly of the student based on a sample starter project that can be accessed by looking at the repository that this project forks.
+This is a project I completed for my <a href="https://parsity.io/" target="_blank">Parsity</a> bootcamp curriculum.
 
-If you have any questions about this project or the program in general, visit [parsity.io](https://parsity.io/) or email hello@parsity.io.
+### How to Play:
+
+- To get started, click here (link to index.html) and open your developer console on Google Chrome by entering Option + &#8984; + J (macOS) or Shift + CTRL + J (on Windows/Linux).
---
+++ main.js
@@ -0,0 +1,52 @@
+const boardState = {
+// will print current state of board
+// for example, the starting state would be:
+// --- 3 2 1
+// ---
+// ---
+
+// After a move:
+// That move was successful, board is now:
+// --- 3 2
+// --- 1
+// ---
+
+// Or:
+// You cannot move a larger disc on top of a smaller one, board is still:
+// --- 3 2
+// --- 1
+// ---
+  board: [],
+  initializeGame: function () {
+    this.board = [ [3, 2, 1],
+                   [],
+                   [] ];
+  },
+  logBoard: function () {
+ // must use map function to print the board
+  },
+  checkWinner: function () {
+// Check to see if the state of the board is the original order (largest to smallest) on the last peg instead of the first:
+// Winner! Play again? (board will be reset to original state)
+// ---
+// ---
+// --- 3 2 1
+  },
+};
+
+boardState.initializeGame();
+
+const moveDisc = function (fromPeg, toPeg) {
+   // check to make sure move is legal. If so,
+   fromPeg--;
+   toPeg--;
+
+   boardState.board[toPeg].push(boardState.board[fromPeg].pop());
+   console.log(boardState.board);
+};
+
+// Extension Options
+// Add set board function with ability to set number of pegs and discs (minimum of 3 of each):
+// const setUp = function () { };
+
+// Create algorithm to play the game
