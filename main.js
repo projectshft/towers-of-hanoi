f9f613c995d5caa30c454ce77dc0b58f511aeb9e
Justin Thomas <nissan95sx.95@gmail.com>
2023-10-29 18:43:15 -0500

    Added Display Board

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+var myBoard = [[5, 4, 3, 2, 1], [], []];
+
+var displayBoard = function (){
+  myBoard.forEach(function (peg, index){
+  console.log('--- ${peg.join("") || "-"}');
+});
+    console.log("---");
+};
+
+var isValidMove = function (source, dest) {
+  var sourcePeg = myBoard [source - 1];
+  var destPeg = myBoard[dest - 1];
+  if (sourcePeg === 0) {
+    console.log("Invalid Move: You cannot move a larger disc on top of a smaller one.");
+    return false;
+  } else if (destPeg.length !== 0 && sourcePeg[sourcePeg.length - 1] > destPeg[destPeg.length - 1]){
+    console.log("")
+    return false;
+  }
+  return true;
+};
