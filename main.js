772f4b349e766ffd0328f9bc65065e5d26d6a8e3
Stephen <stephen.swaringin@gmail.com>
2024-02-06 06:47:09 -0600

    Added functions to start, update, and print the board

--- main.js
+++ main.js
@@ -13,33 +13,57 @@
 // ---
 
 // A board representing 3 pegs, and 5 discs on the first peg in ascending order.  This is a normal starting position for the game.
-// var startingBoard = [
-//   [5, 4, 3, 2, 1],
-//   [],
-//   []
-// ];
 
+var startingBoard = [
+  [5, 4, 3, 2, 1],
+  [],
+  []
+];
 
-// var printBoard = function() {};
+var printBoard = function() {
+  this.board.map(function(peg) {
+    var output = "";
+    peg.forEach(function(disc) {
+      output += ` ${disc} `;
+    })
+    console.log("---" + output);
+  })
+};
 
-// var moveDisc = function () {};
+var startBoard = function() {
+  this.board = startingBoard.map(function (item) {
+    return item;
+  });
+  this.update();
+};
+
+var updateBoard = function() {
+  this.peg1 = this.board[0];
+  this.peg2 = this.board[1];
+  this.peg3 = this.board[2]; 
+}
+
+// var moveDisc = function(source, destination) {
+//     var sourcePeg = this[source];
+//     var destinationPeg = this[destination];
+//     var disc = sourcePeg.pop();
+//     destinationPeg.push(disc);
+// };
 
 // var resetBoard = function() {};
 
 // var checkWinner = function() {};
 
-// var boardState = {
-//   peg1:["5", "4", "3", "2", "1"],
-//   peg2:["", "", "", "", ""],
-//   peg3:["", "", "", "", ""],
-//   moveCount: 0,
-//   print: printBoard,
-//   move: function(source, destination) {
-//     var sourcePeg = this[source];
-//     var destinationPeg = this[destination];
-//     var disc = sourcePeg.pop();
-//     destinationPeg.push(disc);
-//   },
-// };
+var boardState = {
+  board: [],
+  peg1: [],
+  peg2: [],
+  peg3: [],
+  moveCount: 0,
+  start: startBoard,
+  update: updateBoard,
+  print: printBoard,
+  //move: moveDisc,
+};
 
 debugger;
