b58fdfb2b44ff75d9cf221324670c0d8f511c8f4
Stephen <stephen.swaringin@gmail.com>
2024-02-06 05:57:43 -0600

    Adding game script and html page

---
+++ main.js
@@ -0,0 +1,45 @@
+// >>> Towers of Hanoi <<<
+
+// TODO: Make an object responsible for maintaining the state of the board.
+// TODO: Make a function that moves a disc moveDisc(sourcePeg, destinationPeg)
+// TODO: Make a checkWinner function that checks to see if the player has won the game.
+// Winning the game means all of the discs are placed on a different peg in the same order.
+// TODO: When the player wins, announce the winner and reset for a new game.
+// Try to use array helper methods instead of for or while loops.
+
+// example starting board
+// --- 5 4 3 2 1
+// ---
+// ---
+
+// A board representing 3 pegs, and 5 discs on the first peg in ascending order.  This is a normal starting position for the game.
+// var startingBoard = [
+//   [5, 4, 3, 2, 1],
+//   [],
+//   []
+// ];
+
+
+// var printBoard = function() {};
+
+// var moveDisc = function () {};
+
+// var resetBoard = function() {};
+
+// var checkWinner = function() {};
+
+// var boardState = {
+//   peg1:["5", "4", "3", "2", "1"],
+//   peg2:["", "", "", "", ""],
+//   peg3:["", "", "", "", ""],
+//   moveCount: 0,
+//   print: printBoard,
+//   move: function(source, destination) {
+//     var sourcePeg = this[source];
+//     var destinationPeg = this[destination];
+//     var disc = sourcePeg.pop();
+//     destinationPeg.push(disc);
+//   },
+// };
+
+debugger;
