5a5826bf48618fe394b0d445d9af2121cb776437
Spencer Willis <spencer.willis220@gmail.comgit config --global user.email spencer.willis220@gmail.comgit statusgit status>
2024-02-05 20:31:45 -0500

    added the isWon function to determine when the game has been won

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const game = {
-    pegs: [[5, 4, 3, 2, 1], [], []],
+    pegs: [[], [5, 4, 3, 2, 1], []],
     
-    isWon: function () {
-      
+    //This function was necessary to find the sum of the discs on the pegs, I did this so that you can play the game at scale
+    sumOfDiscs: function (discs) {
+      let sum = 0; 
+      for (let i = 0; i < this.pegs.length; i++) {
+        for (let j = 0; j < this.pegs[i].length; j++) {
+          sum += this.pegs[i][j]; 
+          }
+        }
+        return sum; 
+    },
+    //this function lets you know if you won the game by seeing if the sum of the discs in the function have moved to another peg besides peg 1
+    isWon: function () { 
+    const sumOfPegs = this.sumOfDiscs(); 
+
+    if (this.sumOfDiscs(1) === sumOfPegs || this.sumOfDiscs(2) === sumOfPegs) {
+      console.log("You won the game!")
+    return true; 
+    } 
     },
     //The move function moves the discs from the peg array you select first, to the peg array you select second
     move: function (startPeg, endPeg) {
@@ -48,7 +64,7 @@ const game = {
       
     }
 };
-
+console.log(game.isWon()); 
 
 console.log(game.move(0, 2));
 
