f24f12065f2fbc1772d69753f6569df86c3a99b6
Stephen <stephen.swaringin@gmail.com>
2024-02-08 08:41:24 -0600

    Updated checkWinner function (still needs work)

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // >>> Towers of Hanoi <<<
 
-// TODO: Make an object responsible for maintaining the state of the board.
-// TODO: Make a function that moves a disc moveDisc(sourcePeg, destinationPeg)
+// To win Towers of Hanoi, you must successfully move all of the discs from one peg to another.
+// You may only move the topmost disc; and you may not move a larger disc onto a smaller one.
+
 // TODO: Make a checkWinner function that checks to see if the player has won the game.
-// Winning the game means all of the discs are placed on a different peg in the same order.
 // TODO: When the player wins, announce the winner and reset for a new game.
-// Try to use array helper methods instead of for or while loops.
+// TODO: Try to use array helper methods instead of for or while loops.
+// TODO: Make it interactive in a webpage.
 
 // example starting board
 // --- 5 4 3 2 1
@@ -13,7 +14,6 @@
 // ---
 
 // A board representing 3 pegs, and 5 discs on the first peg in ascending order.  This is a normal starting position for the game.
-
 const startingBoard = [
   [5, 4, 3, 2, 1],
   [],
@@ -38,6 +38,8 @@ const startBoard = function() {
 };
 
 const updatePegs = function() {
+  // Establishes references to the items in the board array
+  // Mutation of the peg means mutation of the board.
   this.peg1 = this.board[0];
   this.peg2 = this.board[1];
   this.peg3 = this.board[2]; 
@@ -47,15 +49,19 @@ const moveDisc = function(source, destination) {
     let sourcePeg = this[`peg${source}`];
     let destinationPeg = this[`peg${destination}`];
 
+    // What if I try to move a larger disc on top of a smaller one?
     if (sourcePeg.length === 0) {
       console.log("Sorry. You can't move a disc that isn't there!")
       return;
     }
 
+    // What if I try to move a disc from a peg that is empty?
     if (sourcePeg[sourcePeg.length - 1] > destinationPeg[destinationPeg.length - 1]) {
       console.log("Sorry. You can't move a larger disc on top of a smaller disc.")
       return;
     }
+
+    // What if I try to move the disc to the same peg?
 
     this.moveCount += 1;
 
@@ -63,11 +69,49 @@ const moveDisc = function(source, destination) {
     destinationPeg.push(disc);
 };
 
-const resetBoard = function() {
-
+const resetBoard = function() { //Reset is not actually resetting the board
+  console.log("Would you like to play again?");
+  let answer = "y";
+  if (answer == "n") {
+    console.log("Thanks for playing!")
+    return;
+  } else {
+    // Revert the boardState.board property to the startingBoard value
+    boardState.start();
+  }
 };
 
-// const checkWinner = function() {};
+const checkWinner = function() {
+  // What is the winning condition?
+  // When all of the discs are not on 'peg1' and the discs are in their original order
+  // let winningCondition = [5, 4, 3, 2, 1];
+  
+  // Write a function that skips straight to a win.
+
+  // Put the pegs in an array
+  let pattern = /peg/;
+  let pegs = Object.keys(boardState).filter(function(key) {
+    if (pattern.test(key)) {
+      return key;
+    }
+  });
+
+  // Is peg1 empty?
+  if (this.peg1.length === 0) {
+    // Does another peg have all of the discs?
+    pegs.forEach(function(peg) {
+      if (peg !== "peg1" && boardState[peg].length === 5) {
+        // Are the discs in the correct order?
+        // let isEqual = this.every((value, index) => value === array2[index]);
+        // console.log(isEqual); // Output: true
+        this.winner = true;
+        console.log("Congratulations! You have won this round.")
+        resetBoard();
+      }
+    })
+  }
+
+};
 
 let boardState = {
   board: [],
@@ -75,12 +119,16 @@ let boardState = {
   peg2: [],
   peg3: [],
   moveCount: 0,
+  winner: false,
   start: startBoard,
   update: updatePegs,
   print: printBoard,
   reset: resetBoard,
   move: moveDisc,
+  check: checkWinner,
   //quit: quitGame,
 };
+
+// Start the game with a welcome message.
 
 debugger;
