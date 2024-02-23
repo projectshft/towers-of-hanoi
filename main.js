72e92d1e9ba986ce4c1bd557de2b3b826bcdfa30
Audrey Clark <amazon.audrey@gmail.com>
2024-02-22 23:37:49 -0600

    Update functionality, style, and package.json setting

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
+/* eslint-disable prettier/prettier */
+
 // Requirements:
-// board represented by a 2D array
-// print the board horizontally
 // use .map method and Array Helper methods
-// starting board looks a certain way - maybe need to print the starting board first
 // player submits moves to the game. the game accepts or rejects the move (need error statements). 
 // If move is acceptable, then the board will be updated
 // can only move smaller discs on top of larger discs
-// object responsible for maintaining the state of the board
+
 // a way to move discs from on peg to another
 // need a checkWinner() that checks to see if the player has won the game
 // winning = all the disks are put back in original order on a different peg
@@ -15,69 +14,55 @@
 
 
 const board = {
-  pegs: [[5, 4, 3, 2, 1],
-  [],
-  []
-  ],
+  peg1: [5, 4, 3, 2, 1],
+  peg2: [],
+  peg3: [] 
+};
+
+const startingBoard = {
+  peg1: [5, 4, 3, 2, 1],
+  peg2: [],
+  peg3: [] 
+};
+
+const printBoard = (board) => {
+  Object.values(board).forEach((peg) => console.log('---', peg)
+  )};
+
+printBoard(board);
+
+const moveDisc = (src, dest) => {
+
+  if (src.board.peg < dest.board.peg) {
+    console.log('That move was successful! The board is now:', printBoard(board));
+  } else if (src.board > dest.board.peg){
+    console.log('Error: You cannot move a larger disc on top of a smaller one. The board is still:', printBoard(board));
+  };
 
 
 };
 
-const peg1 = board.pegs[0];
-const peg2 = board.pegs[1];
-const peg3 = board.pegs[2];
-
-
-
-const moveDisc = function(n, start, target, inter){
-  // needs a comparison between big and smaller discs
-  // if else statements
-  // throw specific errors "You cannot move a larger disc on top of a smaller one, board is still:"
-  if(n < 0){
-
-  }
-
-  // update board is move is acceptable - "That move was successful, board is now:"
-
-};
-
-const printBoard = function() {
-  this.board.map()
-;}
 
 const checkWinner = function() {
-  // insert if else statements
-  // need a check for winning board
-
+  const winning = [5, 4, 3, 2, 1];
+  if (board[0][1] === winning || board[0][2] === winning) {
+    console.log('Congrats, you win!'); 
+    
+    const board = startingBoard;
+  };
 };
-
- const winnerWinner = function() {
-  // announce a winner
-  // reset the board
-
-};
-
-const output = function() {
-  console.log()
-};
-
 
 
 // Tests:
 // Move the disc from peg 1 to peg 2
 moveDisc(1, 2);
-console.log("That move was successful, board is now:");
+console.log('That move was successful, board is now:');
 
 // Move disc from peg 1 to peg 3
 moveDisc(1, 3);
-console.log("That move was successful, board is now:");
+console.log('That move was successful, board is now:');
 
 // Move disc from peg 1 to peg 2
 moveDisc(1, 2);
-console.log("You cannot move a larger disc on top of a smaller one, board is still:");
+console.log('You cannot move a larger disc on top of a smaller one, board is still:');
 
-
-// ----------
-// t(n, A, C, B)
-// t(n-1, A, B, C)
-// n -> C 
