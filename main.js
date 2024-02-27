58ba7ca1376ca6b9cf614e57356fa5c97c5569a3
Audrey Clark <amazon.audrey@gmail.com>
2024-02-27 00:05:17 -0600

    Update moving discs, winning, and resetting

--- main.js
+++ main.js
@@ -1,77 +1,106 @@
 /* eslint-disable prettier/prettier */
 
-// Requirements:
-// use .map method and Array Helper methods
-// player submits moves to the game. the game accepts or rejects the move (need error statements). 
-// If move is acceptable, then the board will be updated
-// can only move smaller discs on top of larger discs
-
-// a way to move discs from on peg to another
-// need a checkWinner() that checks to see if the player has won the game
-// winning = all the disks are put back in original order on a different peg
-// once the player has won, the game ends and announces the winner, and the game is reset back to starting position
-// try not to use for or while loops
-
-const successMsg = 'That move was successful! The board is now:';
-const errorMsg = 'Error: You cannot move a larger disc on top of a smaller one. The board is still:';
+const startingBoard = {
+  peg1: [5, 4, 3, 2, 1],
+  peg2: [],
+  peg3: [],
+};
 
 const board = {
   peg1: [5, 4, 3, 2, 1],
   peg2: [],
-  peg3: [] 
-  
+  peg3: [],
 };
-
-// const startPos = [[5, 4, 3, 2, 1], [], []];
 
 // eslint-disable-next-line no-shadow
 const printBoard = (board) => {
-  Object.values(board).forEach((peg) => console.log('---', peg)
-  )};
+  Object.values(board).forEach((peg) => console.log('---', peg));
+};
 
-console.log('The board looks like:', printBoard(board));
+const moveDisc = (currentPeg, targetPeg) => {
+  // Check if the source peg exists in the board
+  if (!board[currentPeg]) {
+    console.error(`Error: The selected peg '${currentPeg}' does not exist.`);
+    return;
+  };
+  // Get the top disc from the source peg i.e. last element
+  const disc = board[currentPeg].pop();
 
-const moveDisc = (currentP, nextP) => {
-  if ( board.pegs[0][1] < board.pegs[0][2]) {
-
-    console.log(successMsg, printBoard(board));
-  } else if (currentP.board.peg[0][1] > nextP.board.peg[0][2]){
-    console.log(errorMsg, printBoard(board));
+  // Check if the target peg exists in the board
+  if (!board[targetPeg]) {
+    console.error(`Error: The selected peg '${targetPeg}' does not exist.`);
+    // Put the disc back on the source peg
+    board[currentPeg].push(disc);
+    return;
+  };
+  // Check if the move is valid (smaller disc on top)
+  if (board[targetPeg].length === 0 || disc < board[targetPeg][board[targetPeg].length - 1]) {
+    // This should add the disc to the top of the target peg
+    board[targetPeg].push(disc);
+    console.log(`Success! You moved disc ${disc} from ${currentPeg} to ${targetPeg}. The board is now: `);
+    printBoard(board);
+  } else {
+    // Invalid move: Put the disc back on the previous peg
+    board[currentPeg].push(disc);
+    console.log(`Invalid move: You cannot place disc ${disc} on top of a smaller one. The board is still: `);
+    printBoard(board);
   };
 };
+
 
 const checkWinner = () => {
-  const winning = [5, 4, 3, 2, 1];
-  if (board.peg2 === winning || board.peg3 === winning) {
-    console.log('Congrats, you win!'); 
+  if (board.peg2.length === 5 || board.peg3.length === 5) {
+    console.log("Congratulations! You've won the game!");
+
+    resetBoard();
+    console.log('Board has been reset:');
+    printBoard(board);
     
-    reset();
+  } else {
+    console.log('Keep playing!');
   };
 };
 
-console.log(checkWinner());
 
-const reset = function() {
-  board.peg1[0].empty();
-  board.peg2[0].empty();
-  board.peg3[0].empty();
+// Steps to win:
+moveDisc('peg1', 'peg2');
+moveDisc('peg1', 'peg3');
+moveDisc('peg2', 'peg3');
+moveDisc('peg1', 'peg2');
+moveDisc('peg3', 'peg1');
+moveDisc('peg3', 'peg2');
+moveDisc('peg1', 'peg2');
+moveDisc('peg1', 'peg3');
+moveDisc('peg2', 'peg3');
+moveDisc('peg2', 'peg1');
+moveDisc('peg3', 'peg1');
+moveDisc('peg2', 'peg3');
+moveDisc('peg1', 'peg2');
+moveDisc('peg1', 'peg3');
+moveDisc('peg2', 'peg3');
+moveDisc('peg1', 'peg2');
+moveDisc('peg3', 'peg1');
+moveDisc('peg3', 'peg2');
+moveDisc('peg1', 'peg2');
+moveDisc('peg3', 'peg1');
+moveDisc('peg2', 'peg3');
+moveDisc('peg2', 'peg1');
+moveDisc('peg3', 'peg1');
+moveDisc('peg3', 'peg2');
+moveDisc('peg1', 'peg2');
+moveDisc('peg1', 'peg3');
+moveDisc('peg2', 'peg3');
+moveDisc('peg1', 'peg2');
+moveDisc('peg3', 'peg1');
+moveDisc('peg3', 'peg2');
+moveDisc('peg1', 'peg2');
+
+checkWinner();
+
+
+function resetBoard() {
+  // Deep copy the initialBoard to avoid modifying the original
+  board.peg1 = [...startingBoard.peg1];
+  board.peg2 = [...startingBoard.peg2];
+  board.peg3 = [...startingBoard.peg3];
 };
-
-
-
-// Tests:
-// Move the disc from peg 1 to peg 2
-moveDisc(1, 2);
-console.log(successMsg);
-
-// Move disc from peg 1 to peg 3
-moveDisc(1, 3);
-console.log(successMsg);
-
-// Move disc from peg 1 to peg 2
-moveDisc(1, 2);
-console.log(errorMsg);
-
-
-
-
